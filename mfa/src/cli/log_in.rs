9e0662547959f3775e92de1f7ae6549a1da36eea
bouzuya <m@bouzuya.net>
2020-12-22 21:25:49 +0900

    add get_employee_session_new to log_in

--- mfa/src/cli/log_in.rs
+++ mfa/src/cli/log_in.rs
@@ -1,6 +1,19 @@
 use anyhow::Result;
 
+use crate::http_client::{HttpClient, HttpMethod, HttpResponse};
+
 pub fn log_in() -> Result<()> {
-    println!("log-in");
+    let response = get_employee_session_new()?;
+    println!("{:?}", response.status());
+    println!("{:?}", response.body());
+    println!("{:?}", response.cookie());
     Ok(())
+}
+
+fn get_employee_session_new() -> Result<HttpResponse> {
+    let url = "https://attendance.moneyforward.com/employee_session/new";
+    let client = HttpClient::new()?;
+    let request = client.request(HttpMethod::GET, url)?;
+    let response = client.execute(request)?;
+    Ok(response)
 }
--- mfa/src/http_client.rs
+++ mfa/src/http_client.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use reqwest::{
     blocking::{Client, Request, Response},
     redirect::Policy,
@@ -9,16 +9,13 @@ pub struct HttpClient(Client);
 
 impl HttpClient {
     pub fn new() -> Result<Self> {
-        let client = Client::builder()
-            .redirect(Policy::none())
-            .build()
-            .with_context(|| "http client build")?;
+        let client = Client::builder().redirect(Policy::none()).build()?;
         Ok(Self(client))
     }
 
     pub fn execute(&self, request: HttpRequest) -> Result<HttpResponse> {
         let response = self.0.execute(request.0)?;
-        Ok(HttpResponse::of(response))
+        Ok(HttpResponse::of(response)?)
     }
 
     pub fn request(&self, method: HttpMethod, url: &str) -> Result<HttpRequest> {
@@ -44,10 +41,37 @@ impl HttpRequest {
 }
 
 #[derive(Debug)]
-pub struct HttpResponse(Response);
+pub struct HttpResponse {
+    body: String,
+    cookie: String,
+    status: u16,
+}
 
 impl HttpResponse {
-    fn of(response: Response) -> Self {
-        Self(response)
+    fn of(response: Response) -> Result<Self> {
+        let cookie = response
+            .cookies()
+            .map(|c| format!("{}={}", c.name(), c.value()))
+            .collect::<Vec<String>>()
+            .join("; ");
+        let status = response.status().as_u16();
+        let body = response.text()?;
+        Ok(Self {
+            body,
+            cookie,
+            status,
+        })
+    }
+
+    pub fn body(&self) -> String {
+        self.body.to_string()
+    }
+
+    pub fn cookie(&self) -> String {
+        self.cookie.to_string()
+    }
+
+    pub fn status(&self) -> u16 {
+        self.status
     }
 }
