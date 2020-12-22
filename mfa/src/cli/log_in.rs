47daf65a73b417ff73926440e4e8a0d7da667817
bouzuya <m@bouzuya.net>
2020-12-22 21:28:04 +0900

    add parse_employee_session_new_html to log_in

--- mfa/src/cli/log_in.rs
+++ mfa/src/cli/log_in.rs
@@ -1,12 +1,15 @@
-use anyhow::Result;
-
 use crate::http_client::{HttpClient, HttpMethod, HttpResponse};
+use anyhow::{Context, Result};
 
 pub fn log_in() -> Result<()> {
     let response = get_employee_session_new()?;
+    let html = response.body();
+    let authenticity_token =
+        parse_employee_session_new_html(&html).with_context(|| "no authenticity_token")?;
     println!("{:?}", response.status());
     println!("{:?}", response.body());
     println!("{:?}", response.cookie());
+    println!("{:?}", authenticity_token);
     Ok(())
 }
 
@@ -16,4 +19,19 @@ fn get_employee_session_new() -> Result<HttpResponse> {
     let request = client.request(HttpMethod::GET, url)?;
     let response = client.execute(request)?;
     Ok(response)
+}
+
+fn parse_employee_session_new_html(s: &str) -> Option<String> {
+    use scraper::html::Select;
+    use scraper::node::Element;
+    use scraper::{ElementRef, Html, Selector};
+
+    let document = Html::parse_document(s);
+    let selector = Selector::parse(r#"input[name="authenticity_token"]"#).unwrap();
+    let mut select: Select = document.select(&selector);
+    select.next().and_then(|input: ElementRef| {
+        let element: &Element = input.value();
+        let value: Option<&str> = element.attr("value");
+        value.map(|s| s.to_string())
+    })
 }
