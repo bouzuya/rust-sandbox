ed8a780f281ad0ed1a40edf7a83e26e397b59b40
bouzuya <m@bouzuya.net>
2021-05-02 15:21:27 +0900

    b-ls: add recursive option

--- b-ls/src/main.rs
+++ b-ls/src/main.rs
@@ -1,26 +1,51 @@
-use std::{fs, io};
+use std::{fs, io, path::Path};
+use structopt::StructOpt;
 
-fn read_dir(path: &str) -> io::Result<Vec<fs::DirEntry>> {
+#[derive(Debug, StructOpt)]
+#[structopt(name = "b-ls", about = "bouzuya's ls: list directory contents")]
+struct Opt {
+    #[structopt(
+        short = "R",
+        long = "recursive",
+        help = "List subdirectories recursively"
+    )]
+    recursive: bool,
+}
+
+fn read_dir(dir: &Path) -> io::Result<Vec<fs::DirEntry>> {
     let mut entries = vec![];
-    for entry in fs::read_dir(path)? {
+    for entry in dir.read_dir()? {
         entries.push(entry?);
     }
     Ok(entries)
 }
 
-fn main() -> io::Result<()> {
-    let entries = read_dir(".").expect("read_dir failed");
+fn list(opt: &Opt, dir: &Path, file_names: &mut Vec<String>) {
+    let entries = read_dir(dir).expect("read_dir failed");
 
-    let mut file_names = vec![];
     for entry in entries {
         let file_name = entry
-            .file_name()
-            .into_string()
-            .expect("file_name is not string");
+            .path()
+            .strip_prefix(".")
+            .expect("strip_prefix failed")
+            .to_str()
+            .expect("file_name is not string")
+            .to_string();
         let file_type = entry.file_type().expect("file_type failed");
         let is_dir = file_type.is_dir();
         file_names.push(format!("{}{}", file_name, if is_dir { "/" } else { "" }));
+        if is_dir && opt.recursive {
+            list(opt, entry.path().as_path(), file_names);
+        }
     }
+}
+
+fn main() -> io::Result<()> {
+    let opt = Opt::from_args();
+
+    let mut file_names = vec![];
+
+    list(&opt, &Path::new("."), &mut file_names);
 
     file_names.sort();
 
