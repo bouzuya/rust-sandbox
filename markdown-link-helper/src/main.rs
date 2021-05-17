22852ad815d5d2ddbc51fdded0d4370a99e3bc78
bouzuya <m@bouzuya.net>
2021-05-17 21:10:50 +0900

    markdown-link-helper: add --help and --version

--- markdown-link-helper/src/main.rs
+++ markdown-link-helper/src/main.rs
@@ -1,8 +1,16 @@
-use std::{env, fs};
+use std::fs;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "markdown-link-helper", about = "markdown link helper")]
+struct Opt {
+    #[structopt(name = "FILE", help = "The markdown file")]
+    file: String,
+}
 
 fn main() {
-    let file = env::args().nth(1).expect("The file is not specified.");
-    let content = fs::read_to_string(&file).expect("Failed to load the file.");
+    let opt = Opt::from_args();
+    let content = fs::read_to_string(&opt.file).unwrap();
     let rules = markdown_link_helper::build_rules();
     markdown_link_helper::run(&rules, &content);
 }
