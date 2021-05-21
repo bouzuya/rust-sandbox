06e71d6a24100729fbfd84be50a484d18cca5325
bouzuya <m@bouzuya.net>
2021-05-21 22:29:41 +0900

    b: fix lib

--- b/src/lib.rs
+++ b/src/lib.rs
@@ -1,32 +1,8 @@
+mod entry;
 mod parse;
+mod template;
+mod template_entry;
 
-use parse::{parse, Token};
-
-use std::collections::BTreeMap;
-
-pub fn render(template: &str, data: &BTreeMap<String, String>) -> String {
-    let mut t = String::new();
-    let tokens = parse(template).unwrap();
-    for token in tokens {
-        match token {
-            Token::Str(s) => t.push_str(s),
-            Token::Var(v) => {
-                let v = data.get(v).unwrap();
-                t.push_str(v.as_str());
-            }
-        }
-    }
-    t
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn render_test() {
-        let mut map = BTreeMap::new();
-        map.insert("x".to_string(), "y".to_string());
-        assert_eq!(render("{{x}}", &map), "y".to_string());
-    }
-}
+pub use entry::{list_entries, Entry};
+pub use template::{ParseTemplateError, Template};
+pub use template_entry::TemplateEntry;
--- b/src/main.rs
+++ b/src/main.rs
@@ -1,11 +1,11 @@
-use b::render;
+use b::{list_entries, TemplateEntry};
 use serde_json::Value;
 use std::{
     collections::BTreeMap,
-    fs,
+    convert::TryFrom,
+    env, fs,
     io::{self, Read},
-    path::{Path, PathBuf},
-    str::FromStr,
+    path::PathBuf,
 };
 use structopt::StructOpt;
 
@@ -28,55 +28,6 @@ enum Subcommand {
         )]
         template: PathBuf,
     },
-}
-
-fn read_dir(dir: &Path) -> io::Result<Vec<fs::DirEntry>> {
-    let mut entries = vec![];
-    for entry in dir.read_dir()? {
-        entries.push(entry?);
-    }
-    Ok(entries)
-}
-
-fn list(template_file: &Path) -> Vec<String> {
-    let mut file_names = vec![];
-    file_names.push(template_file.to_str().unwrap().to_string());
-    if template_file.is_dir() {
-        list2(&mut file_names, template_file);
-    }
-    file_names
-}
-
-fn list2(file_names: &mut Vec<String>, dir: &Path) {
-    let entries = read_dir(dir).expect("read_dir failed");
-    for entry in entries {
-        let path_buf = entry.path();
-        let file_name = path_buf
-            .to_str()
-            .expect("file_name is not string")
-            .to_string();
-        let file_type = entry.file_type().expect("file_type failed");
-        let is_dir = file_type.is_dir();
-        file_names.push(file_name);
-        if is_dir {
-            list2(file_names, path_buf.as_path());
-        }
-    }
-}
-
-fn create(tmpl: &Path, root_dir: &Path, data: &BTreeMap<String, String>) {
-    let file_name_tmpl = tmpl.strip_prefix(root_dir).unwrap().to_str().unwrap();
-    let dest = render(file_name_tmpl, &data);
-    if tmpl.is_dir() {
-        if !PathBuf::from_str(dest.as_str()).unwrap().exists() {
-            fs::create_dir(dest.as_str()).unwrap();
-        }
-    } else {
-        let content_tmpl = fs::read_to_string(tmpl).unwrap();
-        let content = render(content_tmpl.as_str(), &data);
-        fs::write(dest.as_str(), content).unwrap();
-    }
-    println!("{}", dest.as_str());
 }
 
 fn main() {
@@ -109,10 +60,15 @@ fn main() {
                 map
             };
 
-            let file_names = list(template.as_path());
-            let root_dir = template.parent().unwrap();
-            for file_name in file_names {
-                create(PathBuf::from(file_name).as_path(), root_dir, &data);
+            let entries = list_entries(template.as_path()).unwrap();
+            let templates = entries
+                .iter()
+                .map(|e| TemplateEntry::try_from(e))
+                .collect::<Result<Vec<TemplateEntry>, _>>()
+                .unwrap();
+            let root_dir = env::current_dir().unwrap();
+            for template in templates {
+                template.render(root_dir.as_path(), &data);
             }
         }
     }
