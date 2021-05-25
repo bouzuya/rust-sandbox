f45daa7e67e5dbd7c0db0b12fc973ff53b2ea577
bouzuya <m@bouzuya.net>
2021-05-25 23:50:26 +0900

    bbn: fix integration tests

--- bbn/tests/bbn.rs
+++ bbn/tests/bbn.rs
@@ -1,11 +1,23 @@
 use assert_cmd::Command;
+use std::fs;
+use tempfile::tempdir;
 
 #[test]
 fn test() {
+    let dir = tempdir().unwrap();
+    let data = dir.path().join("data");
+    let data2021 = data.join("2021");
+    let data202102 = data2021.join("02");
+    fs::create_dir_all(data202102.as_path()).unwrap();
+    let data20210203 = data202102.join("2021-02-03-TITLE.json");
+    fs::write(data20210203, r#"{"title":"TITLE1"}"#).unwrap();
+
     Command::cargo_bin("bbn")
         .unwrap()
         .arg("list")
-        .arg("date:--05-23")
+        .arg("--data-dir")
+        .arg(data.canonicalize().unwrap().as_path().to_str().unwrap())
+        .arg("date:--02-03")
         .assert()
         .success();
 }
