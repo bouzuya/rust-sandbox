4594c973e308c419fd4ff18016c0635e3b6d39a2
bouzuya <m@bouzuya.net>
2021-05-25 23:41:20 +0900

    bbn: use new query

--- bbn/src/main.rs
+++ bbn/src/main.rs
@@ -1,15 +1,10 @@
+mod post;
 mod query;
 
+use post::list_posts;
 use query::Query;
-use serde_json::Value;
-use std::{
-    convert::TryFrom,
-    ffi::OsStr,
-    fs, io,
-    path::{Path, PathBuf},
-};
+use std::{convert::TryFrom, path::PathBuf};
 use structopt::StructOpt;
-use thiserror::Error;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -28,51 +23,12 @@ enum Subcommand {
     },
 }
 
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct Post {
-    date: String,
-    title: String,
-}
-
-#[derive(Debug, Error)]
-enum ListPostsError {
-    #[error("read dir error")]
-    ReadDir(#[from] io::Error),
-}
-
-fn list_posts(path: &Path, query: &str) -> Result<Vec<Post>, ListPostsError> {
-    let mut entries = vec![];
-    if path.is_dir() {
-        for dir_entry in path.read_dir()? {
-            let path_buf = path.join(dir_entry?.path());
-            let dir_posts = list_posts(path_buf.as_path(), query)?;
-            entries.extend(dir_posts);
-        }
-    } else {
-        if path.extension() == Some(OsStr::new("json")) {
-            let date = path.file_stem().unwrap().to_str().unwrap().to_string();
-            // YYYY-MM-DD
-            if &date[5..] == query {
-                let content = fs::read_to_string(path)?;
-                let json: Value = serde_json::from_str(&content).unwrap();
-                let title = json.get("title").unwrap().as_str().unwrap().to_string();
-                entries.push(Post { date, title });
-            }
-        }
-    }
-    Ok(entries)
-}
-
 fn main() {
     let opt = Opt::from_args();
     match opt.subcommand {
         Subcommand::List { data_dir, query } => {
             let query = Query::try_from(query.as_str()).unwrap();
-            let q = query.to_string().as_bytes()[7..]
-                .iter()
-                .map(|&b| char::from_u32(b as u32).unwrap())
-                .collect::<String>();
-            let mut posts = list_posts(data_dir.as_path(), &q).unwrap();
+            let mut posts = list_posts(data_dir.as_path(), &query).unwrap();
             posts.sort();
             posts.reverse();
             for post in posts {
---
+++ bbn/src/post.rs
@@ -0,0 +1,106 @@
+use crate::query::Query;
+use serde_json::Value;
+use std::{ffi::OsStr, fs, io, path::Path};
+use thiserror::Error;
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Post {
+    pub date: String,
+    pub title: String,
+}
+
+#[derive(Debug, Error)]
+pub enum ListPostsError {
+    #[error("read dir error")]
+    ReadDir(#[from] io::Error),
+}
+
+fn get_date<'a>(path: &'a Path) -> Option<&'a str> {
+    path.file_stem()
+        .and_then(|os_str| os_str.to_str())
+        .and_then(|s| s.get(0..10))
+}
+
+pub fn list_posts(path: &Path, query: &Query) -> Result<Vec<Post>, ListPostsError> {
+    // data/YYYY/MM/YYYY-MM-DD(-TITLE).json
+    list_posts_year(path, query)
+}
+
+fn list_posts_year(path: &Path, query: &Query) -> Result<Vec<Post>, ListPostsError> {
+    let mut years = vec![];
+    for dir_entry in path.read_dir()? {
+        let path_buf = dir_entry?.path();
+        match query.year() {
+            None => years.push(path_buf),
+            Some(yyyy) => {
+                if path_buf.file_name() == Some(OsStr::new(yyyy)) {
+                    years.push(path_buf);
+                }
+            }
+        }
+    }
+    let mut posts = vec![];
+    for year in years {
+        let path_buf = path.join(year);
+        let dir_posts = list_posts_month(path_buf.as_path(), query)?;
+        posts.extend(dir_posts);
+    }
+    posts.sort();
+    Ok(posts)
+}
+
+fn list_posts_month(path: &Path, query: &Query) -> Result<Vec<Post>, ListPostsError> {
+    let mut months = vec![];
+    for dir_entry in path.read_dir()? {
+        let path_buf = dir_entry?.path();
+        match query.month() {
+            None => months.push(path_buf),
+            Some(mm) => {
+                if path_buf.file_name() == Some(OsStr::new(mm)) {
+                    months.push(path_buf);
+                }
+            }
+        }
+    }
+    let mut posts = vec![];
+    for month in months {
+        let path_buf = path.join(month);
+        let dir_posts = list_posts_day(path_buf.as_path(), query)?;
+        posts.extend(dir_posts);
+    }
+    Ok(posts)
+}
+
+fn list_posts_day(path: &Path, query: &Query) -> Result<Vec<Post>, ListPostsError> {
+    let mut days = vec![];
+    for dir_entry in path.read_dir()? {
+        let path_buf = dir_entry?.path();
+        if path_buf.extension() != Some(OsStr::new("json")) {
+            continue;
+        }
+        match query.day() {
+            None => days.push(path_buf),
+            Some(dd) => {
+                // YYYY-MM-DD(-TITLE).json
+                if let Some(file_stem) = path_buf.file_stem() {
+                    if let Some(file_stem) = file_stem.to_str() {
+                        if file_stem.get(8..8 + 2) == Some(dd) {
+                            days.push(path_buf);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let mut posts = vec![];
+    for day in days {
+        let path_buf = path.join(day);
+        let date = get_date(path_buf.as_path()).unwrap().to_string();
+        let content = fs::read_to_string(path_buf)?;
+        let json: Value = serde_json::from_str(&content).unwrap();
+        let title = json.get("title").unwrap().as_str().unwrap().to_string();
+        posts.push(Post { date, title });
+    }
+    Ok(posts)
+}
