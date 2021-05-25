d70bc01ea0f85a180fc62d736606eb44bbcd63c4
bouzuya <m@bouzuya.net>
2021-05-25 23:38:55 +0900

    bbn: improve date: query

--- bbn/src/query.rs
+++ bbn/src/query.rs
@@ -1,4 +1,5 @@
 use nom::{
+    branch::alt,
     bytes::complete::tag,
     character::complete::{char, one_of},
     combinator::all_consuming,
@@ -8,49 +9,111 @@ use nom::{
 use thiserror::Error;
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Query((u8, u8));
+pub struct Query(Date);
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Date(Option<String>, Option<String>, Option<String>);
 
 #[derive(Debug, Error)]
 pub enum ParseQueryError {
     #[error("parse error")]
     Parse,
-    #[error("invalid number error")]
-    InvalidNumber,
 }
 
 impl std::fmt::Display for Query {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "date:--{:02}-{:02}", self.0 .0, self.0 .1)
+        let date = &self.0;
+        match (&date.0, &date.1, &date.2) {
+            (None, None, None) => write!(f, ""),
+            (None, None, Some(dd)) => write!(f, "date:---{}", dd),
+            (None, Some(mm), None) => write!(f, "date:--{}", mm),
+            (None, Some(mm), Some(dd)) => write!(f, "date:--{}-{}", mm, dd),
+            (Some(yyyy), None, None) => write!(f, "date:{}", yyyy),
+            (Some(_), None, Some(_)) => unreachable!(),
+            (Some(yyyy), Some(mm), None) => write!(f, "date:{}-{}", yyyy, mm),
+            (Some(yyyy), Some(mm), Some(dd)) => write!(f, "date:{}-{}-{}", yyyy, mm, dd),
+        }
     }
 }
 
-fn parse(s: &str) -> IResult<&str, (Vec<char>, Vec<char>)> {
-    // date:--MM-DD
-    let (s, _) = tag("date:")(s)?;
+fn yyyymmdd(s: &str) -> IResult<&str, Date> {
+    let (s, y) = count(one_of("0123456789"), 4)(s)?;
+    let (s, _) = char('-')(s)?;
+    let (s, m) = count(one_of("0123456789"), 2)(s)?;
+    let (s, _) = char('-')(s)?;
+    let (s, d) = count(one_of("0123456789"), 2)(s)?;
+    Ok((
+        s,
+        Date(
+            Some(y.iter().collect::<String>()),
+            Some(m.iter().collect::<String>()),
+            Some(d.iter().collect::<String>()),
+        ),
+    ))
+}
+
+fn yyyymm(s: &str) -> IResult<&str, Date> {
+    let (s, y) = count(one_of("0123456789"), 4)(s)?;
+    let (s, _) = char('-')(s)?;
+    let (s, m) = count(one_of("0123456789"), 2)(s)?;
+    Ok((
+        s,
+        Date(
+            Some(y.iter().collect::<String>()),
+            Some(m.iter().collect::<String>()),
+            None,
+        ),
+    ))
+}
+
+fn yyyy(s: &str) -> IResult<&str, Date> {
+    let (s, y) = count(one_of("0123456789"), 4)(s)?;
+    Ok((s, Date(Some(y.iter().collect::<String>()), None, None)))
+}
+
+fn mmdd(s: &str) -> IResult<&str, Date> {
     let (s, _) = char('-')(s)?;
     let (s, _) = char('-')(s)?;
     let (s, m) = count(one_of("0123456789"), 2)(s)?;
     let (s, _) = char('-')(s)?;
-    let (s, d) = all_consuming(count(one_of("0123456789"), 2))(s)?;
-    Ok((s, (m, d)))
+    let (s, d) = count(one_of("0123456789"), 2)(s)?;
+    Ok((
+        s,
+        Date(
+            None,
+            Some(m.iter().collect::<String>()),
+            Some(d.iter().collect::<String>()),
+        ),
+    ))
+}
+
+fn mm(s: &str) -> IResult<&str, Date> {
+    let (s, _) = char('-')(s)?;
+    let (s, _) = char('-')(s)?;
+    let (s, m) = count(one_of("0123456789"), 2)(s)?;
+    Ok((s, Date(None, Some(m.iter().collect::<String>()), None)))
+}
+
+fn dd(s: &str) -> IResult<&str, Date> {
+    let (s, _) = char('-')(s)?;
+    let (s, _) = char('-')(s)?;
+    let (s, _) = char('-')(s)?;
+    let (s, d) = count(one_of("0123456789"), 2)(s)?;
+    Ok((s, Date(None, None, Some(d.iter().collect::<String>()))))
+}
+
+fn parse(s: &str) -> IResult<&str, Date> {
+    let (s, _) = tag("date:")(s)?;
+    let (s, date) = all_consuming(alt((yyyymmdd, yyyymm, yyyy, mmdd, mm, dd)))(s)?;
+    Ok((s, date))
 }
 
 impl std::str::FromStr for Query {
     type Err = ParseQueryError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (_, (m, d)) = parse(s).map_err(|_| ParseQueryError::Parse)?;
-        let m = m
-            .iter()
-            .collect::<String>()
-            .parse::<u8>()
-            .map_err(|_| ParseQueryError::InvalidNumber)?;
-        let d = d
-            .iter()
-            .collect::<String>()
-            .parse::<u8>()
-            .map_err(|_| ParseQueryError::InvalidNumber)?;
-        Ok(Self((m, d)))
+        let (_, date) = parse(s).map_err(|_| ParseQueryError::Parse)?;
+        Ok(Self(date))
     }
 }
 
@@ -60,11 +123,12 @@ mod tests {
 
     #[test]
     fn str_convert() {
-        // --MM-DD
-        assert_eq!(
-            "date:--01-01".parse::<Query>().unwrap().to_string(),
-            "date:--01-01".to_string()
-        );
-        assert_eq!("date:--01-01".parse::<Query>().unwrap(), Query((1, 1)));
+        let f = |s: &str| assert_eq!(s.parse::<Query>().unwrap().to_string(), s.to_string());
+        f("date:2021-02-03");
+        f("date:2021-02");
+        f("date:2021");
+        f("date:--02-03");
+        f("date:--02");
+        f("date:---03");
     }
 }
