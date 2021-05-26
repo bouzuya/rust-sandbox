8f461b5d1e1db689e69002f2577a401ba9a79fa7
bouzuya <m@bouzuya.net>
2021-05-26 22:37:48 +0900

    bbn: add view subcommand

--- bbn/src/main.rs
+++ bbn/src/main.rs
@@ -1,6 +1,7 @@
 mod post;
 mod query;
 
+use date_range::date::Date;
 use post::list_posts;
 use query::Query;
 use std::{convert::TryFrom, path::PathBuf};
@@ -21,6 +22,13 @@ enum Subcommand {
         #[structopt(name = "query", help = "query")]
         query: String,
     },
+    #[structopt(name = "view", about = "Views the blog post")]
+    View {
+        #[structopt(long = "data-dir", help = "the data dir")]
+        data_dir: PathBuf,
+        #[structopt(name = "date", help = "the date")]
+        date: Date,
+    },
 }
 
 fn main() {
@@ -34,6 +42,18 @@ fn main() {
             for post in posts {
                 println!("{} {}", post.date, post.title);
             }
+        }
+        Subcommand::View { data_dir, date } => {
+            let query_string = format!("date:{}", date);
+            let query = Query::try_from(query_string.as_str()).unwrap();
+            let posts = list_posts(data_dir.as_path(), &query).unwrap();
+            let post = posts.first().unwrap();
+            println!(
+                "{} {} https://blog.bouzuya.net/{}/",
+                post.date,
+                post.title,
+                date.to_string().replace('-', "/")
+            );
         }
     }
 }
--- bbn/tests/bbn.rs
+++ bbn/tests/bbn.rs
@@ -3,7 +3,7 @@ use std::fs;
 use tempfile::tempdir;
 
 #[test]
-fn test() {
+fn list_test() {
     let dir = tempdir().unwrap();
     let data = dir.path().join("data");
     let data2021 = data.join("2021");
@@ -20,4 +20,25 @@ fn test() {
         .arg("date:--02-03")
         .assert()
         .success();
+}
+
+#[test]
+fn view_test() {
+    let dir = tempdir().unwrap();
+    let data = dir.path().join("data");
+    let data2021 = data.join("2021");
+    let data202102 = data2021.join("02");
+    fs::create_dir_all(data202102.as_path()).unwrap();
+    let data20210203 = data202102.join("2021-02-03-TITLE.json");
+    fs::write(data20210203, r#"{"title":"TITLE1"}"#).unwrap();
+
+    Command::cargo_bin("bbn")
+        .unwrap()
+        .arg("view")
+        .arg("--data-dir")
+        .arg(data.canonicalize().unwrap().as_path().to_str().unwrap())
+        .arg("2021-02-03")
+        .assert()
+        .success()
+        .stdout("2021-02-03 TITLE1 https://blog.bouzuya.net/2021/02/03/\n");
 }
