bd483e3069bb940c6a857d72c9e9de411f77cd5c
bouzuya <m@bouzuya.net>
2021-05-28 22:34:08 +0900

    bejson: add stdin support

--- bejson/src/main.rs
+++ bejson/src/main.rs
@@ -1,5 +1,11 @@
 use bejson::JsonValue;
-use std::{fs, path::PathBuf};
+use fs::File;
+use std::{
+    error::Error,
+    fs,
+    io::{self, Read},
+    path::{Path, PathBuf},
+};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -7,10 +13,27 @@ struct Opt {
     file: PathBuf,
 }
 
-fn main() {
-    // TODO: tests/bejson.rs
+fn read_to_string_from_handle(handle: &mut impl Read) -> io::Result<String> {
+    let mut content = String::new();
+    handle.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn read_file_or_stdin(path: &Path) -> io::Result<String> {
+    if path == Path::new("-") {
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        read_to_string_from_handle(&mut handle)
+    } else {
+        read_to_string_from_handle(&mut File::open(path)?)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
-    let content = fs::read_to_string(opt.file).unwrap();
-    let json = content.parse::<JsonValue>().unwrap();
-    println!("{}", json.eval().unwrap());
+    let content = read_file_or_stdin(opt.file.as_path())?;
+    let bejson = content.parse::<JsonValue>()?;
+    let json = bejson.eval()?;
+    println!("{}", json);
+    Ok(())
 }
