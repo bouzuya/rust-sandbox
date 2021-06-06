47cf738ea02f4f80622ac6030a23df028f69ca25
bouzuya <m@bouzuya.net>
2021-06-06 14:57:39 +0900

    weight: add repository

--- weight/src/main.rs
+++ weight/src/main.rs
@@ -4,6 +4,7 @@ mod sqlite;
 
 use crate::jsonl::{read_jsonl, write_jsonl};
 use crate::set::Set;
+use async_trait::async_trait;
 use sqlite::{read_sqlite, write_sqlite};
 use std::{collections::BTreeMap, io, path::PathBuf};
 use structopt::{clap::Shell, StructOpt};
@@ -50,19 +51,71 @@ enum Subcommand {
     },
 }
 
+#[async_trait]
+trait EventRepository {
+    async fn find_all(&self) -> anyhow::Result<Vec<Set>>;
+    async fn save(&self, events: &Vec<Set>) -> anyhow::Result<()>;
+}
+
+struct JsonlEventRepository {
+    data_file: PathBuf,
+}
+
+impl JsonlEventRepository {
+    fn new(data_file: PathBuf) -> Self {
+        Self { data_file }
+    }
+}
+
+#[async_trait]
+impl EventRepository for JsonlEventRepository {
+    async fn find_all(&self) -> anyhow::Result<Vec<Set>> {
+        Ok(read_jsonl(self.data_file.as_path())?)
+    }
+
+    async fn save(&self, events: &Vec<Set>) -> anyhow::Result<()> {
+        Ok(write_jsonl(self.data_file.as_path(), events)?)
+    }
+}
+
+struct SqliteEventRepository {
+    data_file: PathBuf,
+}
+
+impl SqliteEventRepository {
+    fn new(data_file: PathBuf) -> Self {
+        Self { data_file }
+    }
+}
+
+#[async_trait]
+impl EventRepository for SqliteEventRepository {
+    async fn find_all(&self) -> anyhow::Result<Vec<Set>> {
+        Ok(read_sqlite(self.data_file.as_path()).await?)
+    }
+
+    async fn save(&self, events: &Vec<Set>) -> anyhow::Result<()> {
+        Ok(write_sqlite(self.data_file.as_path(), events).await?)
+    }
+}
+
+fn event_repository(data_file_type: DataFileType, data_file: PathBuf) -> Box<dyn EventRepository> {
+    match data_file_type {
+        DataFileType::Jsonl => Box::new(JsonlEventRepository::new(data_file)),
+        DataFileType::Sqlite => Box::new(SqliteEventRepository::new(data_file)),
+    }
+}
+
 #[async_std::main]
 async fn main() {
     let opt = Opt::from_args();
-
+    let repository = event_repository(opt.data_file_type, opt.data_file);
     match opt.subcommand {
         Subcommand::Completion { shell } => {
             Opt::clap().gen_completions_to("weight", shell, &mut io::stdout())
         }
         Subcommand::List => {
-            let events = match opt.data_file_type {
-                DataFileType::Jsonl => read_jsonl(opt.data_file.as_path()).unwrap(),
-                DataFileType::Sqlite => read_sqlite(opt.data_file.as_path()).await.unwrap(),
-            };
+            let events = repository.find_all().await.unwrap();
             let state = events.iter().fold(BTreeMap::new(), |mut map, e| {
                 map.insert(e.key(), e.value());
                 map
@@ -72,17 +125,9 @@ async fn main() {
             }
         }
         Subcommand::Set { key, value } => {
-            let mut events = match opt.data_file_type {
-                DataFileType::Jsonl => read_jsonl(opt.data_file.as_path()).unwrap(),
-                DataFileType::Sqlite => read_sqlite(opt.data_file.as_path()).await.unwrap(),
-            };
+            let mut events = repository.find_all().await.unwrap();
             events.push(Set::new(key, value).unwrap());
-            match opt.data_file_type {
-                DataFileType::Jsonl => write_jsonl(opt.data_file.as_path(), &events).unwrap(),
-                DataFileType::Sqlite => write_sqlite(opt.data_file.as_path(), &events)
-                    .await
-                    .unwrap(),
-            };
+            repository.save(&events).await.unwrap();
         }
     }
 }
