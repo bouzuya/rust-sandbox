c309b4e378c76fbe52a69a411bb0e1febbaed70b
bouzuya <m@bouzuya.net>
2021-07-06 23:12:23 +0900

    bbn: refactor to use bbn_repository in view

--- bbn/src/command/view.rs
+++ bbn/src/command/view.rs
@@ -1,19 +1,20 @@
-use crate::post::list_posts;
-use crate::query::Query;
+use crate::bbn_repository::BbnRepository;
+use anyhow::bail;
 use date_range::date::Date;
-use std::convert::TryFrom;
 use std::path::PathBuf;
 
 pub fn view(data_dir: PathBuf, date: Date) -> anyhow::Result<()> {
-    let query_string = format!("date:{}", date);
-    let query = Query::try_from(query_string.as_str()).unwrap();
-    let posts = list_posts(data_dir.as_path(), &query).unwrap();
-    let post = posts.first().unwrap();
-    println!(
-        "{} {} https://blog.bouzuya.net/{}/",
-        post.date,
-        post.title,
-        date.to_string().replace('-', "/")
-    );
+    let repository = BbnRepository::new(data_dir);
+    match repository.find_by_date(date)? {
+        Some(entry) => {
+            println!(
+                "{} {} https://blog.bouzuya.net/{}/",
+                entry.entry_id.date(),
+                entry.title,
+                date.to_string().replace('-', "/")
+            );
+        }
+        None => bail!("not found"),
+    }
     Ok(())
 }
