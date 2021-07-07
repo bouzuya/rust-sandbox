f7a4f766f9bda0c9186aa3d5f0d430653df5fd26
bouzuya <m@bouzuya.net>
2021-07-07 20:34:03 +0900

    bbn: refactor to use find_meta_by_id

--- bbn/src/command/view.rs
+++ bbn/src/command/view.rs
@@ -5,21 +5,25 @@ use std::path::PathBuf;
 
 pub fn view(data_dir: PathBuf, date: Date) -> anyhow::Result<()> {
     let repository = BbnRepository::new(data_dir);
-    match repository.find_by_date(date)? {
-        Some(entry) => {
+    let entry_id = repository.find_id_by_date(date)?;
+    let entry_meta = entry_id
+        .as_ref()
+        .and_then(|entry_id| repository.find_meta_by_id(entry_id).transpose())
+        .transpose()?;
+    match (entry_id, entry_meta) {
+        (Some(entry_id), Some(entry_meta)) => {
             println!(
                 "{}{} {} https://blog.bouzuya.net/{}/",
-                entry.entry_id.date(),
-                entry
-                    .entry_id
+                entry_id.date(),
+                entry_id
                     .id_title()
                     .map(|s| format!(" {}", s))
                     .unwrap_or_default(),
-                entry.title,
-                date.to_string().replace('-', "/")
+                entry_meta.title,
+                entry_id.date().to_string().replace('-', "/")
             );
         }
-        None => bail!("not found"),
+        _ => bail!("not found"),
     }
     Ok(())
 }
