b3bc280525287b0f65e87871edbd0f8d0a678b60
bouzuya <m@bouzuya.net>
2021-07-08 23:30:09 +0900

    bbn: refactor to remove unused fn

--- bbn/src/bbn_repository.rs
+++ bbn/src/bbn_repository.rs
@@ -1,19 +1,15 @@
 use std::{
-    convert::TryFrom,
     ffi::OsStr,
     fs, io,
     path::{Path, PathBuf},
     str::FromStr,
 };
 
-use anyhow::Context;
 use date_range::date::{Date, YearMonth};
 use serde_json::Value;
 use thiserror::Error;
 
-use crate::{
-    entry::Entry, entry_id::EntryId, entry_meta::EntryMeta, query::Query, timestamp::Timestamp,
-};
+use crate::{entry_id::EntryId, entry_meta::EntryMeta, query::Query, timestamp::Timestamp};
 
 #[derive(Debug, serde::Deserialize)]
 struct MetaJson {
@@ -37,47 +33,6 @@ impl MetaJson {
 #[derive(Debug)]
 pub struct BbnRepository {
     data_dir: PathBuf,
-}
-
-fn get_bbn_entry(data_dir: &Path, entry_id: EntryId) -> anyhow::Result<crate::entry::Entry> {
-    let date = entry_id.date();
-    let id_title = entry_id.id_title();
-    let dir = data_dir
-        .join(date.year().to_string())
-        .join(date.month().to_string());
-    let id_title_suffix = id_title
-        .clone()
-        .map(|s| format!("-{}", s))
-        .unwrap_or_default();
-    let content = fs::read_to_string(dir.join(format!("{}{}.md", date, id_title_suffix)))?;
-    let json_content = fs::read_to_string(dir.join(format!("{}{}.json", date, id_title_suffix)))?;
-    let json: Value = serde_json::from_str(&json_content)?;
-    let minutes = json
-        .get("minutes")
-        .context("get minutes")?
-        .as_u64()
-        .context("parse minutes")?;
-    let pubdate = Timestamp::from_rfc3339(
-        &json
-            .get("pubdate")
-            .context("get pubdate")?
-            .as_str()
-            .context("parse pubdate")?
-            .to_string(),
-    )?;
-    let title = json
-        .get("title")
-        .context("get title")?
-        .as_str()
-        .context("parse title")?
-        .to_string();
-    Ok(crate::entry::Entry {
-        content,
-        entry_id,
-        minutes,
-        pubdate,
-        title,
-    })
 }
 
 impl BbnRepository {
@@ -124,25 +79,6 @@ impl BbnRepository {
         Ok(Some(meta_json.into_meta()?))
     }
 
-    pub fn find_entry_ids(&self) -> anyhow::Result<Vec<EntryId>> {
-        let query = Query::try_from("")?;
-        let posts = list_posts(self.data_dir.as_path(), &query)?;
-        posts
-            .into_iter()
-            .map(|post| self.post_to_entry_id(post))
-            .collect::<anyhow::Result<Vec<EntryId>>>()
-    }
-
-    pub fn find_by_date(&self, date: Date) -> anyhow::Result<Option<Entry>> {
-        let query_string = format!("date:{}", date);
-        let query = Query::try_from(query_string.as_str()).unwrap();
-        let posts = list_posts(self.data_dir.as_path(), &query).unwrap();
-        match posts.first() {
-            None => Ok(None),
-            Some(post) => self.post_to_entry(post),
-        }
-    }
-
     fn find_ids_by_year_month(&self, year_month: YearMonth) -> anyhow::Result<Vec<EntryId>> {
         let entry_dir = self
             .data_dir
@@ -162,12 +98,6 @@ impl BbnRepository {
             }
         }
         Ok(entry_ids)
-    }
-
-    fn post_to_entry(&self, post: &Post) -> anyhow::Result<Option<Entry>> {
-        let date = Date::from_str(post.date.as_str())?;
-        let entry_id = EntryId::new(date, post.id_title.clone());
-        get_bbn_entry(self.data_dir.as_path(), entry_id).map(Some)
     }
 
     fn post_to_entry_id(&self, post: Post) -> anyhow::Result<EntryId> {
@@ -292,7 +222,7 @@ fn list_posts_day(path: &Path, query: &Query) -> Result<Vec<Post>, ListPostsErro
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
+    use std::{convert::TryFrom, fs};
 
     use tempfile::tempdir;
 
@@ -319,56 +249,6 @@ mod tests {
         )?;
         fs::write(entry_content_path.as_path(), r#"CONTENT2"#)?;
         Ok(data_dir)
-    }
-
-    #[test]
-    fn find_entry_ids_test() -> anyhow::Result<()> {
-        let temp_dir = tempdir()?;
-        let data_dir = create_test_dir(temp_dir.path())?;
-
-        let repository = BbnRepository::new(data_dir);
-        let entry_ids = repository.find_entry_ids()?;
-
-        assert_eq!(
-            entry_ids,
-            vec![
-                EntryId::from_str("2021-07-06")?,
-                EntryId::from_str("2021-07-07-id1")?
-            ]
-        );
-        Ok(())
-    }
-
-    #[test]
-    fn find_by_date_test() -> anyhow::Result<()> {
-        let temp_dir = tempdir()?;
-        let data_dir = create_test_dir(temp_dir.path())?;
-        let repository = BbnRepository::new(data_dir);
-        assert_eq!(
-            repository.find_by_date(Date::from_str("2021-07-06")?)?,
-            Some(Entry {
-                content: "CONTENT1".to_string(),
-                entry_id: EntryId::new(Date::from_str("2021-07-06")?, None),
-                minutes: 5,
-                pubdate: Timestamp::from_rfc3339("2021-07-06T23:59:59+09:00")?,
-                title: "TITLE1".to_string(),
-            })
-        );
-        assert_eq!(
-            repository.find_by_date(Date::from_str("2021-07-07")?)?,
-            Some(Entry {
-                content: "CONTENT2".to_string(),
-                entry_id: EntryId::new(Date::from_str("2021-07-07")?, Some("id1".to_string())),
-                minutes: 6,
-                pubdate: Timestamp::from_rfc3339("2021-07-07T23:59:59+09:00")?,
-                title: "TITLE2".to_string(),
-            })
-        );
-        assert_eq!(
-            repository.find_by_date(Date::from_str("2021-07-08")?)?,
-            None
-        );
-        Ok(())
     }
 
     #[test]
@@ -416,7 +296,21 @@ mod tests {
 
     #[test]
     fn find_ids_by_query_test() -> anyhow::Result<()> {
-        // TODO
+        // TODO: Query
+
+        let temp_dir = tempdir()?;
+        let data_dir = create_test_dir(temp_dir.path())?;
+
+        let repository = BbnRepository::new(data_dir);
+        let entry_ids = repository.find_ids_by_query(Query::try_from("")?)?;
+
+        assert_eq!(
+            entry_ids,
+            vec![
+                EntryId::from_str("2021-07-06")?,
+                EntryId::from_str("2021-07-07-id1")?
+            ]
+        );
         Ok(())
     }
 
