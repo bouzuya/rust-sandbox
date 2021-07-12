a03058dddb96ea3c7abf0976184dfa677de0f43d
bouzuya <m@bouzuya.net>
2021-07-12 22:37:24 +0900

    bbn: fix tests

--- bbn/tests/bbn.rs
+++ bbn/tests/bbn.rs
@@ -4,49 +4,83 @@ use tempfile::tempdir;
 
 #[test]
 fn list_test() {
-    let dir = tempdir().unwrap();
-    let data = dir.path().join("data");
-    let data2021 = data.join("2021");
-    let data202102 = data2021.join("02");
-    fs::create_dir_all(data202102.as_path()).unwrap();
-    let data20210203 = data202102.join("2021-02-03-TITLE.json");
+    let temp_dir = tempdir().unwrap();
+    let config_dir = temp_dir.path().join("config");
+    let data_dir = temp_dir.path().join("data");
+    let entry_dir = data_dir.join("2021").join("02");
+    fs::create_dir_all(entry_dir.as_path()).unwrap();
+    let data20210203 = entry_dir.join("2021-02-03-TITLE.json");
     fs::write(
         data20210203,
         r#"{"minutes":5,"pubdate":"2021-02-03T00:00:00+09:00","tags":[],"title":"TITLE1"}"#,
     )
     .unwrap();
+    let hatena_blog_data_file = temp_dir.path().join("hatena-blog.db");
 
     Command::cargo_bin("bbn")
         .unwrap()
-        .arg("list")
+        .arg("config")
         .arg("--data-dir")
-        .arg(data.canonicalize().unwrap().as_path().to_str().unwrap())
+        .arg(data_dir)
+        .arg("--hatena-blog-data-file")
+        .arg(hatena_blog_data_file)
+        .env(
+            "BBN_TEST_CONFIG_DIR",
+            config_dir.as_os_str().to_str().unwrap(),
+        )
+        .assert()
+        .success();
+    Command::cargo_bin("bbn")
+        .unwrap()
+        .arg("list")
         .arg("date:--02-03")
+        .env(
+            "BBN_TEST_CONFIG_DIR",
+            config_dir.as_os_str().to_str().unwrap(),
+        )
         .assert()
         .success();
 }
 
 #[test]
 fn view_test() {
-    let dir = tempdir().unwrap();
-    let data = dir.path().join("data");
-    let data2021 = data.join("2021");
-    let data202102 = data2021.join("02");
-    fs::create_dir_all(data202102.as_path()).unwrap();
-    let data20210203 = data202102.join("2021-02-03-ID_TITLE.json");
+    let temp_dir = tempdir().unwrap();
+    let config_dir = temp_dir.path().join("config");
+    let data_dir = temp_dir.path().join("data");
+    let entry_dir = data_dir.join("2021").join("02");
+    fs::create_dir_all(entry_dir.as_path()).unwrap();
+    let meta_file = entry_dir.join("2021-02-03-ID_TITLE.json");
     fs::write(
-        data20210203,
+        meta_file,
         r#"{"minutes":5,"pubdate":"2021-02-03T00:00:00+09:00","tags":[],"title":"TITLE1"}"#,
     )
     .unwrap();
+    let content_file = entry_dir.join("2021-02-03-ID_TITLE.md");
+    fs::write(content_file, r#"Hello"#).unwrap();
+    let hatena_blog_data_file = temp_dir.path().join("hatena-blog.db");
 
     Command::cargo_bin("bbn")
         .unwrap()
-        .arg("view")
+        .arg("config")
         .arg("--data-dir")
-        .arg(data.canonicalize().unwrap().as_path().to_str().unwrap())
+        .arg(data_dir)
+        .arg("--hatena-blog-data-file")
+        .arg(hatena_blog_data_file)
+        .env(
+            "BBN_TEST_CONFIG_DIR",
+            config_dir.as_os_str().to_str().unwrap(),
+        )
+        .assert()
+        .success();
+    Command::cargo_bin("bbn")
+        .unwrap()
+        .arg("view")
         .arg("2021-02-03")
+        .env(
+            "BBN_TEST_CONFIG_DIR",
+            config_dir.as_os_str().to_str().unwrap(),
+        )
         .assert()
         .success()
-        .stdout("2021-02-03 ID_TITLE TITLE1 https://blog.bouzuya.net/2021/02/03/\n");
+        .stdout("2021-02-03 ID_TITLE TITLE1 https://blog.bouzuya.net/2021/02/03/\nHello\n");
 }
