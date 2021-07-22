a7e9169bb5ab999604099141e1574e440350e891
bouzuya <m@bouzuya.net>
2021-07-22 22:38:29 +0900

    bbn: refactor to add HatenaBlogEntry

--- bbn/src/hatena_blog.rs
+++ bbn/src/hatena_blog.rs
@@ -1,5 +1,6 @@
 mod download_entry;
 mod hatena_blog_client;
+mod hatena_blog_entry;
 mod hatena_blog_entry_id;
 mod hatena_blog_repository;
 mod indexing;
@@ -8,6 +9,7 @@ mod upload_entry;
 
 pub use self::download_entry::*;
 pub use self::hatena_blog_client::*;
+pub use self::hatena_blog_entry::*;
 pub use self::hatena_blog_entry_id::*;
 pub use self::hatena_blog_repository::*;
 pub use self::indexing::*;
---
+++ bbn/src/hatena_blog/hatena_blog_entry.rs
@@ -0,0 +1,56 @@
+use hatena_blog::{Entry, EntryId};
+
+use crate::hatena_blog::HatenaBlogEntryId;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct HatenaBlogEntry {
+    pub author_name: String,
+    pub categories: Vec<String>,
+    pub content: String,
+    pub draft: bool,
+    pub edited: String,
+    pub id: HatenaBlogEntryId,
+    pub published: String,
+    pub title: String,
+    pub updated: String,
+}
+
+impl From<Entry> for HatenaBlogEntry {
+    fn from(entry: Entry) -> Self {
+        Self {
+            author_name: entry.author_name,
+            categories: entry.categories,
+            content: entry.content,
+            draft: entry.draft,
+            edited: entry.edited,
+            id: HatenaBlogEntryId::from(entry.id),
+            published: entry.published,
+            title: entry.title,
+            updated: entry.updated,
+        }
+    }
+}
+
+impl From<HatenaBlogEntry> for Entry {
+    fn from(hatena_blog_entry: HatenaBlogEntry) -> Self {
+        Self {
+            author_name: hatena_blog_entry.author_name,
+            categories: hatena_blog_entry.categories,
+            content: hatena_blog_entry.content,
+            draft: hatena_blog_entry.draft,
+            edited: hatena_blog_entry.edited,
+            id: EntryId::from(&hatena_blog_entry.id),
+            published: hatena_blog_entry.published,
+            title: hatena_blog_entry.title,
+            updated: hatena_blog_entry.updated,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn entry_conversion_test() {
+        // TODO:
+    }
+}
