12631e2c9152a1c95102f1f2b3a96de0d5879544
bouzuya <m@bouzuya.net>
2021-07-22 22:17:58 +0900

    bbn: refactor to add string conversion support for HatenaBlogEntryId

--- bbn/src/hatena_blog/hatena_blog_entry_id.rs
+++ bbn/src/hatena_blog/hatena_blog_entry_id.rs
@@ -3,6 +3,20 @@ use hatena_blog::EntryId;
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct HatenaBlogEntryId(EntryId);
 
+impl std::fmt::Display for HatenaBlogEntryId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::str::FromStr for HatenaBlogEntryId {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(EntryId::from_str(s)?))
+    }
+}
+
 impl From<EntryId> for HatenaBlogEntryId {
     fn from(entry_id: EntryId) -> Self {
         Self(entry_id)
@@ -30,5 +44,11 @@ mod tests {
             EntryId::from(&HatenaBlogEntryId::from(entry_id.clone())),
             entry_id
         );
+    }
+
+    #[test]
+    fn string_conversion_test() {
+        let s = "ABC";
+        assert_eq!(HatenaBlogEntryId::from_str(s).unwrap().to_string(), s);
     }
 }
