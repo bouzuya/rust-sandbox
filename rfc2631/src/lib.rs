7e0ba8cb3794e925898443df6e144ad6e56243db
bouzuya <m@bouzuya.net>
2021-09-18 08:20:34 +0900

    rfc2631: add SharedSecret::{from_bytes_be,to_bytes_be}

--- rfc2631/src/lib.rs
+++ rfc2631/src/lib.rs
@@ -56,6 +56,16 @@ impl PrivateKey {
 #[derive(Debug, Eq, PartialEq)]
 pub struct SharedSecret(BigUint);
 
+impl SharedSecret {
+    pub fn from_bytes_be(bytes: &[u8]) -> Self {
+        Self(BigUint::from_bytes_be(bytes))
+    }
+
+    pub fn to_bytes_be(&self) -> Vec<u8> {
+        self.0.to_bytes_be()
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct Group {
     g: Generator,
