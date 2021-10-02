0535bda9011fe2a27faa8dc524aa747c79d1573a
bouzuya <m@bouzuya.net>
2021-10-02 23:34:51 +0900

    b: impl FromStr for TagParam

--- b/crates/query/src/tag_param.rs
+++ b/crates/query/src/tag_param.rs
@@ -1,13 +1,24 @@
+use std::str::FromStr;
+
 use nom::{
     branch::alt,
     bytes::{
         complete::{tag, take_while1},
         streaming::take_while,
     },
-    combinator::map,
+    combinator::{all_consuming, map},
     sequence::delimited,
     IResult,
 };
+use thiserror::Error;
+
+// ParseTagParamError
+
+#[derive(Debug, Eq, Error, PartialEq)]
+#[error("parse tag param error")]
+pub struct ParseTagParamError;
+
+// TagParam
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct TagParam(String);
@@ -19,6 +30,16 @@ impl std::fmt::Display for TagParam {
         } else {
             write!(f, "tag:{}", self.0)
         }
+    }
+}
+
+impl FromStr for TagParam {
+    type Err = ParseTagParamError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        all_consuming(parse)(s)
+            .map(|(_, p)| p)
+            .map_err(|_| ParseTagParamError)
     }
 }
 
@@ -48,16 +69,10 @@ mod tests {
 
     #[test]
     fn str_conversion_test() -> anyhow::Result<()> {
-        {
-            let s = "tag:\"abc 123\"";
-            let (_, t) = parse(s)?;
-            assert_eq!(t.to_string(), s.to_string());
-        }
-        {
-            let s = "tag:abc";
-            let (_, t) = parse(s)?;
-            assert_eq!(t.to_string(), s.to_string());
-        }
+        let s = "tag:\"abc 123\"";
+        assert_eq!(TagParam::from_str(s)?.to_string(), s.to_string());
+        let s = "tag:abc";
+        assert_eq!(TagParam::from_str(s)?.to_string(), s.to_string());
         Ok(())
     }
 }
