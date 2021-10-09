e76dc63073065371f8e36570513f1af06591e6b5
bouzuya <m@bouzuya.net>
2021-10-09 15:38:47 +0900

    b: add Query::naive_date_time_range

--- b/crates/query/src/query.rs
+++ b/crates/query/src/query.rs
@@ -1,3 +1,6 @@
+use std::{cmp, str::FromStr};
+
+use chrono::{NaiveDate, NaiveDateTime};
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -19,6 +22,85 @@ pub struct ParseQueryError;
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Query(Vec<QueryParam>);
 
+impl Query {
+    pub fn naive_date_time_range(&self) -> (NaiveDateTime, NaiveDateTime) {
+        let mut min = NaiveDateTime::from_str("0000-01-01T00:00:00").unwrap();
+        let mut max = NaiveDateTime::from_str("9999-12-31T23:59:59").unwrap();
+        let empty = (max, min);
+        for query_param in self.clone().into_iter() {
+            match query_param {
+                QueryParam::Date(datea_param) => match datea_param {
+                    crate::DateParam::Single(date_param_single) => {
+                        let date_range = match (
+                            date_param_single.year(),
+                            date_param_single.month(),
+                            date_param_single.day_of_month(),
+                        ) {
+                            (None, None, None) => continue,
+                            (None, None, Some(_)) => unreachable!(),
+                            (None, Some(_), None) => unreachable!(),
+                            (None, Some(_), Some(_)) => unreachable!(),
+                            (Some(yyyy), None, None) => {
+                                let year = u16::from(yyyy) as i32;
+                                let mn = NaiveDate::from_ymd(year, 1, 1);
+                                let mx = NaiveDate::from_ymd(year, 12, 31);
+                                (mn, mx)
+                            }
+                            (Some(_), None, Some(_)) => unreachable!(),
+                            (Some(yyyy), Some(mm), None) => {
+                                let year = u16::from(yyyy) as i32;
+                                let month = u8::from(mm) as u32;
+                                let mn = NaiveDate::from_ymd(year, month, 1);
+                                let last_day_of_month = NaiveDate::from_ymd(
+                                    match month {
+                                        12 => year + 1,
+                                        _ => year,
+                                    },
+                                    match month {
+                                        12 => 1,
+                                        _ => month + 1,
+                                    },
+                                    1,
+                                )
+                                .signed_duration_since(mn)
+                                .num_days()
+                                    as u32;
+                                let mx = NaiveDate::from_ymd(year, month, last_day_of_month);
+                                (mn, mx)
+                            }
+                            (Some(yyyy), Some(mm), Some(dd)) => {
+                                let year = u16::from(yyyy) as i32;
+                                let month = u8::from(mm) as u32;
+                                let day_of_month = u8::from(dd) as u32;
+                                let mn = NaiveDate::from_ymd(year, month, day_of_month);
+                                let mx = mn;
+                                (mn, mx)
+                            }
+                        };
+                        let date_time_range = (
+                            date_range.0.and_hms(0, 0, 0),
+                            date_range.1.and_hms(23, 59, 59),
+                        );
+                        let (mn, mx) = date_time_range;
+                        if max < mn || mx < min {
+                            return empty;
+                        }
+                        min = cmp::max(min, mn);
+                        max = cmp::min(max, mx);
+                        if min > max {
+                            return empty;
+                        }
+                    }
+                    crate::DateParam::Range(_) => todo!(),
+                },
+                QueryParam::Tag(_) => continue,
+            }
+        }
+
+        (min, max)
+    }
+}
+
 impl std::fmt::Display for Query {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -31,19 +113,6 @@ impl std::fmt::Display for Query {
                 .join(" ")
         )
     }
-}
-
-fn parse(s: &str) -> IResult<&str, Query> {
-    map(
-        all_consuming(separated_list0(
-            tag(" "),
-            alt((
-                map(date_param, QueryParam::Date),
-                map(tag_param, QueryParam::Tag),
-            )),
-        )),
-        Query,
-    )(s)
 }
 
 impl std::str::FromStr for Query {
@@ -62,6 +131,19 @@ impl IntoIterator for Query {
     fn into_iter(self) -> Self::IntoIter {
         self.0.into_iter()
     }
+}
+
+fn parse(s: &str) -> IResult<&str, Query> {
+    map(
+        all_consuming(separated_list0(
+            tag(" "),
+            alt((
+                map(date_param, QueryParam::Date),
+                map(tag_param, QueryParam::Tag),
+            )),
+        )),
+        Query,
+    )(s)
 }
 
 #[cfg(test)]
@@ -102,6 +184,41 @@ mod tests {
     #[test]
     fn default_test() -> anyhow::Result<()> {
         assert_eq!(Query::default(), Query::from_str("")?);
+        Ok(())
+    }
+
+    #[test]
+    fn naive_date_time_range() -> anyhow::Result<()> {
+        assert_eq!(
+            Query::from_str("")?.naive_date_time_range(),
+            (
+                NaiveDateTime::from_str("0000-01-01T00:00:00")?,
+                NaiveDateTime::from_str("9999-12-31T23:59:59")?,
+            )
+        );
+        assert_eq!(
+            Query::from_str("date:2021")?.naive_date_time_range(),
+            (
+                NaiveDateTime::from_str("2021-01-01T00:00:00")?,
+                NaiveDateTime::from_str("2021-12-31T23:59:59")?,
+            )
+        );
+        assert_eq!(
+            Query::from_str("date:2021-02")?.naive_date_time_range(),
+            (
+                NaiveDateTime::from_str("2021-02-01T00:00:00")?,
+                NaiveDateTime::from_str("2021-02-28T23:59:59")?,
+            )
+        );
+        assert_eq!(
+            Query::from_str("date:2021-02-03")?.naive_date_time_range(),
+            (
+                NaiveDateTime::from_str("2021-02-03T00:00:00")?,
+                NaiveDateTime::from_str("2021-02-03T23:59:59")?,
+            )
+        );
+        // TODO: date:2021-02-03/2021-03-04
+
         Ok(())
     }
 }
