c4f4c3d5019f22dce9a20e0219336d312d185842
bouzuya <m@bouzuya.net>
2021-10-12 05:37:05 +0900

    date-time: rename day_of_month tests

--- date_time/src/local_date/day_of_month.rs
+++ date_time/src/local_date/day_of_month.rs
@@ -67,31 +67,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn str_convert() {
-        // str -(from_str / parse)-> DayOfMonth
-        // str <-(to_string & as_str)- DayOfMonth
-        type PDE = ParseDayOfMonthError;
+    fn str_conversion_test() {
+        type E = ParseDayOfMonthError;
         let f = |s: &str| s.parse::<DayOfMonth>();
         assert_eq!(f("01").map(|d| d.to_string()), Ok("01".to_string()));
         assert_eq!(f("31").map(|d| d.to_string()), Ok("31".to_string()));
-        assert_eq!(f(""), Err(PDE::InvalidLength));
-        assert_eq!(f("1"), Err(PDE::InvalidLength));
-        assert_eq!(f("100"), Err(PDE::InvalidLength));
-        assert_eq!(f("0a"), Err(PDE::InvalidDigit));
-        assert_eq!(f("+1"), Err(PDE::InvalidDigit));
-        assert_eq!(f("00"), Err(PDE::OutOfRange));
-        assert_eq!(f("32"), Err(PDE::OutOfRange));
+        assert_eq!(f(""), Err(E::InvalidLength));
+        assert_eq!(f("1"), Err(E::InvalidLength));
+        assert_eq!(f("100"), Err(E::InvalidLength));
+        assert_eq!(f("0a"), Err(E::InvalidDigit));
+        assert_eq!(f("+1"), Err(E::InvalidDigit));
+        assert_eq!(f("00"), Err(E::OutOfRange));
+        assert_eq!(f("32"), Err(E::OutOfRange));
     }
 
     #[test]
-    fn u8_convert() {
-        // u8 -(try_from)-> DayOfMonth
-        // u8 <-(from)- DayOfMonth
+    fn u8_conversion_test() {
         type E = TryFromDayOfMonthError;
         let f = |d: u8| DayOfMonth::try_from(d);
         assert_eq!(f(0_u8), Err(E::OutOfRange));
-        assert_eq!(f(1_u8).map(|d| u8::from(d)), Ok(1_u8));
-        assert_eq!(f(31_u8).map(|d| u8::from(d)), Ok(31_u8));
+        assert_eq!(f(1_u8).map(u8::from), Ok(1_u8));
+        assert_eq!(f(31_u8).map(u8::from), Ok(31_u8));
         assert_eq!(f(32_u8), Err(E::OutOfRange));
     }
 }
