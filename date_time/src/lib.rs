a0a008ca4ef1a21dd832930529a1312fecda2975
bouzuya <m@bouzuya.net>
2021-10-18 22:02:19 +0900

    date-time: LocalTime -> Time (file name)

--- date_time/src/lib.rs
+++ date_time/src/lib.rs
@@ -1,13 +1,13 @@
 mod date;
 mod instant;
 mod local_date_time;
-mod local_time;
 mod offset_date_time;
+mod time;
 mod time_zone_offset;
 
 pub use self::date::*;
 pub use self::instant::*;
 pub use self::local_date_time::*;
-pub use self::local_time::*;
 pub use self::offset_date_time::*;
+pub use self::time::*;
 pub use self::time_zone_offset::*;
