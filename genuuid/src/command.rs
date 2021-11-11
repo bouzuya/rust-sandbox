58ef317b9acb318270401889bdd1767db476e136
bouzuya <m@bouzuya.net>
2021-11-11 23:22:34 +0900

    genuuid: extract mod command

---
+++ genuuid/src/command.rs
@@ -0,0 +1,107 @@
+use axum::{
+    extract::{Extension, Query, RawQuery},
+    http::{header::LOCATION, HeaderMap, HeaderValue, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    AddExtensionLayer, Router, Server,
+};
+use std::{collections::HashMap, sync::Arc};
+use uuid::Uuid;
+
+fn generate_impl(count: Option<usize>) -> Vec<String> {
+    let mut generated = vec![];
+    let count = count.unwrap_or(1).clamp(1, 100);
+    for _ in 0..count {
+        let uuid = Uuid::new_v4();
+        generated.push(uuid.to_string());
+    }
+    generated
+}
+
+pub fn generate(count: Option<usize>) -> anyhow::Result<()> {
+    let generated = generate_impl(count);
+    let message = generated.join("\n");
+    print!("{}", message);
+    Ok(())
+}
+
+async fn handler_root(
+    Extension(state): Extension<Arc<State>>,
+    RawQuery(query): RawQuery,
+) -> impl IntoResponse {
+    let location = format!(
+        "{}/uuids.txt{}",
+        state.base_path,
+        query.map(|q| format!("?{}", q)).unwrap_or_default()
+    );
+    let mut header_map = HeaderMap::new();
+    header_map.append(
+        LOCATION,
+        HeaderValue::from_str(&location).expect("state contains not ascii"),
+    );
+    (StatusCode::SEE_OTHER, header_map, ())
+}
+
+async fn handler_uuids(Query(params): Query<HashMap<String, String>>) -> impl IntoResponse {
+    let count = params
+        .get("count")
+        .and_then(|s| s.parse::<usize>().ok())
+        .map(|count| count.clamp(1, 100));
+    generate_impl(count).join("\n")
+}
+
+struct State {
+    base_path: String,
+}
+
+pub async fn server() -> anyhow::Result<()> {
+    let base_path = std::env::var("BASE_PATH").unwrap_or_else(|_| "".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let host = "0.0.0.0";
+    let addr = format!("{}:{}", host, port).parse()?;
+
+    let state = State {
+        base_path: base_path.clone(),
+    };
+    let router = Router::new()
+        .route("/", get(handler_root))
+        .route("/uuids.txt", get(handler_uuids));
+    let wrapped_router = if base_path.is_empty() {
+        router
+    } else {
+        Router::new()
+            .route("/", get(handler_root))
+            .nest(base_path.as_str(), router)
+    }
+    .layer(AddExtensionLayer::new(Arc::new(state)));
+
+    Ok(Server::bind(&addr)
+        .serve(wrapped_router.into_make_service())
+        .await?)
+}
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn generate_impl_test() {
+        // count
+        assert_eq!(generate_impl(None).len(), 1);
+        assert_eq!(generate_impl(Some(0)).len(), 1);
+        assert_eq!(generate_impl(Some(1)).len(), 1);
+        assert_eq!(generate_impl(Some(2)).len(), 2);
+        assert_eq!(generate_impl(Some(100)).len(), 100);
+        assert_eq!(generate_impl(Some(101)).len(), 100);
+
+        // uniqueness
+        assert_eq!(
+            generate_impl(Some(100))
+                .into_iter()
+                .collect::<HashSet<_>>()
+                .len(),
+            100
+        );
+    }
+}
--- genuuid/src/main.rs
+++ genuuid/src/main.rs
@@ -1,23 +1,8 @@
-use axum::{
-    extract::{Extension, Query, RawQuery},
-    http::{header::LOCATION, HeaderMap, HeaderValue, StatusCode},
-    response::IntoResponse,
-    routing::get,
-    AddExtensionLayer, Router, Server,
-};
-use std::{collections::HashMap, io, sync::Arc};
-use structopt::{clap::Shell, StructOpt};
-use uuid::Uuid;
+mod command;
 
-fn generate(count: Option<usize>) -> Vec<String> {
-    let mut generated = vec![];
-    let count = count.unwrap_or(1).clamp(1, 100);
-    for _ in 0..count {
-        let uuid = Uuid::new_v4();
-        generated.push(uuid.to_string());
-    }
-    generated
-}
+use std::io;
+
+use structopt::{clap::Shell, StructOpt};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -41,61 +26,6 @@ enum Subcommand {
     Server,
 }
 
-async fn handler_root(
-    Extension(state): Extension<Arc<State>>,
-    RawQuery(query): RawQuery,
-) -> impl IntoResponse {
-    let location = format!(
-        "{}/uuids.txt{}",
-        state.base_path,
-        query.map(|q| format!("?{}", q)).unwrap_or_default()
-    );
-    let mut header_map = HeaderMap::new();
-    header_map.append(
-        LOCATION,
-        HeaderValue::from_str(&location).expect("state contains not ascii"),
-    );
-    (StatusCode::SEE_OTHER, header_map, ())
-}
-
-async fn handler_uuids(Query(params): Query<HashMap<String, String>>) -> impl IntoResponse {
-    let count = params
-        .get("count")
-        .and_then(|s| s.parse::<usize>().ok())
-        .map(|count| count.clamp(1, 100));
-    generate(count).join("\n")
-}
-
-struct State {
-    base_path: String,
-}
-
-async fn server() -> anyhow::Result<()> {
-    let base_path = std::env::var("BASE_PATH").unwrap_or_else(|_| "".to_string());
-    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
-    let host = "0.0.0.0";
-    let addr = format!("{}:{}", host, port).parse()?;
-
-    let state = State {
-        base_path: base_path.clone(),
-    };
-    let router = Router::new()
-        .route("/", get(handler_root))
-        .route("/uuids.txt", get(handler_uuids));
-    let wrapped_router = if base_path.is_empty() {
-        router
-    } else {
-        Router::new()
-            .route("/", get(handler_root))
-            .nest(base_path.as_str(), router)
-    }
-    .layer(AddExtensionLayer::new(Arc::new(state)));
-
-    Ok(Server::bind(&addr)
-        .serve(wrapped_router.into_make_service())
-        .await?)
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let opt = Opt::from_args();
@@ -107,39 +37,7 @@ async fn main() -> anyhow::Result<()> {
             Opt::clap().gen_completions_to("genuuid", shell, &mut io::stdout());
             Ok(())
         }
-        Subcommand::Generate { count } => {
-            let generated = generate(count);
-            let message = generated.join("\n");
-            print!("{}", message);
-            Ok(())
-        }
-        Subcommand::Server => server().await,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashSet;
-
-    use super::*;
-
-    #[test]
-    fn generate_test() {
-        // count
-        assert_eq!(generate(None).len(), 1);
-        assert_eq!(generate(Some(0)).len(), 1);
-        assert_eq!(generate(Some(1)).len(), 1);
-        assert_eq!(generate(Some(2)).len(), 2);
-        assert_eq!(generate(Some(100)).len(), 100);
-        assert_eq!(generate(Some(101)).len(), 100);
-
-        // uniqueness
-        assert_eq!(
-            generate(Some(100))
-                .into_iter()
-                .collect::<HashSet<_>>()
-                .len(),
-            100
-        );
+        Subcommand::Generate { count } => command::generate(count),
+        Subcommand::Server => command::server().await,
     }
 }
