22893f78e87728fe50d9e6fbfb0ac61a700739d0
bouzuya <m@bouzuya.net>
2021-12-25 22:53:37 +0900

    its: Added IssueId

--- its/src/main.rs
+++ its/src/main.rs
@@ -1,13 +1,26 @@
+use ulid::Ulid;
+
+#[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
+struct IssueId(Ulid);
+
+impl IssueId {
+    pub fn new(id: Ulid) -> Self {
+        Self(id)
+    }
+}
+
 #[derive(Debug)]
-struct Issue {}
+struct Issue {
+    id: IssueId,
+}
 
 impl Issue {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(id: IssueId) -> Self {
+        Self { id }
     }
 }
 
 fn main() {
-    let issue = Issue::new();
+    let issue = Issue::new(IssueId::new(Ulid::new()));
     println!("issue created : {:?}", issue);
 }
