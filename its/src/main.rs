2518529aef35c1d762f6d5be66a08ab5f8c47ab3
bouzuya <m@bouzuya.net>
2022-01-03 23:34:47 +0900

    its: Added --title option to issue-create subcommand

--- its/src/main.rs
+++ its/src/main.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use crate::{
     entity::IssueTitle,
     workflow::{create_issue_workflow, CreateIssue},
@@ -8,11 +6,13 @@ use crate::{
 mod entity;
 mod workflow;
 
-fn main() {
-    let command = CreateIssue::new(
-        // TODO: use option
-        IssueTitle::from_str("title").unwrap(),
-    );
+#[argopt::subcmd(name = "issue-create")]
+fn issue_create(#[opt(long = "title")] title: Option<String>) {
+    let title = IssueTitle::try_from(title.unwrap_or_default()).unwrap();
+    let command = CreateIssue::new(title);
     let event = create_issue_workflow(command);
     println!("issue created : {:?}", event);
 }
+
+#[argopt::cmd_group(commands = [issue_create])]
+fn main() {}
--- its/tests/its.rs
+++ its/tests/its.rs
@@ -1,11 +1,31 @@
 use assert_cmd::Command;
 
 #[test]
+fn its_no_args() -> anyhow::Result<()> {
+    Command::cargo_bin("its")?
+        .assert()
+        .stderr(predicates::str::contains("USAGE:"))
+        .failure();
+    Ok(())
+}
+
+#[test]
 fn its_issue_create() -> anyhow::Result<()> {
     Command::cargo_bin("its")?
         .arg("issue-create")
         .assert()
         .stdout(predicates::str::contains("issue created"))
+        .success();
+    Ok(())
+}
+
+#[test]
+fn its_issue_create_with_title() -> anyhow::Result<()> {
+    Command::cargo_bin("its")?
+        .args(&["issue-create", "--title", "title1"])
+        .assert()
+        .stdout(predicates::str::contains("issue created"))
+        .stdout(predicates::str::contains("title1"))
         .success();
     Ok(())
 }
