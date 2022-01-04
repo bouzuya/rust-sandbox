d4ae6d04c0a3b0458590183af37dc62dada7cb0e
bouzuya <m@bouzuya.net>
2022-01-04 22:56:58 +0900

    its: Added Issue::status

--- its/src/entity.rs
+++ its/src/entity.rs
@@ -5,6 +5,7 @@ mod issue_title;
 use ulid::Ulid;
 
 pub use self::issue_number::IssueNumber;
+use self::issue_status::IssueStatus;
 pub use self::issue_title::IssueTitle;
 
 #[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
@@ -20,16 +21,26 @@ impl IssueId {
 pub struct Issue {
     id: IssueId,
     number: IssueNumber,
+    status: IssueStatus,
     title: IssueTitle,
 }
 
 impl Issue {
     pub fn new(id: IssueId, number: IssueNumber, title: IssueTitle) -> Self {
-        Self { id, number, title }
+        Self {
+            id,
+            number,
+            status: IssueStatus::Todo,
+            title,
+        }
     }
 
     pub fn number(&self) -> IssueNumber {
         self.number
+    }
+
+    pub fn status(&self) -> IssueStatus {
+        self.status
     }
 
     pub fn title(&self) -> &IssueTitle {
@@ -50,6 +61,7 @@ mod tests {
         let title = IssueTitle::from_str("title1")?;
         let issue = Issue::new(IssueId::new(ulid), number, title.clone());
         assert_eq!(issue.number(), number);
+        assert_eq!(issue.status(), IssueStatus::Todo);
         assert_eq!(issue.title(), &title);
         Ok(())
     }
