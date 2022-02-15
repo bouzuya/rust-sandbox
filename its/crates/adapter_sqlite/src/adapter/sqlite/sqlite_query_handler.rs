9bded88c0901e7e56dd9b11ef7f7bb26fb8a0990
bouzuya <m@bouzuya.net>
2022-02-15 22:57:10 +0900

    its: Fixed SqliteQueryHandler error handling

--- its/crates/adapter_sqlite/src/adapter/sqlite/sqlite_query_handler.rs
+++ its/crates/adapter_sqlite/src/adapter/sqlite/sqlite_query_handler.rs
@@ -3,20 +3,20 @@ use std::{fs, path::Path, str::FromStr};
 use domain::aggregate::IssueAggregate;
 use sqlx::{
     any::{AnyArguments, AnyConnectOptions},
-    query::{Query, QueryAs},
+    query::Query,
     sqlite::{SqliteConnectOptions, SqliteJournalMode},
-    Any, AnyPool, Executor, FromRow,
+    Any, AnyPool, FromRow,
 };
 use thiserror::Error;
 
 // QueryIssue
 
 #[derive(Clone, Debug, FromRow)]
-struct QueryIssue {
-    id: String,
-    status: String,
-    title: String,
-    due: Option<String>,
+pub struct QueryIssue {
+    pub id: String,
+    pub status: String,
+    pub title: String,
+    pub due: Option<String>,
 }
 
 // QueryHandlerError
@@ -27,9 +27,15 @@ pub enum QueryHandlerError {
     Unknown(String),
 }
 
+impl From<sqlx::Error> for QueryHandlerError {
+    fn from(e: sqlx::Error) -> Self {
+        Self::Unknown(e.to_string())
+    }
+}
+
 // SqliteQueryHandler
 
-struct SqliteQueryHandler {
+pub struct SqliteQueryHandler {
     pool: AnyPool,
 }
 
@@ -44,66 +50,39 @@ impl SqliteQueryHandler {
                 .join("query.sqlite")
                 .to_str()
                 .ok_or_else(|| QueryHandlerError::Unknown("data_dir is not UTF-8".to_string()))?
-        ))
-        .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?
+        ))?
         .journal_mode(SqliteJournalMode::Delete);
         let options = AnyConnectOptions::from(options);
-        let pool = AnyPool::connect_with(options)
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+        let pool = AnyPool::connect_with(options).await?;
 
-        let mut transaction = pool
-            .begin()
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+        let mut transaction = pool.begin().await?;
         sqlx::query(include_str!("../../../sql/create_issues.sql"))
             .execute(&mut *transaction)
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
-        transaction
-            .commit()
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+            .await?;
+        transaction.commit().await?;
 
         Ok(Self { pool })
     }
 
-    pub async fn save_issue(&self, issue: IssueAggregate) -> anyhow::Result<()> {
-        let mut transaction = self
-            .pool
-            .begin()
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+    pub async fn save_issue(&self, issue: IssueAggregate) -> Result<(), QueryHandlerError> {
+        let mut transaction = self.pool.begin().await?;
         let query: Query<Any, AnyArguments> =
             sqlx::query(include_str!("../../../sql/query/delete_issue.sql"))
                 .bind(issue.issue().id().to_string());
-        query
-            .execute(&mut transaction)
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+        query.execute(&mut transaction).await?;
         let query: Query<Any, AnyArguments> =
             sqlx::query(include_str!("../../../sql/query/insert_issue.sql"))
                 .bind(issue.issue().id().to_string())
                 .bind(issue.issue().status().to_string())
                 .bind(issue.issue().title().to_string())
                 .bind(issue.issue().due().map(|d| d.to_string()));
-        query
-            .execute(&mut transaction)
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
-        transaction
-            .commit()
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+        query.execute(&mut transaction).await?;
+        transaction.commit().await?;
         Ok(())
     }
 
-    pub async fn issue_list(&self) -> anyhow::Result<Vec<QueryIssue>> {
-        let mut transaction = self
-            .pool
-            .begin()
-            .await
-            .map_err(|e| QueryHandlerError::Unknown(e.to_string()))?;
+    pub async fn issue_list(&self) -> Result<Vec<QueryIssue>, QueryHandlerError> {
+        let mut transaction = self.pool.begin().await?;
         let issues: Vec<QueryIssue> =
             sqlx::query_as(include_str!("../../../sql/query/select_issues.sql"))
                 .fetch_all(&mut transaction)
