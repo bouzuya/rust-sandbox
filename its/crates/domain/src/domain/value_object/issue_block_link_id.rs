acc7b293cc59e434be4e11de84fb4babf4d9593d
bouzuya <m@bouzuya.net>
2022-02-22 06:24:50 +0900

    its: impl FromStr for IssueBlockLinkId

--- its/crates/domain/src/domain/value_object/issue_block_link_id.rs
+++ its/crates/domain/src/domain/value_object/issue_block_link_id.rs
@@ -1,11 +1,53 @@
-use crate::IssueId;
+use std::{fmt::Display, str::FromStr};
 
-#[derive(Clone, Debug)]
+use thiserror::Error;
+
+use crate::{IssueId, ParseIssueIdError};
+
+#[derive(Debug, Error)]
+pub enum ParseIssueBlockLinkError {
+    #[error("InvalidFormat: {0}")]
+    InvalidFormat(String),
+    #[error("InvalidIssueId: {0}")]
+    InvalidIssueId(#[from] ParseIssueIdError),
+    #[error("InvalidBlockedId: {0}")]
+    InvalidBlockedIssueId(IssueId),
+}
+
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct IssueBlockLinkId(IssueId, IssueId);
 
+impl Display for IssueBlockLinkId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} -> {}", self.0, self.1)
+    }
+}
+
+impl FromStr for IssueBlockLinkId {
+    type Err = ParseIssueBlockLinkError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let splitted = s.split(" -> ").collect::<Vec<&str>>();
+        if splitted.len() != 2 {
+            return Err(Self::Err::InvalidFormat(s.to_string()));
+        }
+        let issue_id = IssueId::from_str(splitted[0])?;
+        let blocked_issue_id = IssueId::from_str(splitted[1])?;
+        Self::new(issue_id, blocked_issue_id)
+    }
+}
+
 impl IssueBlockLinkId {
-    pub fn new(issue_id1: IssueId, issue_id2: IssueId) -> Self {
-        Self(issue_id1, issue_id2)
+    pub fn new(
+        issue_id: IssueId,
+        blocked_issue_id: IssueId,
+    ) -> Result<Self, ParseIssueBlockLinkError> {
+        if issue_id == blocked_issue_id {
+            return Err(ParseIssueBlockLinkError::InvalidBlockedIssueId(
+                blocked_issue_id,
+            ));
+        }
+        Ok(Self(issue_id, blocked_issue_id))
     }
 }
 
@@ -17,7 +59,16 @@ mod tests {
     fn test() -> anyhow::Result<()> {
         let issue_id1 = "1".parse()?;
         let issue_id2 = "2".parse()?;
-        let _ = IssueBlockLinkId::new(issue_id1, issue_id2);
+        let issue_block_link = IssueBlockLinkId::new(issue_id1, issue_id2)?;
+        assert_eq!(issue_block_link.to_string(), "1 -> 2");
+        let parsed = IssueBlockLinkId::from_str("1 -> 2")?;
+        assert_eq!(issue_block_link, parsed);
+
+        assert!(IssueBlockLinkId::from_str("1 -> 1").is_err());
+        assert!(IssueBlockLinkId::from_str("1 -- 1").is_err());
+        assert!(IssueBlockLinkId::from_str("a -> 1").is_err());
+        assert!(IssueBlockLinkId::from_str("1 -> a").is_err());
+        assert!(IssueBlockLinkId::from_str("1 -> 1 -> 1").is_err());
         Ok(())
     }
 }
