9bf5e8a466a4e63c509e73f6362b8b6a112988dc
bouzuya <m@bouzuya.net>
2022-02-23 06:57:14 +0900

    its: Updated IssueBlockLink aggregate

--- its/crates/domain/src/domain/aggregate/issue_block_link.rs
+++ its/crates/domain/src/domain/aggregate/issue_block_link.rs
@@ -1,25 +1,59 @@
-use crate::{IssueId, Version};
+use thiserror::Error;
+
+use crate::{domain::entity::IssueBlockLink, IssueBlockLinkId, IssueId, Version};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct IssueBlockLinkAggregate {
-    issue_id: IssueId,
-    blocked_issue_id: IssueId,
-    blocked: bool,
+    issue_block_link: IssueBlockLink,
     version: Version,
 }
 
+#[derive(Clone, Debug, Error)]
+enum IssueBlockLinkAggregateError {
+    #[error("Block")]
+    Block,
+    #[error("NextVersion")]
+    NoNextVersion,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum IssueBlockLinkAggregateEvent {
+    // TODO: Id
+    Blocked,
+    // TODO: Id
+    Unblocked,
+}
+
+type IssueBlockLinkAggregateResult =
+    Result<(IssueBlockLinkAggregate, IssueBlockLinkAggregateEvent), IssueBlockLinkAggregateError>;
+
 impl IssueBlockLinkAggregate {
-    pub(crate) fn block(issue_id: IssueId, blocked_issue_id: IssueId) -> Self {
-        Self {
-            issue_id,
-            blocked_issue_id,
-            blocked: true,
-            version: Version::from(1_u64),
-        }
+    pub fn block(issue_id: IssueId, blocked_issue_id: IssueId) -> IssueBlockLinkAggregateResult {
+        let id = IssueBlockLinkId::new(issue_id, blocked_issue_id)
+            .map_err(|_| IssueBlockLinkAggregateError::Block)?;
+        let issue_block_link = IssueBlockLink::new(id);
+        Ok((
+            Self {
+                issue_block_link,
+                version: Version::from(1_u64),
+            },
+            IssueBlockLinkAggregateEvent::Blocked,
+        ))
     }
 
-    pub(crate) fn unblock(&mut self) {
+    pub fn unblock(&self) -> IssueBlockLinkAggregateResult {
         // TODO: check blocked
-        self.blocked = false;
+        let updated_issue_block_link = self.issue_block_link.unblock();
+        let updated_version = self
+            .version
+            .next()
+            .ok_or(IssueBlockLinkAggregateError::NoNextVersion)?;
+        Ok((
+            Self {
+                issue_block_link: updated_issue_block_link,
+                version: updated_version,
+            },
+            IssueBlockLinkAggregateEvent::Unblocked,
+        ))
     }
 }
