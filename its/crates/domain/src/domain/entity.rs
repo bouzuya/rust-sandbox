bc9b1da463d45b649123bcf34f14e3e82bbb75dd
bouzuya <m@bouzuya.net>
2022-02-23 06:56:44 +0900

    its: Updated IssueBlockLink entity

--- its/crates/domain/src/domain/entity.rs
+++ its/crates/domain/src/domain/entity.rs
@@ -2,4 +2,4 @@ mod issue;
 mod issue_block_link;
 
 pub use self::issue::*;
-pub use self::issue_block_link::*;
+pub(crate) use self::issue_block_link::*;
--- its/crates/domain/src/domain/entity/issue_block_link.rs
+++ its/crates/domain/src/domain/entity/issue_block_link.rs
@@ -1,7 +1,7 @@
 use crate::{IssueBlockLinkId, IssueBlockLinkStatus, IssueId};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct IssueBlockLink {
+pub(crate) struct IssueBlockLink {
     id: IssueBlockLinkId,
     status: IssueBlockLinkStatus,
 }
@@ -14,24 +14,30 @@ impl IssueBlockLink {
         }
     }
 
-    pub fn issue_id(&self) -> &IssueId {
+    pub(crate) fn issue_id(&self) -> &IssueId {
         self.id.issue_id()
     }
 
-    pub fn blocked_issue_id(&self) -> &IssueId {
+    pub(crate) fn blocked_issue_id(&self) -> &IssueId {
         self.id.blocked_issue_id()
     }
 
-    pub fn is_blocked(&self) -> bool {
+    pub(crate) fn is_blocked(&self) -> bool {
         self.status == IssueBlockLinkStatus::Blocked
     }
 
-    pub(crate) fn block(&mut self) {
-        self.status = IssueBlockLinkStatus::Blocked;
+    pub(crate) fn block(&self) -> Self {
+        Self {
+            id: self.id.clone(),
+            status: IssueBlockLinkStatus::Blocked,
+        }
     }
 
-    pub(crate) fn unblock(&mut self) {
-        self.status = IssueBlockLinkStatus::Unblocked;
+    pub(crate) fn unblock(&self) -> Self {
+        Self {
+            id: self.id.clone(),
+            status: IssueBlockLinkStatus::Unblocked,
+        }
     }
 }
 
@@ -44,16 +50,16 @@ mod tests {
     #[test]
     fn test() -> anyhow::Result<()> {
         let id = IssueBlockLinkId::from_str("1 -> 2")?;
-        let mut link = IssueBlockLink::new(id.clone());
+        let link = IssueBlockLink::new(id.clone());
         assert_eq!(link.issue_id(), id.issue_id());
         assert_eq!(link.blocked_issue_id(), id.blocked_issue_id());
         assert!(link.is_blocked());
 
-        link.unblock();
-        assert!(!link.is_blocked());
+        let unblocked = link.unblock();
+        assert!(!unblocked.is_blocked());
 
-        link.block();
-        assert!(link.is_blocked());
+        let blocked = unblocked.block();
+        assert!(blocked.is_blocked());
 
         Ok(())
     }
