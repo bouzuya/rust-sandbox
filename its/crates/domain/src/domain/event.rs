b32b24750c2aaeb19017aa02cec694b2d9e5b7be
bouzuya <m@bouzuya.net>
2022-02-27 07:22:14 +0900

    its: Added DomainEvent

--- its/crates/domain/src/domain/event.rs
+++ its/crates/domain/src/domain/event.rs
@@ -4,8 +4,17 @@ mod issue_created_v2;
 mod issue_finished;
 mod issue_updated;
 
+use crate::aggregate::IssueAggregateEvent;
+use crate::aggregate::IssueBlockLinkAggregateEvent;
+
 pub use self::issue_blocked::*;
 pub use self::issue_created::*;
 pub use self::issue_created_v2::*;
 pub use self::issue_finished::*;
 pub use self::issue_updated::*;
+
+#[derive(Debug)]
+pub enum DomainEvent {
+    Issue(IssueAggregateEvent),
+    IssueBlockLink(IssueBlockLinkAggregateEvent),
+}
