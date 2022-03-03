d76c44c42c753cd2c161e6007749afda4f544961
bouzuya <m@bouzuya.net>
2022-03-04 06:50:13 +0900

    its: Added IssueBlockLinkIdRow

--- its/crates/adapter_sqlite/src/adapter/sqlite/sqlite_issue_block_link_repository.rs
+++ its/crates/adapter_sqlite/src/adapter/sqlite/sqlite_issue_block_link_repository.rs
@@ -6,14 +6,43 @@ use std::{
 
 use async_trait::async_trait;
 use domain::{
-    aggregate::{IssueBlockLinkAggregate, IssueBlockLinkAggregateEvent},
+    aggregate::{
+        IssueBlockLinkAggregate, IssueBlockLinkAggregateError, IssueBlockLinkAggregateEvent,
+    },
     IssueBlockLinkId,
 };
 
-use sqlx::AnyPool;
+use sqlx::{any::AnyRow, AnyPool, FromRow, Row};
 use use_case::{IssueBlockLinkRepository, IssueBlockLinkRepositoryError};
 
 use crate::SqliteConnectionPool;
+
+use super::event_store::AggregateId;
+
+#[derive(Debug)]
+struct IssueBlockLinkIdRow {
+    issue_block_link_id: String,
+    aggregate_id: String,
+}
+
+impl IssueBlockLinkIdRow {
+    fn issue_block_link_id(&self) -> IssueBlockLinkId {
+        IssueBlockLinkId::from_str(self.issue_block_link_id.as_str()).unwrap()
+    }
+
+    fn aggregate_id(&self) -> AggregateId {
+        AggregateId::from_str(self.aggregate_id.as_str()).unwrap()
+    }
+}
+
+impl<'r> FromRow<'r, AnyRow> for IssueBlockLinkIdRow {
+    fn from_row(row: &'r AnyRow) -> Result<Self, sqlx::Error> {
+        Ok(Self {
+            issue_block_link_id: row.get("issue_block_link_id"),
+            aggregate_id: row.get("aggregate_id"),
+        })
+    }
+}
 
 #[derive(Debug)]
 pub struct SqliteIssueBlockLinkRepository {
