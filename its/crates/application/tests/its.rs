2b18db486c6d3a357c888b74b0505a299e74ea60
bouzuya <m@bouzuya.net>
2022-03-14 06:58:33 +0900

    its: Fixed to allow parallel execution of tests

--- its/crates/application/tests/its.rs
+++ its/crates/application/tests/its.rs
@@ -5,9 +5,8 @@ use assert_cmd::Command;
 #[test]
 fn its_no_args() -> anyhow::Result<()> {
     let temp_dir = tempfile::tempdir()?;
-    env::set_var("XDG_STATE_HOME", temp_dir.path().as_os_str());
-
     Command::cargo_bin("its")?
+        .env("XDG_STATE_HOME", temp_dir.path().as_os_str())
         .assert()
         .stderr(predicates::str::contains("USAGE:"))
         .failure();
@@ -17,9 +16,8 @@ fn its_no_args() -> anyhow::Result<()> {
 #[test]
 fn its_issue_create() -> anyhow::Result<()> {
     let temp_dir = tempfile::tempdir()?;
-    env::set_var("XDG_STATE_HOME", temp_dir.path().as_os_str());
-
     Command::cargo_bin("its")?
+        .env("XDG_STATE_HOME", temp_dir.path().as_os_str())
         .arg("issue-create")
         .assert()
         .stdout(predicates::str::contains("issue created"))
@@ -30,10 +28,9 @@ fn its_issue_create() -> anyhow::Result<()> {
 #[test]
 fn its_issue_create_with_title() -> anyhow::Result<()> {
     let temp_dir = tempfile::tempdir()?;
-    env::set_var("XDG_STATE_HOME", temp_dir.path().as_os_str());
-
     Command::cargo_bin("its")?
         .args(&["issue-create", "--title", "title1"])
+        .env("XDG_STATE_HOME", temp_dir.path().as_os_str())
         .assert()
         .stdout(predicates::str::contains("issue created"))
         .stdout(predicates::str::contains("title1"))
