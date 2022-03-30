24ebd95b3314cc870219e2da8df339c1b8e13c25
bouzuya <m@bouzuya.net>
2022-03-30 21:42:37 +0900

    its: Added --command-database-connection-uri option

--- its/crates/application/src/bin/its.rs
+++ its/crates/application/src/bin/its.rs
@@ -24,7 +24,7 @@ struct App {
 }
 
 impl App {
-    async fn new() -> anyhow::Result<Self> {
+    async fn new(command_database_connection_uri: Option<String>) -> anyhow::Result<Self> {
         let data_dir = Self::state_dir()?;
         if !data_dir.exists() {
             fs::create_dir_all(data_dir.as_path())?;
@@ -35,7 +35,10 @@ impl App {
                 path.to_str().context("path is not utf-8")?
             ))
         };
-        let command_connection_uri = new_connection_uri(data_dir.join("command.sqlite"))?;
+        let command_connection_uri = match command_database_connection_uri {
+            Some(s) => s,
+            None => new_connection_uri(data_dir.join("command.sqlite"))?,
+        };
         let query_connection_uri = new_connection_uri(data_dir.join("query.sqlite"))?;
         let connection_pool = SqliteConnectionPool::new(&command_connection_uri).await?;
         let issue_block_link_repository =
@@ -113,12 +116,13 @@ impl HasIssueManagementContextUseCase for App {
 fn issue_block(
     #[opt(name = "issue-id")] issue_id: String,
     #[opt(name = "blocked-issue-id")] blocked_issue_id: String,
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
 ) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
             let use_case = app.issue_management_context_use_case();
             let issue_id = IssueId::from_str(issue_id.as_str())?;
             let blocked_issue_id = IssueId::from_str(blocked_issue_id.as_str())?;
@@ -135,12 +139,13 @@ fn issue_block(
 fn issue_create(
     #[opt(long = "title")] title: Option<String>,
     #[opt(long = "due")] due: Option<String>,
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
 ) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
             let use_case = app.issue_management_context_use_case();
             let issue_title = IssueTitle::try_from(title.unwrap_or_default())?;
             let issue_due = due.map(|s| IssueDue::from_str(s.as_str())).transpose()?;
@@ -154,12 +159,15 @@ fn issue_create(
 }
 
 #[argopt::subcmd(name = "issue-finish")]
-fn issue_finish(issue_id: String) -> anyhow::Result<()> {
+fn issue_finish(
+    issue_id: String,
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
+) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
             let use_case = app.issue_management_context_use_case();
             let issue_id = IssueId::from_str(issue_id.as_str())?;
             let command = use_case.finish_issue(issue_id).into();
@@ -172,12 +180,14 @@ fn issue_finish(issue_id: String) -> anyhow::Result<()> {
 }
 
 #[argopt::subcmd(name = "issue-list")]
-fn issue_list() -> anyhow::Result<()> {
+fn issue_list(
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
+) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
 
             let issues = app.query_handler.issue_list().await?;
             println!("{}", serde_json::to_string(&issues)?);
@@ -189,12 +199,13 @@ fn issue_list() -> anyhow::Result<()> {
 fn issue_unblock(
     #[opt(name = "issue-id")] issue_id: String,
     #[opt(name = "blocked-issue-id")] blocked_issue_id: String,
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
 ) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
             let use_case = app.issue_management_context_use_case();
             let issue_id = IssueId::from_str(issue_id.as_str())?;
             let blocked_issue_id = IssueId::from_str(blocked_issue_id.as_str())?;
@@ -209,12 +220,16 @@ fn issue_unblock(
 }
 
 #[argopt::subcmd(name = "issue-update")]
-fn issue_update(issue_id: String, #[opt(long = "due")] due: Option<String>) -> anyhow::Result<()> {
+fn issue_update(
+    issue_id: String,
+    #[opt(long = "due")] due: Option<String>,
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
+) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
             let use_case = app.issue_management_context_use_case();
             let issue_id = IssueId::from_str(issue_id.as_str())?;
             let issue_due = due.map(|s| IssueDue::from_str(s.as_str())).transpose()?;
@@ -231,12 +246,15 @@ fn issue_update(issue_id: String, #[opt(long = "due")] due: Option<String>) -> a
 }
 
 #[argopt::subcmd(name = "issue-view")]
-fn issue_view(issue_id: String) -> anyhow::Result<()> {
+fn issue_view(
+    issue_id: String,
+    #[opt(long, name = "uri")] command_database_connection_uri: Option<String>,
+) -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
         .block_on(async {
-            let app = App::new().await?;
+            let app = App::new(command_database_connection_uri).await?;
             let issue_id = IssueId::from_str(issue_id.as_str())?;
 
             let issue = app.query_handler.issue_view(&issue_id).await?;
