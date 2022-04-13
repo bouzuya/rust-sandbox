1fb9a00cf0577c5ecc4ecef981278506f77fbfaf
bouzuya <m@bouzuya.net>
2022-04-14 06:56:14 +0900

    its: Added impl FromStr for IssueResolution

--- its/crates/domain/src/domain/value_object/issue_resolution.rs
+++ its/crates/domain/src/domain/value_object/issue_resolution.rs
@@ -1,2 +1,35 @@
+use thiserror::Error;
+
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct IssueResolution(String);
+pub struct IssueResolution(String);
+
+#[derive(Debug, Eq, Error, PartialEq)]
+#[error("ParseIssueResolutionError")]
+pub struct ParseIssueResolutionError {}
+
+impl std::fmt::Display for IssueResolution {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::str::FromStr for IssueResolution {
+    type Err = ParseIssueResolutionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn string_conversion_test() -> anyhow::Result<()> {
+        assert_eq!(IssueResolution::from_str("a")?.to_string(), "a");
+        Ok(())
+    }
+}
