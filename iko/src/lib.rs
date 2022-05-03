e53f82b4fea0cfff15c088c4ea51762ddf01f1b8
bouzuya <m@bouzuya.net>
2022-05-03 22:51:09 +0900

    iko: Extract migration_status_row

--- iko/src/lib.rs
+++ iko/src/lib.rs
@@ -1,4 +1,5 @@
 mod migration_status;
+mod migration_status_row;
 mod migration_status_value;
 mod migrator;
 mod version;
--- iko/src/migrator.rs
+++ iko/src/migrator.rs
@@ -1,71 +1,14 @@
-use std::str::FromStr;
-
 use async_trait::async_trait;
-use sqlx::{
-    any::{AnyArguments, AnyRow},
-    query::Query,
-    Any, AnyPool, FromRow, Row,
-};
+use sqlx::{any::AnyArguments, query::Query, Any, AnyPool};
 
 use crate::{
-    migration_status::MigrationStatus, migration_status_value::MigrationStatusValue,
-    version::Version,
+    migration_status::MigrationStatus, migration_status_row::MigrationStatusRow, version::Version,
 };
 
 #[async_trait]
 pub trait Migration {
     async fn migrate(&self, pool: AnyPool) -> sqlx::Result<()>;
     fn version(&self) -> u32;
-}
-
-struct MigrationStatusRow {
-    current_version: i64,
-    updated_version: Option<i64>,
-    value: String,
-}
-
-impl MigrationStatusRow {
-    fn current_version(&self) -> Version {
-        Version::try_from(self.current_version).expect("persisted current_version is invalid")
-    }
-
-    fn updated_version(&self) -> Option<Version> {
-        self.updated_version
-            .map(Version::try_from)
-            .transpose()
-            .expect("persisted updated_version is invalid")
-    }
-
-    fn value(&self) -> MigrationStatusValue {
-        MigrationStatusValue::from_str(self.value.as_str())
-            .expect("persisted migration_status is invalid")
-    }
-}
-
-impl From<MigrationStatusRow> for MigrationStatus {
-    fn from(row: MigrationStatusRow) -> Self {
-        match row.value() {
-            MigrationStatusValue::InProgress => MigrationStatus::InProgress {
-                current_version: row.current_version(),
-                updated_version: row
-                    .updated_version()
-                    .expect("persisted updated_version is invalid"),
-            },
-            MigrationStatusValue::Completed => MigrationStatus::Completed {
-                current_version: row.current_version(),
-            },
-        }
-    }
-}
-
-impl<'r> FromRow<'r, AnyRow> for MigrationStatusRow {
-    fn from_row(row: &'r AnyRow) -> Result<Self, sqlx::Error> {
-        Ok(Self {
-            current_version: row.try_get("current_version")?,
-            updated_version: row.try_get("updated_version")?,
-            value: row.try_get("value")?,
-        })
-    }
 }
 
 #[derive(Debug, thiserror::Error)]
