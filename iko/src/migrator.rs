aab66709d0b71dc7a20755a42a6b5f597a9eca1f
bouzuya <m@bouzuya.net>
2022-05-11 06:52:40 +0900

    iko: Extract some methods

--- iko/src/migrator.rs
+++ iko/src/migrator.rs
@@ -1,6 +1,6 @@
 use sqlx::AnyPool;
 
-use crate::{migration, migrations::Migrations, query};
+use crate::{migration, migration_status::MigrationStatus, migrations::Migrations, query};
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -30,28 +30,22 @@ impl Migrator {
     pub async fn migrate(&self, migrations: &Migrations) -> Result<()> {
         self.create_table().await?;
         for migration in migrations.iter() {
-            let mut transaction = self.pool.begin().await?;
-            let migration_status = query::select_migration_status(&mut transaction).await?;
-            transaction.commit().await?;
-
+            let migration_status = self.select_migration_status().await?;
             if migration_status.current_version() >= migration.version() {
                 continue;
             }
 
-            let mut transaction = self.pool.begin().await?;
             let in_progress = migration_status.in_progress(migration.version())?;
-            query::update_migration_status(&mut transaction, &migration_status, &in_progress)
+            self.update_migration_status(&migration_status, &in_progress)
                 .await?;
-            transaction.commit().await?;
 
             migration.migrate()(self.pool.clone()).await?;
 
             // ここで失敗した場合は migration_status = in_progress で残る
             // Migration::migrate での失敗と区別がつかないため、ユーザーに手動で直してもらう
-            let mut transaction = self.pool.begin().await?;
             let completed = in_progress.complete()?;
-            query::update_migration_status(&mut transaction, &in_progress, &completed).await?;
-            transaction.commit().await?;
+            self.update_migration_status(&in_progress, &completed)
+                .await?;
         }
         Ok(())
     }
@@ -60,6 +54,24 @@ impl Migrator {
         let mut transaction = self.pool.begin().await?;
         query::create_migration_status_table(&mut transaction).await?;
         query::insert_migration_status(&mut transaction).await?;
+        transaction.commit().await?;
+        Ok(())
+    }
+
+    async fn select_migration_status(&self) -> Result<MigrationStatus> {
+        let mut transaction = self.pool.begin().await?;
+        let migration_status = query::select_migration_status(&mut transaction).await?;
+        transaction.commit().await?;
+        Ok(migration_status)
+    }
+
+    async fn update_migration_status(
+        &self,
+        current: &MigrationStatus,
+        updated: &MigrationStatus,
+    ) -> Result<()> {
+        let mut transaction = self.pool.begin().await?;
+        query::update_migration_status(&mut transaction, current, updated).await?;
         transaction.commit().await?;
         Ok(())
     }
