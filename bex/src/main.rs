88e901f95fdcae58e33f9509b9f6fee4f4236ac8
bouzuya <m@bouzuya.net>
2022-05-17 22:02:25 +0900

    bex: Extract request mod

--- bex/src/main.rs
+++ bex/src/main.rs
@@ -1,66 +1,10 @@
 use std::{env, io};
 
-use hyper::StatusCode;
-use reqwest::Response;
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use crate::request::{
+    access_token_request, authorization_request, AccessTokenRequest, AuthorizationRequest,
+};
 
-#[derive(Debug, thiserror::Error)]
-enum Error {
-    #[error("request {0}")]
-    Request(#[from] reqwest::Error),
-    #[error(
-        "status X-Error: {x_error:?}, X-Error-Code: {x_error_code:?}, HTTP Status: {status_code}"
-    )]
-    Status {
-        status_code: u16,
-        x_error_code: Option<String>,
-        x_error: Option<String>,
-    },
-}
-
-fn check_status_code(response: &Response) -> Option<Error> {
-    let status = response.status();
-    if status == StatusCode::OK {
-        return None;
-    }
-
-    let headers = response.headers();
-    let x_error_code = headers.get("X-Error-Code");
-    let x_error = headers.get("X-Error");
-    Some(Error::Status {
-        status_code: status.as_u16(),
-        x_error_code: x_error_code
-            .map(|v| v.to_str())
-            .transpose()
-            .unwrap()
-            .map(|v| v.to_owned()),
-        x_error: x_error
-            .map(|v| v.to_str())
-            .transpose()
-            .unwrap()
-            .map(|v| v.to_owned()),
-    })
-}
-
-async fn post<T, U>(url: &str, body: &T) -> Result<U, Error>
-where
-    T: Serialize + ?Sized,
-    U: DeserializeOwned,
-{
-    let client = reqwest::Client::new();
-    let response = client
-        .post(url)
-        .header("Content-Type", "application/json; charset=UTF8")
-        .header("X-Accept", "application/json")
-        .json(&body)
-        .send()
-        .await?;
-    if let Some(error) = check_status_code(&response) {
-        return Err(error);
-    }
-    let response_body = response.json::<U>().await?;
-    Ok(response_body)
-}
+mod request;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -70,24 +14,11 @@ async fn main() -> anyhow::Result<()> {
     let state = "state1";
 
     // Step 2: Obtain a request token
-    #[derive(Debug, Serialize)]
-    struct OAuthRequestRequestBody<'a> {
-        consumer_key: &'a str,
-        redirect_uri: &'a str,
-        state: Option<&'a str>,
-    }
-    #[derive(Debug, Deserialize)]
-    struct OAuthRequestResponseBody {
-        code: String,
-    }
-    let response_body: OAuthRequestResponseBody = post(
-        "https://getpocket.com/v3/oauth/request",
-        &OAuthRequestRequestBody {
-            consumer_key: consumer_key.as_str(),
-            redirect_uri,
-            state: Some(state),
-        },
-    )
+    let response_body = authorization_request(&AuthorizationRequest {
+        consumer_key: consumer_key.as_str(),
+        redirect_uri,
+        state: Some(state),
+    })
     .await?;
     println!("{:#?}", response_body);
     let request_token = response_body.code;
@@ -106,24 +37,10 @@ async fn main() -> anyhow::Result<()> {
     io::stdin().read_line(&mut buffer)?;
 
     // Step 5: Convert a request token into a Pocket access token
-    #[derive(Debug, Serialize)]
-    struct OAuthAuthorizeRequestBody<'a> {
-        consumer_key: &'a str,
-        code: &'a str,
-    }
-    #[derive(Debug, Deserialize)]
-    struct OAuthAuthorizeResponseBody {
-        access_token: String,
-        username: String,
-        state: Option<String>,
-    }
-    let response_body: OAuthAuthorizeResponseBody = post(
-        "https://getpocket.com/v3/oauth/authorize",
-        &OAuthAuthorizeRequestBody {
-            consumer_key: consumer_key.as_str(),
-            code: request_token.as_str(),
-        },
-    )
+    let response_body = access_token_request(&AccessTokenRequest {
+        consumer_key: consumer_key.as_str(),
+        code: request_token.as_str(),
+    })
     .await?;
     println!("{:#?}", response_body);
     if response_body.state.as_deref() != Some(state) {
---
+++ bex/src/request.rs
@@ -0,0 +1,98 @@
+use hyper::StatusCode;
+use reqwest::Response;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("request {0}")]
+    Request(#[from] reqwest::Error),
+    #[error(
+        "status X-Error: {x_error:?}, X-Error-Code: {x_error_code:?}, HTTP Status: {status_code}"
+    )]
+    Status {
+        status_code: u16,
+        x_error_code: Option<String>,
+        x_error: Option<String>,
+    },
+}
+
+#[derive(Debug, Serialize)]
+pub struct AuthorizationRequest<'a> {
+    pub consumer_key: &'a str,
+    pub redirect_uri: &'a str,
+    pub state: Option<&'a str>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AuthorizationResponse {
+    pub code: String,
+}
+
+pub async fn authorization_request(
+    request: &AuthorizationRequest<'_>,
+) -> Result<AuthorizationResponse, Error> {
+    post("https://getpocket.com/v3/oauth/request", request).await
+}
+
+#[derive(Debug, Serialize)]
+pub struct AccessTokenRequest<'a> {
+    pub consumer_key: &'a str,
+    pub code: &'a str,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AccessTokenResponse {
+    pub access_token: String,
+    pub username: String,
+    pub state: Option<String>,
+}
+
+pub async fn access_token_request(
+    request: &AccessTokenRequest<'_>,
+) -> Result<AccessTokenResponse, Error> {
+    post("https://getpocket.com/v3/oauth/authorize", request).await
+}
+
+fn check_status_code(response: &Response) -> Option<Error> {
+    let status = response.status();
+    if status == StatusCode::OK {
+        return None;
+    }
+
+    let headers = response.headers();
+    let x_error_code = headers.get("X-Error-Code");
+    let x_error = headers.get("X-Error");
+    Some(Error::Status {
+        status_code: status.as_u16(),
+        x_error_code: x_error_code
+            .map(|v| v.to_str())
+            .transpose()
+            .unwrap()
+            .map(|v| v.to_owned()),
+        x_error: x_error
+            .map(|v| v.to_str())
+            .transpose()
+            .unwrap()
+            .map(|v| v.to_owned()),
+    })
+}
+
+async fn post<T, U>(url: &str, body: &T) -> Result<U, Error>
+where
+    T: Serialize + ?Sized,
+    U: DeserializeOwned,
+{
+    let client = reqwest::Client::new();
+    let response = client
+        .post(url)
+        .header("Content-Type", "application/json; charset=UTF8")
+        .header("X-Accept", "application/json")
+        .json(&body)
+        .send()
+        .await?;
+    if let Some(error) = check_status_code(&response) {
+        return Err(error);
+    }
+    let response_body = response.json::<U>().await?;
+    Ok(response_body)
+}
