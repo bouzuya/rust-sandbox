7328a2fdf7d960a38a3933a772077410f3dbb101
bouzuya <m@bouzuya.net>
2022-05-21 06:52:09 +0900

    bex: Add RetrieveResponse

--- bex/src/main.rs
+++ bex/src/main.rs
@@ -139,7 +139,51 @@ async fn main() -> anyhow::Result<()> {
         offset: None,
     })
     .await?;
+
     println!("{:#?}", response_body);
+
+    #[derive(Debug)]
+    struct RetrieveResponse {
+        item_id: String,
+        // resolved_id - A unique identifier similar to the item_id but is unique to the actual url of the saved item. The resolved_id identifies unique urls. For example a direct link to a New York Times article and a link that redirects (ex a shortened bit.ly url) to the same article will share the same resolved_id. If this value is 0, it means that Pocket has not processed the item. Normally this happens within seconds but is possible you may request the item before it has been resolved.
+        // given_url - The actual url that was saved with the item. This url should be used if the user wants to view the item.
+        // resolved_url - The final url of the item. For example if the item was a shortened bit.ly link, this will be the actual article the url linked to.
+        // given_title - The title that was saved along with the item.
+        // resolved_title - The title that Pocket found for the item when it was parsed
+        // favorite - 0 or 1 - 1 If the item is favorited
+        // status - 0, 1, 2 - 1 if the item is archived - 2 if the item should be deleted
+        // excerpt - The first few lines of the item (articles only)
+        // is_article - 0 or 1 - 1 if the item is an article
+        // has_image - 0, 1, or 2 - 1 if the item has images in it - 2 if the item is an image
+        // has_video - 0, 1, or 2 - 1 if the item has videos in it - 2 if the item is a video
+        // word_count - How many words are in the article
+        // tags - A JSON object of the user tags associated with the item
+        // authors - A JSON object listing all of the authors associated with the item
+        // images - A JSON object listing all of the images associated with the item
+        // videos - A JSON object listing all of the videos associated with the item
+    }
+
+    let list = response_body
+        .get("list")
+        .map(|v| v.as_object())
+        .unwrap()
+        .unwrap();
+    let (item_id_q, item) = list.iter().next().unwrap();
+    let item_id = item
+        .as_object()
+        .and_then(|o| o.get("item_id"))
+        .and_then(|v| v.as_str())
+        .unwrap()
+        .to_owned();
+    if item_id.as_str() != item_id_q.as_str() {
+        // TODO: Error
+        println!(
+            "item_id does not match: item_id = {}, key = {}",
+            item_id, item_id_q
+        );
+    }
+    let response = RetrieveResponse { item_id };
+    println!("{:#?}", response);
 
     Ok(())
 }
