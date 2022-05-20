d6b8200f29be3f7e75ca0ee5442573470ce31db0
bouzuya <m@bouzuya.net>
2022-05-20 21:29:52 +0900

    bex: Add Credential

--- bex/src/main.rs
+++ bex/src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, io};
+use std::{
+    env, fs, io,
+    path::{Path, PathBuf},
+};
+
+use serde::{Deserialize, Serialize};
+use xdg::BaseDirectories;
 
 use crate::request::{
     access_token_request, authorization_request, retrieve_request, AccessTokenRequest,
@@ -7,23 +13,33 @@ use crate::request::{
 
 mod request;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn state_dir() -> anyhow::Result<PathBuf> {
+    let prefix = "net.bouzuya.rust-sandbox.bex";
+    Ok(match env::var_os("BEX_STATE_DIR") {
+        Some(state_dir) => PathBuf::from(state_dir),
+        None => BaseDirectories::with_prefix(prefix)?.get_state_home(),
+    })
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Credential {
+    access_token: String,
+    username: String,
+}
+
+async fn authorize(consumer_key: &str) -> anyhow::Result<Credential> {
     // Step 1: Obtain a platform consumer key
-    let consumer_key = env::var("CONSUMER_KEY")?;
     let redirect_uri = "pocketapp1234:authorizationFinished";
     let state = "state1";
 
     // Step 2: Obtain a request token
     let response_body = authorization_request(&AuthorizationRequest {
-        consumer_key: consumer_key.as_str(),
+        consumer_key,
         redirect_uri,
         state: Some(state),
     })
     .await?;
-    println!("{:#?}", response_body);
     let request_token = response_body.code;
-    println!("request_token = {}", request_token);
 
     // Step 3: Redirect user to Pocket to continue authorization
     // TODO: encode uri component
@@ -39,11 +55,10 @@ async fn main() -> anyhow::Result<()> {
 
     // Step 5: Convert a request token into a Pocket access token
     let response_body = access_token_request(&AccessTokenRequest {
-        consumer_key: consumer_key.as_str(),
+        consumer_key,
         code: request_token.as_str(),
     })
     .await?;
-    println!("{:#?}", response_body);
     if response_body.state.as_deref() != Some(state) {
         // TODO: Error
         println!(
@@ -52,11 +67,62 @@ async fn main() -> anyhow::Result<()> {
             response_body.state.as_deref()
         );
     }
-    let access_token = response_body.access_token;
-    println!("access_token = {}", access_token);
-    let username = response_body.username;
-    println!("username = {}", username);
 
+    let credential = Credential {
+        access_token: response_body.access_token,
+        username: response_body.username,
+    };
+
+    Ok(credential)
+}
+
+struct CredentialStore {
+    path: PathBuf,
+}
+
+impl CredentialStore {
+    fn new<P: AsRef<Path>>(dir: P) -> Self {
+        Self {
+            path: dir.as_ref().join("credential.json"),
+        }
+    }
+
+    fn load(&self) -> anyhow::Result<Option<Credential>> {
+        let p = self.path.as_path();
+        if p.exists() {
+            let s = fs::read_to_string(p)?;
+            Ok(serde_json::from_str(&s)?)
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn store(&self, credential: &Credential) -> anyhow::Result<()> {
+        let p = self.path.as_path();
+        if let Some(dir) = p.parent() {
+            fs::create_dir_all(dir)?;
+        }
+        fs::write(p, serde_json::to_string(credential)?)?;
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let consumer_key = env::var("CONSUMER_KEY")?;
+    let state_dir = state_dir()?;
+    let credential_store = CredentialStore::new(state_dir.as_path());
+    let credential = match credential_store.load()? {
+        Some(c) => c,
+        None => {
+            let credential = authorize(consumer_key.as_str()).await?;
+            credential_store.store(&credential)?;
+            credential
+        }
+    };
+    println!("{:#?}", credential);
+
+    let access_token = credential.access_token;
     let response_body = retrieve_request(&RetrieveRequest {
         consumer_key: consumer_key.as_str(),
         access_token: access_token.as_str(),
