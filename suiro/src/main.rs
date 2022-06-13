147f17171a5150643d921678a9f7ffc1020bf6a0
bouzuya <m@bouzuya.net>
2022-06-14 07:03:30 +0900

    suiro: Extract block mod

--- suiro/src/main.rs
+++ suiro/src/main.rs
@@ -1,46 +1,28 @@
-use std::fmt::Display;
+mod block;
 
-enum Block {
-    I(u8),
-    L(u8),
-    T(u8),
-}
+use block::Block;
 
-impl Display for Block {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let c = match self {
-            Block::I(d) => match d {
-                0 => '│',
-                1 => '─',
-                _ => unreachable!(),
-            },
-            Block::L(d) => match d {
-                0 => '└',
-                1 => '┌',
-                2 => '┐',
-                3 => '┘',
-                _ => unreachable!(),
-            },
-            Block::T(d) => match d {
-                0 => '┬',
-                1 => '┤',
-                2 => '┴',
-                3 => '├',
-                _ => unreachable!(),
-            },
-        };
-        write!(f, "{}", c)
+fn print_board(width: usize, height: usize, blocks: &[Block]) {
+    for i in 0..height {
+        for j in 0..width {
+            print!("{}", blocks[i * width + j]);
+        }
+        println!();
     }
 }
 
 fn main() {
     let width = 2;
     let height = 2;
-    let board = vec![Block::I(1), Block::L(2), Block::T(0), Block::L(0)];
-    for i in 0..height {
-        for j in 0..width {
-            print!("{}", board[i * width + j]);
-        }
+    let mut blocks = vec![Block::I(1), Block::L(0), Block::T(0), Block::L(0)];
+    print_board(width, height, &blocks);
+    println!();
+
+    let operations = vec![(0, 1), (0, 1)];
+    for o in operations.iter().copied() {
+        let p = o.0 * width + o.1;
+        blocks[p] = blocks[p].rotate();
+        print_board(width, height, &blocks);
         println!();
     }
 }
