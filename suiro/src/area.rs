bf69d54a926893998a13cbebf75e7eeaed615216
bouzuya <m@bouzuya.net>
2022-06-14 20:56:48 +0900

    suiro: Add area mod

---
+++ suiro/src/area.rs
@@ -0,0 +1,93 @@
+use crate::Block;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("too few pipes")]
+    TooFewPipes,
+    #[error("too many pipes")]
+    TooManyPipes,
+}
+
+pub struct Area {
+    width: u8,
+    height: u8,
+    pipes: Vec<Block>,
+}
+
+impl Area {
+    pub fn new(width: u8, height: u8, pipes: Vec<Block>) -> Result<Self, Error> {
+        let length = u16::try_from(pipes.len()).map_err(|_| Error::TooManyPipes)?;
+        match length.cmp(&(u16::from(width) * u16::from(height))) {
+            std::cmp::Ordering::Less => Err(Error::TooFewPipes),
+            std::cmp::Ordering::Greater => Err(Error::TooManyPipes),
+            std::cmp::Ordering::Equal => Ok(()),
+        }?;
+        Ok(Self {
+            width,
+            height,
+            pipes,
+        })
+    }
+
+    pub fn print(&self) {
+        let w = usize::from(self.width);
+        let h = usize::from(self.height);
+        for i in 0..h {
+            for j in 0..w {
+                print!("{}", self.pipes[i * w + j]);
+            }
+            println!();
+        }
+    }
+
+    pub fn pipe(&self, x: u8, y: u8) -> Block {
+        let x = usize::from(x);
+        let y = usize::from(y);
+        let w = usize::from(self.width);
+        self.pipes[y * w + x]
+    }
+
+    pub fn rotate(&mut self, x: u8, y: u8) {
+        let x = usize::from(x);
+        let y = usize::from(y);
+        let w = usize::from(self.width);
+        self.pipes[y * w + x] = self.pipes[y * w + x].rotate();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_test() -> anyhow::Result<()> {
+        let area = Area::new(
+            2,
+            2,
+            vec![Block::I(1), Block::L(0), Block::T(0), Block::L(0)],
+        )?;
+        assert_eq!(area.pipe(0, 0), Block::I(1));
+        assert_eq!(area.pipe(1, 0), Block::L(0));
+        assert_eq!(area.pipe(0, 1), Block::T(0));
+        assert_eq!(area.pipe(1, 1), Block::L(0));
+        Ok(())
+    }
+
+    #[test]
+    fn rotate_test() -> anyhow::Result<()> {
+        let mut area = Area::new(
+            2,
+            2,
+            vec![Block::I(1), Block::L(0), Block::T(0), Block::L(0)],
+        )?;
+        assert_eq!(area.pipe(0, 0), Block::I(1));
+        assert_eq!(area.pipe(1, 0), Block::L(0));
+        assert_eq!(area.pipe(0, 1), Block::T(0));
+        assert_eq!(area.pipe(1, 1), Block::L(0));
+        area.rotate(0, 1);
+        assert_eq!(area.pipe(0, 1), Block::T(1));
+        area.rotate(0, 0);
+        assert_eq!(area.pipe(0, 0), Block::I(0));
+        Ok(())
+    }
+}
--- suiro/src/main.rs
+++ suiro/src/main.rs
@@ -1,28 +1,23 @@
+mod area;
 mod block;
 
-use block::Block;
+use self::area::Area;
+use self::block::Block;
 
-fn print_board(width: usize, height: usize, blocks: &[Block]) {
-    for i in 0..height {
-        for j in 0..width {
-            print!("{}", blocks[i * width + j]);
-        }
-        println!();
-    }
-}
-
-fn main() {
-    let width = 2;
-    let height = 2;
-    let mut blocks = vec![Block::I(1), Block::L(0), Block::T(0), Block::L(0)];
-    print_board(width, height, &blocks);
+fn main() -> anyhow::Result<()> {
+    let mut area = Area::new(
+        2,
+        2,
+        vec![Block::I(1), Block::L(0), Block::T(0), Block::L(0)],
+    )?;
+    area.print();
     println!();
 
     let operations = vec![(0, 1), (0, 1)];
     for o in operations.iter().copied() {
-        let p = o.0 * width + o.1;
-        blocks[p] = blocks[p].rotate();
-        print_board(width, height, &blocks);
+        area.rotate(o.1, o.0);
+        area.print();
         println!();
     }
+    Ok(())
 }
