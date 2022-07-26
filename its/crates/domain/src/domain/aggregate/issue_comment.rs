54c3a5c956b26bb97f5650df87e0df7921a2318c
bouzuya <m@bouzuya.net>
2022-07-26 19:07:13 +0900

    its: Add issue_comment aggregate

--- its/crates/domain/src/domain/aggregate/issue_comment.rs
+++ its/crates/domain/src/domain/aggregate/issue_comment.rs
@@ -2,4 +2,14 @@ pub mod attribute;
 pub mod entity;
 pub mod event;
 
+use crate::Version;
+
+use self::entity::IssueComment;
 pub use self::event::Event;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct IssueCommentAggregate {
+    events: Vec<Event>,
+    issue_comment: IssueComment,
+    version: Version,
+}
