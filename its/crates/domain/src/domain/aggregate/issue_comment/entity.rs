45f89f62dc5aeeacd081a5fc0709ec1a7d82c661
bouzuya <m@bouzuya.net>
2022-07-27 18:53:01 +0900

    its: Add test

--- its/crates/domain/src/domain/aggregate/issue_comment/entity.rs
+++ its/crates/domain/src/domain/aggregate/issue_comment/entity.rs
@@ -26,10 +26,34 @@ impl IssueComment {
 
 #[cfg(test)]
 mod tests {
+
+    use std::str::FromStr;
+
+    use limited_date_time::Instant;
+
+    use crate::Version;
+
     use super::*;
 
     #[test]
-    fn test() {
+    fn test() -> anyhow::Result<()> {
+        let at = Instant::now();
+        let issue_comment_id = IssueCommentId::generate();
+        let issue_id = IssueId::from_str("123")?;
+        let text = IssueCommentText::from_str("text")?;
+        let version = Version::from(1_u64);
+        let event = IssueCommentCreated {
+            at,
+            issue_comment_id: issue_comment_id.clone(),
+            issue_id: issue_id.clone(),
+            text: text.clone(),
+            version,
+        };
+        let issue_comment = IssueComment::from_event(event);
+        assert_eq!(issue_comment.id, issue_comment_id);
+        assert_eq!(issue_comment.issue_id, issue_id);
+        assert_eq!(issue_comment.text, text);
         // TODO
+        Ok(())
     }
 }
