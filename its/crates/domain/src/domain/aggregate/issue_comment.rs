6a74756f3e5429e1ad33152b1b0c115c6d873332
bouzuya <m@bouzuya.net>
2022-07-31 07:10:23 +0900

    its: Improve event sequence checks

--- its/crates/domain/src/domain/aggregate/issue_comment.rs
+++ its/crates/domain/src/domain/aggregate/issue_comment.rs
@@ -2,7 +2,9 @@ pub mod attribute;
 pub mod entity;
 pub mod event;
 
-use crate::Version;
+use std::collections::BTreeSet;
+
+use crate::{IssueCommentId, Version};
 
 pub use self::event::Event;
 use self::{entity::IssueComment, event::IssueCommentUpdated};
@@ -26,14 +28,9 @@ pub struct IssueCommentAggregate {
 
 impl IssueCommentAggregate {
     pub fn from_events(events: &[Event]) -> Result<Self> {
-        let first_event = match events.first() {
-            Some(event) => match event {
-                Event::Created(event) => Ok(event),
-                Event::Deleted(_) => Err(Error::InvalidEventSequence),
-                Event::Updated(_) => Err(Error::InvalidEventSequence),
-            },
-            None => Err(Error::InvalidEventSequence),
-        }?;
+        let _ = Self::check_aggregate_id(events)?;
+        let first_event = Self::check_first_event(events)?;
+        let version = Self::check_version(events)?;
         let mut issue_comment = IssueComment::from_event(first_event.clone());
         for event in events.iter().skip(1) {
             issue_comment = match event {
@@ -44,11 +41,45 @@ impl IssueCommentAggregate {
                 }
             }?;
         }
-        let last_event = events.last().expect("empty event sequence").clone();
         Ok(Self {
             events: vec![],
             issue_comment,
-            version: last_event.version(),
+            version,
         })
+    }
+
+    fn check_aggregate_id(events: &[Event]) -> Result<&IssueCommentId> {
+        let set = events
+            .iter()
+            .map(|e| e.issue_comment_id())
+            .collect::<BTreeSet<&IssueCommentId>>();
+        if set.len() == 1 {
+            Ok(set.iter().next().expect("the set contains an element"))
+        } else {
+            Err(Error::InvalidEventSequence)
+        }
+    }
+
+    fn check_first_event(events: &[Event]) -> Result<&event::IssueCommentCreated> {
+        match events.first() {
+            Some(event) => match event {
+                Event::Created(event) => Ok(event),
+                Event::Deleted(_) => Err(Error::InvalidEventSequence),
+                Event::Updated(_) => Err(Error::InvalidEventSequence),
+            },
+            None => Err(Error::InvalidEventSequence),
+        }
+    }
+
+    fn check_version(events: &[Event]) -> Result<Version> {
+        let versions = events.iter().map(|e| e.version()).collect::<Vec<Version>>();
+        let mut prev = *versions.first().ok_or(Error::InvalidEventSequence)?;
+        for curr in versions.into_iter().skip(1) {
+            if curr != prev.next().ok_or(Error::InvalidEventSequence)? {
+                return Err(Error::InvalidEventSequence);
+            }
+            prev = curr;
+        }
+        Ok(prev)
     }
 }
