1582d513463fbe2d3b7089854f6d9357260bdaa9
bouzuya <m@bouzuya.net>
2022-08-06 11:38:18 +0900

    its: Add domain::aggregate::issue_comment::Event tests

--- its/crates/domain/src/domain/aggregate/issue_comment/event.rs
+++ its/crates/domain/src/domain/aggregate/issue_comment/event.rs
@@ -71,15 +71,40 @@ mod tests {
 
     use super::*;
 
-    // TODO: issue_comment_id test
-    // TODO: version test
-
     #[test]
-    fn test() -> anyhow::Result<()> {
+    fn from_test() -> anyhow::Result<()> {
         let issue_comment_id = IssueCommentId::generate();
         let _ = Event::from(issue_comment_created(issue_comment_id.clone())?);
         let _ = Event::from(issue_comment_updated(issue_comment_id.clone())?);
         let _ = Event::from(issue_comment_deleted(issue_comment_id)?);
+        Ok(())
+    }
+
+    #[test]
+    fn issue_comment_id_test() -> anyhow::Result<()> {
+        let issue_comment_id = IssueCommentId::generate();
+        let events = vec![
+            Event::from(issue_comment_created(issue_comment_id.clone())?),
+            Event::from(issue_comment_updated(issue_comment_id.clone())?),
+            Event::from(issue_comment_deleted(issue_comment_id.clone())?),
+        ];
+        for event in events {
+            assert_eq!(event.issue_comment_id(), &issue_comment_id);
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn version_test() -> anyhow::Result<()> {
+        let issue_comment_id = IssueCommentId::generate();
+        let events = vec![
+            Event::from(issue_comment_created(issue_comment_id.clone())?),
+            Event::from(issue_comment_updated(issue_comment_id.clone())?),
+            Event::from(issue_comment_deleted(issue_comment_id)?),
+        ];
+        for (i, event) in events.into_iter().enumerate() {
+            assert_eq!(event.version(), Version::from(i as u64 + 1));
+        }
         Ok(())
     }
 
@@ -103,7 +128,7 @@ mod tests {
         issue_comment_id: IssueCommentId,
     ) -> anyhow::Result<IssueCommentDeleted> {
         let at = Instant::now();
-        let version = Version::from(2);
+        let version = Version::from(3);
         Ok(IssueCommentDeleted {
             at,
             issue_comment_id,
@@ -116,7 +141,7 @@ mod tests {
     ) -> anyhow::Result<IssueCommentUpdated> {
         let at = Instant::now();
         let text = IssueCommentText::from_str("text")?;
-        let version = Version::from(3);
+        let version = Version::from(2);
         Ok(IssueCommentUpdated {
             at,
             issue_comment_id,
