539b3a1054f359a3763f1fbc8dc15a92cc298ca3
bouzuya <m@bouzuya.net>
2022-08-08 07:11:57 +0900

    twiq: Add reqwest example

--- twiq/crates/domain/src/lib.rs
+++ twiq/crates/domain/src/lib.rs
@@ -1,8 +1,14 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() -> anyhow::Result<()> {
+        #[derive(Debug, serde::Deserialize)]
+        struct Json {
+            date: String,
+        }
+        let response = reqwest::get("https://blog.bouzuya.net/2022/08/07/index.json").await?;
+        let json: Json = response.json().await?;
+        assert_eq!(json.date, "2022-08-07");
+        Ok(())
     }
 }
