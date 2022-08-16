7c20bf0333ba7c44a9c2dcde0cbf71cf5f659bfb
bouzuya <m@bouzuya.net>
2022-08-16 21:24:53 +0900

    twiq: Add PORT env support

--- twiq/crates/web/src/main.rs
+++ twiq/crates/web/src/main.rs
@@ -1,11 +1,15 @@
 mod router;
 
+use std::env;
+
 use axum::Server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let app = router::router();
-    let addr = "0.0.0.0:3000".parse()?;
+    let host = "0.0.0.0";
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("{}:{}", host, port).parse()?;
     Server::bind(&addr).serve(app.into_make_service()).await?;
     Ok(())
 }
