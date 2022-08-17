049d5b6c401e7ef74a13507d6b45f756126ebb09
bouzuya <m@bouzuya.net>
2022-08-18 06:56:07 +0900

    twiq: Add createDocument example

--- twiq/crates/db/src/lib.rs
+++ twiq/crates/db/src/lib.rs
@@ -1,54 +1,61 @@
+use std::env;
+
+use reqwest::Response;
+use serde_json::json;
+
+async fn createDocument() -> anyhow::Result<Response> {
+    // await setDoc(doc(db, "cities", "LA"), {
+    //   name: "Los Angeles",
+    //   state: "CA",
+    //   country: "USA"
+    // });
+    //
+    // <https://cloud.google.com/firestore/docs/reference/rest/v1/projects.databases.documents/createDocument>
+
+    let bearer_token = env::var("GOOGLE_BEARER_TOKEN")?;
+    let project_id = env::var("PROJECT_ID")?;
+    let database_id = "(default)";
+    let collection_id = "cities";
+    let document_id = "LA";
+    let path = format!(
+        "/projects/{}/databases/{}/documents/{}?documentId={}",
+        project_id, database_id, collection_id, document_id
+    );
+    let url = format!("https://firestore.googleapis.com/v1{}", path);
+    let body = json!({
+      "fields": {
+        "name": {
+          "stringValue": "Los Angeles"
+        },
+        "state": {
+          "stringValue": "CA"
+        },
+        "country": {
+          "stringValue": "USA"
+        }
+      }
+    });
+    let client = reqwest::Client::new();
+    Ok(client
+        .post(url)
+        .header("Authorization", format!("Bearer {}", bearer_token))
+        .header("Content-Type", "application/json")
+        .header("X-Goog-User-Project", project_id)
+        .body(serde_json::to_string(&body)?)
+        .send()
+        .await?)
+}
+
 #[cfg(test)]
 mod tests {
-    use std::env;
-
-    use serde_json::json;
+    use super::*;
 
     #[tokio::test]
     async fn test() -> anyhow::Result<()> {
-        // await setDoc(doc(db, "cities", "LA"), {
-        //   name: "Los Angeles",
-        //   state: "CA",
-        //   country: "USA"
-        // });
-        //
-        // <https://cloud.google.com/firestore/docs/reference/rest/v1/projects.databases.documents/createDocument>
-        // `POST https://firestore.googleapis.com/v1/{parent=projects/*/databases/*/documents/**}/{collectionId}`
-        // OAuth Scope:
-        // - https://www.googleapis.com/auth/datastore
-        // - https://www.googleapis.com/auth/cloud-platform
-
-        let project_id = env::var("PROJECT_ID")?;
-        let database_id = "(default)"; // env::var("DATABASE_ID")?;
-        let bearer_token = env::var("GOOGLE_BEARER_TOKEN")?;
-        let collection_id = "cities";
-        let client = reqwest::Client::new();
-        let url = format!(
-            "https://firestore.googleapis.com/v1/projects/{}/databases/{}/documents/{}",
-            project_id, database_id, collection_id
-        );
-        let name = format!(
-            "projects/{}/databases/{}/documents/{}",
-            project_id, database_id, collection_id
-        );
-        let body = json!({
-          "name": name,
-          "fields": {
-            "foo": {
-              "stringValue": "bar"
-            }
-          }
-        });
-        let response = client
-            .post(url)
-            .header("X-Goog-User-Project", project_id)
-            .header("Authorization", format!("Bearer {}", bearer_token))
-            .body(serde_json::to_string(&body)?)
-            .send()
-            .await?;
+        let response = createDocument().await?;
         let status = response.status();
-        // assert_eq!(response.bytes().await?, "");
         assert_eq!(status, 200);
+        // assert_eq!(response.bytes().await?, "");
         Ok(())
     }
 }
