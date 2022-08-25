562b5c039290a7995cf08fb42429789d15c8df18
bouzuya <m@bouzuya.net>
2022-08-25 23:09:04 +0900

    twiq: Remove unused functions

--- twiq/crates/db/src/event_store.rs
+++ twiq/crates/db/src/event_store.rs
@@ -105,15 +105,6 @@ pub async fn store(current: Option<EventStreamSeq>, event: Event) -> Result<(), 
                 },
                 update_mask: None,
             });
-            // update_event_stream(
-            //     &bearer_token,
-            //     &project_id,
-            //     database_id,
-            //     update_time,
-            //     event.stream_id(),
-            //     event.stream_seq(),
-            // )
-            // .await?;
         }
         None => {
             let collection_id = "event_streams";
@@ -131,18 +122,9 @@ pub async fn store(current: Option<EventStreamSeq>, event: Event) -> Result<(), 
                 },
                 update_mask: None,
             });
-            // create_event_stream(
-            //     &bearer_token,
-            //     &project_id,
-            //     database_id,
-            //     event.stream_id(),
-            //     event.stream_seq(),
-            // )
-            // .await?;
         }
     }
 
-    // create_event(&bearer_token, &project_id, database_id, event).await?;
     let collection_id = "events";
     let document_id = event.id().to_string();
     writes.push(Write::Update {
@@ -173,78 +155,6 @@ pub async fn store(current: Option<EventStreamSeq>, event: Event) -> Result<(), 
     // TODO: rollback
 
     Ok(())
-}
-
-async fn create_event(
-    bearer_token: &str,
-    project_id: &str,
-    database_id: &str,
-    event: Event,
-) -> Result<Response, Error> {
-    let collection_id = "events";
-    let parent = format!(
-        "projects/{}/databases/{}/documents",
-        project_id, database_id,
-    );
-    let response = firestore_rest::create_document(
-        (bearer_token, project_id),
-        &parent,
-        collection_id,
-        Some(event.id().to_string().as_str()),
-        None,
-        Document {
-            name: "unused".to_owned(),
-            fields: event_to_fields(&event),
-            create_time: None,
-            update_time: None,
-        },
-    )
-    .await
-    .map_err(|e| Error::Unknown(e.to_string()))?;
-    if !response.status().is_success() {
-        return Err(Error::Unknown(format!(
-            "create_event failed: status code ({}) is not success",
-            response.status()
-        )));
-    }
-    Ok(response)
-}
-
-async fn create_event_stream(
-    bearer_token: &str,
-    project_id: &str,
-    database_id: &str,
-    event_stream_id: EventStreamId,
-    event_stream_seq: EventStreamSeq,
-) -> Result<Response, Error> {
-    let collection_id = "event_streams";
-    let parent = format!(
-        "projects/{}/databases/{}/documents",
-        &project_id, &database_id,
-    );
-    let response = firestore_rest::create_document(
-        (bearer_token, project_id),
-        &parent,
-        collection_id,
-        Some(event_stream_id.to_string().as_str()),
-        None,
-        Document {
-            name: "unused".to_owned(),
-            fields: event_stream_to_fields(event_stream_id, event_stream_seq),
-            create_time: None,
-            update_time: None,
-        },
-    )
-    .await
-    .map_err(|e| Error::Unknown(e.to_string()))?;
-    if !response.status().is_success() {
-        // TODO: rollback
-        return Err(Error::Unknown(format!(
-            "create_event_stream failed: status code ({}) is not success",
-            response.status()
-        )));
-    }
-    Ok(response)
 }
 
 async fn get_event_stream(
@@ -317,45 +227,6 @@ async fn get_event_stream(
         event_stream_seq,
         document.update_time.unwrap(),
     ))
-}
-
-async fn update_event_stream(
-    bearer_token: &str,
-    project_id: &str,
-    database_id: &str,
-    current_document_update_time: Timestamp,
-    event_stream_id: EventStreamId,
-    event_stream_seq: EventStreamSeq,
-) -> Result<Response, Error> {
-    let collection_id = "event_streams";
-    let document_id = event_stream_id;
-    let document_name = format!(
-        "projects/{}/databases/{}/documents/{}/{}",
-        &project_id, &database_id, collection_id, document_id
-    );
-    let response = firestore_rest::patch(
-        (bearer_token, project_id),
-        &document_name,
-        None,
-        None,
-        Some(true),
-        Some(current_document_update_time),
-        Document {
-            name: document_name.clone(),
-            fields: event_stream_to_fields(event_stream_id, event_stream_seq),
-            create_time: None,
-            update_time: None,
-        },
-    )
-    .await
-    .map_err(|e| Error::Unknown(e.to_string()))?;
-    if !response.status().is_success() {
-        return Err(Error::Unknown(format!(
-            "update_event_stream failed: status code ({}) is not success",
-            response.status()
-        )));
-    }
-    Ok(response)
 }
 
 #[cfg(test)]
