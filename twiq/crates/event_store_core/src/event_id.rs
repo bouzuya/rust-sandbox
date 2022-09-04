0c730f01bb6a80b817d84f29205455b062067a53
bouzuya <m@bouzuya.net>
2022-09-05 06:57:52 +0900

    twiq: Add impl TryFrom<u128> for EventId

--- twiq/crates/event_store_core/src/event_id.rs
+++ twiq/crates/event_store_core/src/event_id.rs
@@ -1,6 +1,6 @@
 use std::{fmt::Display, str::FromStr};
 
-use uuid::Uuid;
+use uuid::{Uuid, Variant};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -32,8 +32,22 @@ impl FromStr for EventId {
     }
 }
 
+impl TryFrom<u128> for EventId {
+    type Error = Error;
+
+    fn try_from(value: u128) -> Result<Self, Self::Error> {
+        let uuid = Uuid::from_u128(value);
+        if !(uuid.get_version_num() == 4 && uuid.get_variant() == Variant::RFC4122) {
+            return Err(Error::InvalidFormat("u128 value is not UUID v4".to_owned()));
+        }
+        Ok(Self(uuid))
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use uuid::Builder;
+
     use super::*;
 
     #[test]
@@ -41,6 +55,25 @@ mod tests {
         assert_ne!(EventId::generate(), EventId::generate());
         let s = "70ec72e5-7fd8-4681-abfa-d60a9aa993c2";
         assert_eq!(EventId::from_str(s)?.to_string(), s);
+
+        let uuid = Uuid::new_v4();
+        assert!(EventId::try_from(uuid.as_u128()).is_ok());
+        assert!(EventId::try_from(Uuid::nil().as_u128()).is_err());
+        assert!(EventId::try_from(
+            Builder::from_u128(uuid.as_u128())
+                .with_variant(Variant::Microsoft)
+                .as_uuid()
+                .as_u128()
+        )
+        .is_err());
+        assert!(EventId::try_from(
+            Builder::from_u128(uuid.as_u128())
+                .with_version(uuid::Version::Md5)
+                .as_uuid()
+                .as_u128()
+        )
+        .is_err());
+
         Ok(())
     }
 }
