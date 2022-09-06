5a34ed43635a408c5e9f2293754500e7343c619a
bouzuya <m@bouzuya.net>
2022-09-06 23:06:33 +0900

    twiq: Fix User::create

--- twiq/crates/domain/src/aggregate/user.rs
+++ twiq/crates/domain/src/aggregate/user.rs
@@ -1,7 +1,15 @@
 mod event;
 mod value;
 
-use self::event::{Event, UserCreated, UserFetchRequested, UserUpdated};
+use event_store_core::{
+    event_id::EventId, event_stream_id::EventStreamId, event_stream_seq::EventStreamSeq,
+};
+use time::OffsetDateTime;
+
+use self::{
+    event::{Event, UserCreated, UserFetchRequested, UserUpdated},
+    value::{twitter_user_id::TwitterUserId, user_id::UserId},
+};
 
 #[derive(Debug, thiserror::Error)]
 #[error("error")]
@@ -14,11 +22,25 @@ pub struct User {
 }
 
 impl User {
-    pub fn create() -> Self {
-        // TODO
-        Self {
-            events: vec![Event::Created(UserCreated)],
-        }
+    pub fn create(twitter_user_id: TwitterUserId) -> Result<Self> {
+        let id = EventId::generate();
+        let at = OffsetDateTime::now_utc();
+        // user_id = event_stream_id
+        let user_id = UserId::generate();
+        let stream_id = EventStreamId::try_from(u128::from(user_id)).map_err(|_| {
+            // TODO: error handling
+            Error
+        })?;
+        let stream_seq = EventStreamSeq::from(1);
+        Ok(Self {
+            events: vec![Event::Created(UserCreated::new(
+                id,
+                at,
+                stream_id,
+                stream_seq,
+                twitter_user_id,
+            ))],
+        })
     }
 
     pub fn fetch_request(&mut self) -> Result<()> {
@@ -39,8 +61,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_test() {
-        // TODO
+    fn create_test() -> anyhow::Result<()> {
+        let twitter_user_id = "bouzuya".parse()?;
+        let user = User::create(twitter_user_id)?;
+        assert!(matches!(user.events[0], Event::Created(_)));
+        // TODO: check twitter_user_id
+        Ok(())
     }
 
     #[test]
