395190a9ba85a95d9de6e846928a6c7883f1862b
bouzuya <m@bouzuya.net>
2022-09-15 07:17:13 +0900

    twiq: Fix re-export event_store_core types

--- twiq/crates/event_store_core/src/lib.rs
+++ twiq/crates/event_store_core/src/lib.rs
@@ -4,3 +4,9 @@ pub mod event_id;
 pub mod event_stream_id;
 pub mod event_stream_seq;
 pub(crate) mod uuid_v4;
+
+pub use self::event::Event;
+pub use self::event_data::EventData;
+pub use self::event_id::EventId;
+pub use self::event_stream_id::EventStreamId;
+pub use self::event_stream_seq::EventStreamSeq;
