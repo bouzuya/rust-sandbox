f51e7e296ff9378323bc83c1573e7b42521e2700
bouzuya <m@bouzuya.net>
2022-09-14 22:26:42 +0900

    twiq: Add impl From<domain::aggregate::Event> for event_store_core::Event

--- twiq/crates/domain/src/event.rs
+++ twiq/crates/domain/src/event.rs
@@ -1,7 +1,17 @@
-#[derive(Clone, Debug, Eq, PartialEq)]
+use event_store_core::Event as RawEvent;
+
 pub enum Event {
     User(crate::aggregate::user::Event),
     UserRequest(crate::aggregate::user_request::Event),
+}
+
+impl From<Event> for RawEvent {
+    fn from(event: Event) -> Self {
+        match event {
+            Event::User(e) => RawEvent::from(e),
+            Event::UserRequest(e) => RawEvent::from(e),
+        }
+    }
 }
 
 // TODO: test serde
