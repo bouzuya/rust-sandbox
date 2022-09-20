389a2b447463a061ec126bf7d9e4fd5d1022a8a0
bouzuya <m@bouzuya.net>
2022-09-20 19:56:25 +0900

    twiq: Rename event_store from use_case crate

--- twiq/crates/event_store_core/src/event_store.rs
+++ twiq/crates/event_store_core/src/event_store.rs
@@ -2,23 +2,27 @@ use async_trait::async_trait;
 
 use crate::{Event, EventId, EventStreamId, EventStreamSeq};
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, Eq, PartialEq, thiserror::Error)]
 pub enum Error {
     #[error("unknown error : {0}")]
     Unknown(String),
 }
 
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
 #[async_trait]
 pub trait EventStore {
-    async fn find_events_by_event_id_after(&self, event_id: EventId) -> Result<Vec<Event>, Error>;
+    async fn find_event(&self, event_id: EventId) -> Result<Option<Event>>;
+    async fn find_event_ids(&self) -> Result<Vec<EventId>>;
+    async fn find_event_ids_by_event_id_after(&self, event_id: EventId) -> Result<Vec<EventId>>;
+    async fn find_events_by_event_id_after(&self, event_id: EventId) -> Result<Vec<Event>>;
 
     // = find_event_stream_by_id
     async fn find_events_by_event_stream_id(
         &self,
         event_stream_id: EventStreamId,
-    ) -> Result<Vec<Event>, Error>;
+    ) -> Result<Vec<Event>>;
 
     // = store_event_stream
-    async fn store(&self, current: Option<EventStreamSeq>, events: Vec<Event>)
-        -> Result<(), Error>;
+    async fn store(&self, current: Option<EventStreamSeq>, events: Vec<Event>) -> Result<()>;
 }
--- twiq/crates/use_case/src/event_store.rs
+++ twiq/crates/use_case/src/event_store.rs
@@ -1,20 +1,4 @@
-use async_trait::async_trait;
-use event_store_core::{event::Event, event_id::EventId};
-
-#[derive(Debug, Eq, PartialEq, thiserror::Error)]
-pub enum Error {
-    #[error("unknown {0}")]
-    Unknown(String),
-}
-
-pub type Result<T, E = Error> = std::result::Result<T, E>;
-
-#[async_trait]
-pub trait EventStore {
-    async fn find_event(&self, event_id: EventId) -> Result<Option<Event>>;
-    async fn find_event_ids(&self) -> Result<Vec<EventId>>;
-    async fn find_event_ids_by_event_id_after(&self, event_id: EventId) -> Result<Vec<EventId>>;
-}
+pub use event_store_core::event_store::{Error, EventStore};
 
 pub trait HasEventStore {
     type EventStore: EventStore + Send + Sync;
