d6ed1a501a003e29810c656f0a435b5eb6a24da6
bouzuya <m@bouzuya.net>
2022-09-22 22:50:12 +0900

    twiq: Add EventStream::{generate,push,push_event}

--- twiq/crates/event_store_core/src/event_stream.rs
+++ twiq/crates/event_store_core/src/event_stream.rs
@@ -1,4 +1,4 @@
-use crate::{Event, EventStreamId, EventStreamSeq};
+use crate::{Event, EventData, EventId, EventStreamId, EventStreamSeq};
 
 #[derive(Debug, Eq, PartialEq, thiserror::Error)]
 pub enum Error {
@@ -8,6 +8,8 @@ pub enum Error {
     InvalidEventStreamId(EventStreamId),
     #[error("invalid event stream seq : {0}")]
     InvalidEventStreamSeq(EventStreamId),
+    #[error("overflow event stream seq : {0}")]
+    OverflowEventStreamSeq(EventStreamId),
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
@@ -18,6 +20,17 @@ pub struct EventStream {
 }
 
 impl EventStream {
+    pub fn generate(event_data: EventData) -> Self {
+        Self {
+            events: vec![Event::new(
+                EventId::generate(),
+                EventStreamId::generate(),
+                EventStreamSeq::from(1_u32),
+                event_data,
+            )],
+        }
+    }
+
     pub fn new(events: Vec<Event>) -> Result<Self> {
         if events.is_empty() {
             return Err(Error::Empty);
@@ -54,22 +67,54 @@ impl EventStream {
     pub fn events(&self) -> Vec<Event> {
         self.events.clone()
     }
+
+    pub fn push(&mut self, event_data: EventData) -> Result<()> {
+        self.events.push(Event::new(
+            EventId::generate(),
+            self.id(),
+            self.seq()
+                .next()
+                .map_err(|_| Error::OverflowEventStreamSeq(self.id()))?,
+            event_data,
+        ));
+        Ok(())
+    }
+
+    pub fn push_event(&mut self, event: Event) -> Result<()> {
+        if event.stream_id() != self.id() {
+            return Err(Error::InvalidEventStreamId(self.id()));
+        }
+        if event.stream_seq() <= self.seq() {
+            return Err(Error::InvalidEventStreamId(self.id()));
+        }
+        self.events.push(event);
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
 
-    use crate::{EventData, EventId};
+    use crate::EventData;
 
     use super::*;
 
     #[test]
-    fn test() -> anyhow::Result<()> {
-        // empty
-        assert!(EventStream::new(vec![]).is_err());
+    fn generate_test() -> anyhow::Result<()> {
+        let stream = EventStream::generate(EventData::from_str("{}")?);
+        assert_eq!(stream.seq(), EventStreamSeq::from(1_u32));
+        Ok(())
+    }
 
-        // ok (an event)
+    #[test]
+    fn new_test_empty() -> anyhow::Result<()> {
+        assert!(EventStream::new(vec![]).is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn new_test_an_event() -> anyhow::Result<()> {
         let event = Event::new(
             EventId::generate(),
             EventStreamId::generate(),
@@ -80,8 +125,11 @@ mod tests {
         assert_eq!(stream.id(), event.stream_id());
         assert_eq!(stream.seq(), event.stream_seq());
         assert_eq!(stream.events(), vec![event]);
+        Ok(())
+    }
 
-        // invalid event_stream_id
+    #[test]
+    fn new_test_invalid_event_stream_id() -> anyhow::Result<()> {
         let event1 = Event::new(
             EventId::generate(),
             EventStreamId::generate(),
@@ -95,8 +143,11 @@ mod tests {
             EventData::from_str("{}")?,
         );
         assert!(EventStream::new(vec![event1, event2]).is_err());
+        Ok(())
+    }
 
-        // invalid event_stream_seq
+    #[test]
+    fn new_test_invalid_event_stream_seq() -> anyhow::Result<()> {
         let stream_id = EventStreamId::generate();
         let event1 = Event::new(
             EventId::generate(),
@@ -111,8 +162,11 @@ mod tests {
             EventData::from_str("{}")?,
         );
         assert!(EventStream::new(vec![event1, event2]).is_err());
+        Ok(())
+    }
 
-        // ok (two events)
+    #[test]
+    fn new_test_ok_two_events() -> anyhow::Result<()> {
         let stream_id = EventStreamId::generate();
         let event1 = Event::new(
             EventId::generate(),
@@ -130,6 +184,55 @@ mod tests {
         assert_eq!(stream.id(), event1.stream_id());
         assert_eq!(stream.seq(), event2.stream_seq());
         assert_eq!(stream.events(), vec![event1, event2]);
+        Ok(())
+    }
+
+    #[test]
+    fn push_test() -> anyhow::Result<()> {
+        let mut stream = EventStream::generate(EventData::from_str("{}")?);
+        stream.push(EventData::from_str(r#"{"key":123}"#)?)?;
+        assert_eq!(stream.seq(), EventStreamSeq::from(2_u32));
+        Ok(())
+    }
+
+    #[test]
+    fn push_event_test() -> anyhow::Result<()> {
+        let mut stream = EventStream::generate(EventData::from_str("{}")?);
+        stream.push_event(Event::new(
+            EventId::generate(),
+            stream.id(),
+            EventStreamSeq::from(2_u32),
+            EventData::from_str(r#"{"key":123}"#)?,
+        ))?;
+        assert_eq!(stream.seq(), EventStreamSeq::from(2_u32));
+        Ok(())
+    }
+
+    #[test]
+    fn push_event_test_invalid_event_stream_id() -> anyhow::Result<()> {
+        let mut stream = EventStream::generate(EventData::from_str("{}")?);
+        assert!(stream
+            .push_event(Event::new(
+                EventId::generate(),
+                EventStreamId::generate(),
+                EventStreamSeq::from(2_u32),
+                EventData::from_str(r#"{"key":123}"#)?,
+            ))
+            .is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn push_event_test_invalid_event_stream_seq() -> anyhow::Result<()> {
+        let mut stream = EventStream::generate(EventData::from_str("{}")?);
+        assert!(stream
+            .push_event(Event::new(
+                EventId::generate(),
+                stream.id(),
+                EventStreamSeq::from(1_u32),
+                EventData::from_str(r#"{"key":123}"#)?,
+            ))
+            .is_err());
         Ok(())
     }
 }
