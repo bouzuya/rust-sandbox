82c5f4477afbe91937630e8dd4b24c8a0e63fcc0
bouzuya <m@bouzuya.net>
2022-09-24 15:23:42 +0900

    twiq: Add impl TryFrom<RawEvent> for UserRequestStarted

--- twiq/crates/domain/src/aggregate/user_request/event/user_request_started.rs
+++ twiq/crates/domain/src/aggregate/user_request/event/user_request_started.rs
@@ -2,10 +2,18 @@ use std::str::FromStr;
 
 use event_store_core::{
     event_id::EventId, event_stream_id::EventStreamId, event_stream_seq::EventStreamSeq,
-    Event as RawEvent, EventData, EventType,
+    Event as RawEvent, EventData, EventType as RawEventType,
 };
 
-use crate::value::At;
+use crate::{event::EventType, value::At};
+
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
+pub enum Error {
+    #[error("invalid type")]
+    InvalidType,
+    #[error("unknown {0}")]
+    Unknown(String),
+}
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct UserRequestStarted {
@@ -25,11 +33,15 @@ impl UserRequestStarted {
     ) -> Self {
         Self {
             id: id.to_string(),
-            r#type: crate::event::EventType::UserRequestStarted.to_string(),
+            r#type: Self::r#type().to_string(),
             at: at.to_string(),
             stream_id: stream_id.to_string(),
             stream_seq: u32::from(stream_seq),
         }
+    }
+
+    fn r#type() -> EventType {
+        EventType::UserRequestStarted
     }
 }
 
@@ -37,11 +49,24 @@ impl From<UserRequestStarted> for RawEvent {
     fn from(event: UserRequestStarted) -> Self {
         RawEvent::new(
             EventId::from_str(event.id.as_str()).expect("id"),
-            EventType::from(crate::event::EventType::UserRequestStarted),
+            RawEventType::from(UserRequestStarted::r#type()),
             EventStreamId::from_str(event.stream_id.as_str()).expect("stream_id"),
             EventStreamSeq::from(event.stream_seq),
             EventData::try_from(serde_json::to_string(&event).expect("event")).expect("data"),
         )
+    }
+}
+
+impl TryFrom<RawEvent> for UserRequestStarted {
+    type Error = Error;
+
+    fn try_from(raw_event: RawEvent) -> Result<Self, Self::Error> {
+        let event: Self = serde_json::from_str(raw_event.data().as_str())
+            .map_err(|e| Error::Unknown(e.to_string()))?;
+        if event.r#type != Self::r#type().to_string() {
+            return Err(Error::InvalidType);
+        }
+        Ok(event)
     }
 }
 
@@ -52,7 +77,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() -> anyhow::Result<()> {
+    fn json_conversion_test() -> anyhow::Result<()> {
         let o = UserRequestStarted {
             id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
             r#type: "user_request_started".to_owned(),
@@ -68,6 +93,38 @@ mod tests {
   "stream_seq": 1
 }"#;
         serde_test(o, s)?;
+        Ok(())
+    }
+
+    #[test]
+    fn raw_event_conversion_test() -> anyhow::Result<()> {
+        let o = UserRequestStarted {
+            id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
+            r#type: "user_request_started".to_owned(),
+            at: "2022-09-06T22:58:00.000000000Z".to_owned(),
+            stream_id: "a748c956-7e53-45ef-b1f0-1c52676a467c".to_owned(),
+            stream_seq: 1,
+        };
+        let e = RawEvent::new(
+            EventId::from_str("0ecb46f3-01a1-49b2-9405-0b4c40ecefe8")?,
+            RawEventType::from_str("user_request_started")?,
+            EventStreamId::from_str("a748c956-7e53-45ef-b1f0-1c52676a467c")?,
+            EventStreamSeq::from(1_u32),
+            EventData::try_from(serde_json::to_string(&serde_json::from_str::<
+                '_,
+                UserRequestStarted,
+            >(
+                r#"{
+  "id": "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8",
+  "type": "user_request_started",
+  "at": "2022-09-06T22:58:00.000000000Z",
+  "stream_id": "a748c956-7e53-45ef-b1f0-1c52676a467c",
+  "stream_seq": 1
+}"#,
+            )?)?)?,
+        );
+        assert_eq!(RawEvent::from(o.clone()), e);
+        assert_eq!(UserRequestStarted::try_from(e)?, o);
         Ok(())
     }
 }
