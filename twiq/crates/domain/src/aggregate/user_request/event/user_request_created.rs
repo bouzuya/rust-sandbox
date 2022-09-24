5686ee4f97b99443af96c15a53af5f822f7f27b3
bouzuya <m@bouzuya.net>
2022-09-24 15:16:35 +0900

    twiq: Add impl TryFrom<RawEvent> for UserRequestCreated

--- twiq/crates/domain/src/aggregate/user_request/event/user_request_created.rs
+++ twiq/crates/domain/src/aggregate/user_request/event/user_request_created.rs
@@ -2,10 +2,21 @@ use std::str::FromStr;
 
 use event_store_core::{
     event_id::EventId, event_stream_id::EventStreamId, event_stream_seq::EventStreamSeq,
-    Event as RawEvent, EventData, EventType,
+    Event as RawEvent, EventData, EventType as RawEventType,
 };
 
-use crate::value::{At, TwitterUserId, UserId, UserRequestId};
+use crate::{
+    event::EventType,
+    value::{At, TwitterUserId, UserId, UserRequestId},
+};
+
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
+pub enum Error {
+    #[error("invalid type")]
+    InvalidType,
+    #[error("unknown {0}")]
+    Unknown(String),
+}
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct UserRequestCreated {
@@ -29,7 +40,7 @@ impl UserRequestCreated {
     ) -> Self {
         Self {
             id: id.to_string(),
-            r#type: crate::event::EventType::UserRequestCreated.to_string(),
+            r#type: Self::r#type().to_string(),
             at: at.to_string(),
             stream_id: stream_id.to_string(),
             stream_seq: u32::from(stream_seq),
@@ -45,17 +56,34 @@ impl UserRequestCreated {
     pub fn user_request_id(&self) -> UserRequestId {
         UserRequestId::from_str(&self.stream_id).expect("user_request_id")
     }
+
+    fn r#type() -> EventType {
+        EventType::UserRequestCreated
+    }
 }
 
 impl From<UserRequestCreated> for RawEvent {
     fn from(event: UserRequestCreated) -> Self {
         RawEvent::new(
             EventId::from_str(event.id.as_str()).expect("id"),
-            EventType::from(crate::event::EventType::UserRequestCreated),
+            RawEventType::from(UserRequestCreated::r#type()),
             EventStreamId::from_str(event.stream_id.as_str()).expect("stream_id"),
             EventStreamSeq::from(event.stream_seq),
             EventData::try_from(serde_json::to_string(&event).expect("event")).expect("data"),
         )
+    }
+}
+
+impl TryFrom<RawEvent> for UserRequestCreated {
+    type Error = Error;
+
+    fn try_from(raw_event: RawEvent) -> Result<Self, Self::Error> {
+        let event: Self = serde_json::from_str(raw_event.data().as_str())
+            .map_err(|e| Error::Unknown(e.to_string()))?;
+        if event.r#type != Self::r#type().to_string() {
+            return Err(Error::InvalidType);
+        }
+        Ok(event)
     }
 }
 
@@ -69,7 +97,7 @@ mod tests {
     // TODO: test user_request_id
 
     #[test]
-    fn test() -> anyhow::Result<()> {
+    fn json_conversion_test() -> anyhow::Result<()> {
         let o = UserRequestCreated {
             id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
             r#type: "user_request_created".to_owned(),
@@ -89,6 +117,42 @@ mod tests {
   "user_id": "682106dd-b94c-4bd1-a808-e74b3d3fb56a"
 }"#;
         serde_test(o, s)?;
+        Ok(())
+    }
+
+    #[test]
+    fn raw_event_conversion_test() -> anyhow::Result<()> {
+        let o = UserRequestCreated {
+            id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
+            r#type: "user_request_created".to_owned(),
+            at: "2022-09-06T22:58:00.000000000Z".to_owned(),
+            stream_id: "a748c956-7e53-45ef-b1f0-1c52676a467c".to_owned(),
+            stream_seq: 1,
+            twitter_user_id: "twitter_user_id1".to_owned(),
+            user_id: "682106dd-b94c-4bd1-a808-e74b3d3fb56a".to_owned(),
+        };
+        let e = RawEvent::new(
+            EventId::from_str("0ecb46f3-01a1-49b2-9405-0b4c40ecefe8")?,
+            RawEventType::from_str("user_request_created")?,
+            EventStreamId::from_str("a748c956-7e53-45ef-b1f0-1c52676a467c")?,
+            EventStreamSeq::from(1_u32),
+            EventData::try_from(serde_json::to_string(&serde_json::from_str::<
+                '_,
+                UserRequestCreated,
+            >(
+                r#"{
+  "id": "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8",
+  "type": "user_request_created",
+  "at": "2022-09-06T22:58:00.000000000Z",
+  "stream_id": "a748c956-7e53-45ef-b1f0-1c52676a467c",
+  "stream_seq": 1,
+  "twitter_user_id": "twitter_user_id1",
+  "user_id": "682106dd-b94c-4bd1-a808-e74b3d3fb56a"
+}"#,
+            )?)?)?,
+        );
+        assert_eq!(RawEvent::from(o.clone()), e);
+        assert_eq!(UserRequestCreated::try_from(e)?, o);
         Ok(())
     }
 }
