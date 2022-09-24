2c08409015e930d08678cc05624c7582ee09aa20
bouzuya <m@bouzuya.net>
2022-09-24 09:52:39 +0900

    twiq: Add UserRequestCreated::r#type

--- twiq/crates/domain/src/aggregate/user_request/event/user_request_created.rs
+++ twiq/crates/domain/src/aggregate/user_request/event/user_request_created.rs
@@ -10,6 +10,7 @@ use crate::value::{At, TwitterUserId, UserId, UserRequestId};
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct UserRequestCreated {
     pub(super) id: String,
+    pub(super) r#type: String,
     pub(super) at: String,
     pub(super) stream_id: String,
     pub(super) stream_seq: u32,
@@ -28,6 +29,7 @@ impl UserRequestCreated {
     ) -> Self {
         Self {
             id: id.to_string(),
+            r#type: crate::event::EventType::UserRequestCreated.to_string(),
             at: at.to_string(),
             stream_id: stream_id.to_string(),
             stream_seq: u32::from(stream_seq),
@@ -70,6 +72,7 @@ mod tests {
     fn test() -> anyhow::Result<()> {
         let o = UserRequestCreated {
             id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
+            r#type: "user_request_created".to_owned(),
             at: "2022-09-06T22:58:00.000000000Z".to_owned(),
             stream_id: "a748c956-7e53-45ef-b1f0-1c52676a467c".to_owned(),
             stream_seq: 1,
@@ -78,6 +81,7 @@ mod tests {
         };
         let s = r#"{
   "id": "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8",
+  "type": "user_request_created",
   "at": "2022-09-06T22:58:00.000000000Z",
   "stream_id": "a748c956-7e53-45ef-b1f0-1c52676a467c",
   "stream_seq": 1,
