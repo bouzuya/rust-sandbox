6ed9ac68413e77da6bc113cc55ef8b2f24ccd9bf
bouzuya <m@bouzuya.net>
2022-09-24 15:20:49 +0900

    twiq: Add impl TryFrom<RawEvent> for UserRequestFinished

--- twiq/crates/domain/src/aggregate/user_request/event/user_request_finished.rs
+++ twiq/crates/domain/src/aggregate/user_request/event/user_request_finished.rs
@@ -2,13 +2,22 @@ use std::str::FromStr;
 
 use event_store_core::{
     event_id::EventId, event_stream_id::EventStreamId, event_stream_seq::EventStreamSeq,
-    Event as RawEvent, EventData, EventType,
+    Event as RawEvent, EventData, EventType as RawEventType,
 };
 
 use crate::{
     aggregate::user_request::value::user_response::UserResponse,
+    event::EventType,
     value::{At, UserId, UserRequestId},
 };
+
+#[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
+pub enum Error {
+    #[error("invalid type")]
+    InvalidType,
+    #[error("unknown {0}")]
+    Unknown(String),
+}
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct UserRequestFinished {
@@ -33,7 +42,7 @@ impl UserRequestFinished {
     ) -> Self {
         Self {
             id: id.to_string(),
-            r#type: crate::event::EventType::UserRequestFinished.to_string(),
+            r#type: Self::r#type().to_string(),
             at: at.to_string(),
             stream_id: stream_id.to_string(),
             stream_seq: u32::from(stream_seq),
@@ -58,17 +67,34 @@ impl UserRequestFinished {
     pub fn user_response(&self) -> UserResponse {
         UserResponse::new(self.status_code, self.response_body.clone())
     }
+
+    fn r#type() -> EventType {
+        EventType::UserRequestFinished
+    }
 }
 
 impl From<UserRequestFinished> for RawEvent {
     fn from(event: UserRequestFinished) -> Self {
         RawEvent::new(
             EventId::from_str(event.id.as_str()).expect("id"),
-            EventType::from(crate::event::EventType::UserRequestFinished),
+            RawEventType::from(UserRequestFinished::r#type()),
             EventStreamId::from_str(event.stream_id.as_str()).expect("stream_id"),
             EventStreamSeq::from(event.stream_seq),
             EventData::try_from(serde_json::to_string(&event).expect("event")).expect("data"),
         )
+    }
+}
+
+impl TryFrom<RawEvent> for UserRequestFinished {
+    type Error = Error;
+
+    fn try_from(raw_event: RawEvent) -> Result<Self, Self::Error> {
+        let event: Self = serde_json::from_str(raw_event.data().as_str())
+            .map_err(|e| Error::Unknown(e.to_string()))?;
+        if event.r#type != Self::r#type().to_string() {
+            return Err(Error::InvalidType);
+        }
+        Ok(event)
     }
 }
 
@@ -83,7 +109,7 @@ mod tests {
     // TODO: test user_response
 
     #[test]
-    fn test() -> anyhow::Result<()> {
+    fn json_conversion_test() -> anyhow::Result<()> {
         let o = UserRequestFinished {
             id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
             r#type: "user_request_finished".to_owned(),
@@ -105,6 +131,44 @@ mod tests {
   "response_body": "{}"
 }"#;
         serde_test(o, s)?;
+        Ok(())
+    }
+
+    #[test]
+    fn raw_event_conversion_test() -> anyhow::Result<()> {
+        let o = UserRequestFinished {
+            id: "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8".to_owned(),
+            r#type: "user_request_finished".to_owned(),
+            at: "2022-09-06T22:58:00.000000000Z".to_owned(),
+            stream_id: "a748c956-7e53-45ef-b1f0-1c52676a467c".to_owned(),
+            stream_seq: 1,
+            user_id: "5464979d-8c47-47c7-9066-4cfee838c518".to_owned(),
+            status_code: 200,
+            response_body: "{}".to_owned(),
+        };
+        let e = RawEvent::new(
+            EventId::from_str("0ecb46f3-01a1-49b2-9405-0b4c40ecefe8")?,
+            RawEventType::from_str("user_request_finished")?,
+            EventStreamId::from_str("a748c956-7e53-45ef-b1f0-1c52676a467c")?,
+            EventStreamSeq::from(1_u32),
+            EventData::try_from(serde_json::to_string(&serde_json::from_str::<
+                '_,
+                UserRequestFinished,
+            >(
+                r#"{
+  "id": "0ecb46f3-01a1-49b2-9405-0b4c40ecefe8",
+  "type": "user_request_finished",
+  "at": "2022-09-06T22:58:00.000000000Z",
+  "stream_id": "a748c956-7e53-45ef-b1f0-1c52676a467c",
+  "stream_seq": 1,
+  "user_id": "5464979d-8c47-47c7-9066-4cfee838c518",
+  "status_code": 200,
+  "response_body": "{}"
+}"#,
+            )?)?)?,
+        );
+        assert_eq!(RawEvent::from(o.clone()), e);
+        assert_eq!(UserRequestFinished::try_from(e)?, o);
         Ok(())
     }
 }
