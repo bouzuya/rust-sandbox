0928ecdd6a6d4dc2b77175babf70b1058d2ec95a
bouzuya <m@bouzuya.net>
2022-10-10 10:51:29 +0900

    twiq: Add request_user test

--- twiq/crates/use_case/src/command/request_user.rs
+++ twiq/crates/use_case/src/command/request_user.rs
@@ -36,16 +36,56 @@ pub async fn handler<C: Context>(context: &C, command: Command) -> Result<()> {
     let found = user_repository
         .find_by_twitter_user_id(&twitter_user_id)
         .await?;
-    let updated = match found.clone() {
+    let mut updated = match found.clone() {
         None => User::create(twitter_user_id)?,
-        Some(mut user) => {
-            // TODO: &mut self -> &self
-            user.request(At::now())?;
-            user
-        }
+        Some(user) => user.clone(),
     };
+    // TODO: &mut self -> &self
+    updated.request(At::now())?;
     user_repository.store(found, updated).await?;
     Ok(())
 }
 
-// TODO: test
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use anyhow::Context;
+
+    use crate::in_memory_user_repository::InMemoryUserRepository;
+
+    use super::*;
+
+    #[derive(Debug, Default)]
+    struct MockApp {
+        user_repository: InMemoryUserRepository,
+    }
+
+    impl HasUserRepository for MockApp {
+        type UserRepository = InMemoryUserRepository;
+
+        fn user_repository(&self) -> &Self::UserRepository {
+            &self.user_repository
+        }
+    }
+
+    impl Has for MockApp {}
+
+    #[tokio::test]
+    async fn test() -> anyhow::Result<()> {
+        let twitter_user_id = TwitterUserId::from_str("125962981")?;
+        let context = MockApp::default();
+        context
+            .request_user(Command {
+                twitter_user_id: twitter_user_id.clone(),
+            })
+            .await?;
+        let user = context
+            .user_repository
+            .find_by_twitter_user_id(&twitter_user_id)
+            .await?
+            .context("user not found")?;
+        assert_eq!(user.twitter_user_id(), &twitter_user_id);
+        Ok(())
+    }
+}
