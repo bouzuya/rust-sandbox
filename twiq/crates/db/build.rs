eefdc9d1ed8f29cdabf5a8392800956bd07b110f
bouzuya <m@bouzuya.net>
2022-10-21 05:51:19 +0900

    twiq: Add db::firestore_rpc mod

--- twiq/crates/db/build.rs
+++ twiq/crates/db/build.rs
@@ -2,7 +2,7 @@ use std::{fs, path::Path};
 
 fn main() {
     // brew install protobuf
-    let out_dir = Path::new("src/firestore_rpc/gen");
+    let out_dir = Path::new("src/firestore_rpc");
     if out_dir.exists() {
         fs::remove_dir_all(&out_dir).unwrap();
     }
@@ -10,7 +10,7 @@ fn main() {
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .out_dir("src/firestore_rpc/gen")
+        .out_dir(out_dir)
         .compile(
             &["googleapis/google/firestore/v1/firestore.proto"],
             &["googleapis/"],
--- twiq/crates/db/src/firestore_rpc.rs
+++ twiq/crates/db/src/firestore_rpc.rs
@@ -1,1 +1,19 @@
-
+pub mod google {
+    pub mod api {
+        include!("firestore_rpc/google.api.rs");
+    }
+    pub mod firestore {
+        pub mod v1 {
+            include!("firestore_rpc/google.firestore.v1.rs");
+        }
+    }
+    pub mod protobuf {
+        include!("firestore_rpc/google.protobuf.rs");
+    }
+    pub mod r#type {
+        include!("firestore_rpc/google.r#type.rs");
+    }
+    pub mod rpc {
+        include!("firestore_rpc/google.rpc.rs");
+    }
+}
