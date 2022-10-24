b5265434d173186d4ef08035faf728008239fd4b
bouzuya <m@bouzuya.net>
2022-10-25 06:37:37 +0900

    twiq: Add firestore_rpc::helper::value_from_string

--- twiq/crates/db/src/firestore_rpc.rs
+++ twiq/crates/db/src/firestore_rpc.rs
@@ -17,3 +17,28 @@ pub mod google {
         include!("firestore_rpc/google.rpc.rs");
     }
 }
+
+pub mod helper {
+    use super::google::firestore::v1::{value::ValueType, Value};
+
+    pub fn value_from_string(s: String) -> Value {
+        Value {
+            value_type: Some(ValueType::StringValue(s)),
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test() {
+            assert_eq!(
+                value_from_string("abc".to_owned()),
+                Value {
+                    value_type: Some(ValueType::StringValue("abc".to_owned()))
+                }
+            );
+        }
+    }
+}
