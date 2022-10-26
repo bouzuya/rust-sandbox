b96dc52bef73465655eeb23abeccd6dfbe380b89
bouzuya <m@bouzuya.net>
2022-10-27 07:05:30 +0900

    twiq: Fix crates/db/build.rs

--- twiq/crates/db/build.rs
+++ twiq/crates/db/build.rs
@@ -1,5 +1,7 @@
 use std::{fs, path::Path};
 
+use prost_build::Config;
+
 fn main() {
     // brew install protobuf
     let out_dir = Path::new("src/firestore_rpc");
@@ -7,11 +9,17 @@ fn main() {
         fs::remove_dir_all(&out_dir).unwrap();
     }
     fs::create_dir_all(&out_dir).unwrap();
+    let mut config = Config::new();
+    config.disable_comments(&[
+        "google.api.HttpRule",
+        "google.firestore.v1.StructuredQuery.start_at",
+    ]);
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
         .out_dir(out_dir)
-        .compile(
+        .compile_with_config(
+            config,
             &["proto/googleapis/google/firestore/v1/firestore.proto"],
             &["proto/googleapis"],
         )
