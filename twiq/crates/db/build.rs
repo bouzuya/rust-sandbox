4e67f62a3d259f0c58ac6cb879b75ce0bb00d045
bouzuya <m@bouzuya.net>
2022-10-27 22:48:50 +0900

    twiq: Fix doc-comments errors

--- twiq/crates/db/build.rs
+++ twiq/crates/db/build.rs
@@ -11,13 +11,20 @@ fn main() {
     fs::create_dir_all(&out_dir).unwrap();
     let mut config = Config::new();
     config.disable_comments(&[
+        // message や field は消せるが service や rpc は消せない
+        // 前者は prost-build crate の範囲、後者は tonic-build crate の範囲
         "google.api.HttpRule",
         "google.firestore.v1.StructuredQuery.start_at",
+        "google.firestore.v1.StructuredAggregationQuery.Aggregation.alias",
+        "google.firestore.v1.StructuredAggregationQuery.Aggregation.Count.up_to",
     ]);
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
         .out_dir(out_dir)
+        // service および rpc の comments 出力の廃止
+        .disable_comments("google.firestore.v1.Firestore")
+        .disable_comments("google.firestore.v1.Firestore.RunAggregationQuery")
         .compile_with_config(
             config,
             &["proto/googleapis/google/firestore/v1/firestore.proto"],
