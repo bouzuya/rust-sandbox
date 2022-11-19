edf3a6930c44b9ac9deccf64a94acf6b4c560c2a
bouzuya <m@bouzuya.net>
2022-11-19 22:58:18 +0900

    twiq: Fix firestore_user_store error handling

--- twiq/crates/db/src/firestore_user_store.rs
+++ twiq/crates/db/src/firestore_user_store.rs
@@ -12,6 +12,22 @@ use crate::{
 };
 use query_handler::{user::User, user_store};
 
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("firestore_rpc::helper {0}")]
+    FirestoreRpcHelper(#[from] crate::firestore_rpc::helper::Error),
+    #[error("firestore_rpc::helper::GetFieldError {0}")]
+    FirestoreRpcHelperGetField(#[from] crate::firestore_rpc::helper::GetFieldError),
+}
+
+impl From<Error> for user_store::Error {
+    fn from(e: Error) -> Self {
+        user_store::Error::Unknown(e.to_string())
+    }
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
 pub struct FirestoreUserStore {
     config: Config,
 }
@@ -20,22 +36,14 @@ impl FirestoreUserStore {
     const QUERY_USERS: &'static str = "query_users";
     const QUERY_TWITTER_USER_IDS: &'static str = "query_twitter_user_ids";
 
-    async fn begin_transaction(&self) -> user_store::Result<FirestoreTransaction> {
+    async fn begin_transaction(&self) -> Result<FirestoreTransaction> {
         let (project_id, database_id) = (self.config.project_id(), self.config.database_id());
         let transaction =
-            FirestoreTransaction::begin(project_id.to_owned(), database_id.to_owned())
-                .await
-                .map_err(|e| user_store::Error::Unknown(e.to_string()))?;
+            FirestoreTransaction::begin(project_id.to_owned(), database_id.to_owned()).await?;
         Ok(transaction)
     }
-}
 
-#[async_trait]
-impl user_store::UserStore for FirestoreUserStore {
-    async fn find_by_twitter_user_id(
-        &self,
-        twitter_user_id: &String,
-    ) -> user_store::Result<Option<User>> {
+    async fn find_by_twitter_user_id(&self, twitter_user_id: &String) -> Result<Option<User>> {
         let transaction = self.begin_transaction().await?;
 
         // get user_id
@@ -46,8 +54,7 @@ impl user_store::UserStore for FirestoreUserStore {
             Ok(None) => return Ok(None),
             Ok(Some(doc)) => Ok(doc),
             Err(e) => Err(e),
-        }
-        .map_err(|e| user_store::Error::Unknown(e.to_string()))?;
+        }?;
         let user_id = get_field_as_str(&document, "user_id").expect("user_id not found");
 
         // get user
@@ -55,23 +62,16 @@ impl user_store::UserStore for FirestoreUserStore {
             Ok(None) => return Ok(None),
             Ok(Some(doc)) => Ok(doc),
             Err(e) => Err(e),
-        }
-        .map_err(|e| user_store::Error::Unknown(e.to_string()))?;
+        }?;
         let query_user = User {
-            user_id: get_field_as_str(&document, "user_id")
-                .expect("user_id not found")
-                .to_owned(),
-            twitter_user_id: get_field_as_str(&document, "twitter_user_id")
-                .expect("twitter_user_id not found")
-                .to_owned(),
-            twitter_user_name: get_field_as_str(&document, "twitter_user_name")
-                .expect("twitter_user_name not found")
-                .to_owned(),
+            user_id: get_field_as_str(&document, "user_id")?.to_owned(),
+            twitter_user_id: get_field_as_str(&document, "twitter_user_id")?.to_owned(),
+            twitter_user_name: get_field_as_str(&document, "twitter_user_name")?.to_owned(),
         };
         Ok(Some(query_user))
     }
 
-    async fn store(&self, _before: Option<User>, after: User) -> user_store::Result<()> {
+    async fn store(&self, _before: Option<User>, after: User) -> Result<()> {
         let transaction = self.begin_transaction().await?;
 
         // store query_twitter_user_id
@@ -95,8 +95,7 @@ impl user_store::UserStore for FirestoreUserStore {
                 current_document: None,
                 operation: Some(Operation::Update(document)),
             })
-            .await
-            .map_err(|e| user_store::Error::Unknown(e.to_string()))?;
+            .await?;
 
         // store query_user
         let document = Document {
@@ -124,13 +123,23 @@ impl user_store::UserStore for FirestoreUserStore {
                 current_document: None,
                 operation: Some(Operation::Update(document)),
             })
-            .await
-            .map_err(|e| user_store::Error::Unknown(e.to_string()))?;
+            .await?;
 
-        transaction
-            .commit()
-            .await
-            .map_err(|e| user_store::Error::Unknown(e.to_string()))?;
+        transaction.commit().await?;
         Ok(())
+    }
+}
+
+#[async_trait]
+impl user_store::UserStore for FirestoreUserStore {
+    async fn find_by_twitter_user_id(
+        &self,
+        twitter_user_id: &String,
+    ) -> user_store::Result<Option<User>> {
+        Ok(self.find_by_twitter_user_id(twitter_user_id).await?)
+    }
+
+    async fn store(&self, before: Option<User>, after: User) -> user_store::Result<()> {
+        Ok(self.store(before, after).await?)
     }
 }
