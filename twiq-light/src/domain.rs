bf986a05c1b6467b00d6cabe8372dedb92764d78
bouzuya <m@bouzuya.net>
2022-12-09 07:27:14 +0900

    twiq-light: Add queue example

--- twiq-light/src/domain.rs
+++ twiq-light/src/domain.rs
@@ -4,3 +4,30 @@ pub struct MyTweet {
     pub at: String,
     pub text: String,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::VecDeque;
+
+    #[test]
+    fn test() {
+        let mut queue = VecDeque::new();
+
+        queue.push_back("item1");
+        queue.push_back("item2");
+        queue.push_back("item3");
+
+        assert_eq!(
+            queue
+                .iter()
+                .copied()
+                .collect::<Vec<&'static str>>()
+                .join("\n"),
+            "item1\nitem2\nitem3\n"
+        );
+
+        assert_eq!(queue.pop_front(), Some("item1"));
+        assert_eq!(queue.pop_front(), Some("item2"));
+        assert_eq!(queue.pop_front(), Some("item3"));
+    }
+}
