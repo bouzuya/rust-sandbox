b0ff4568c93a80587a961486441821fa840abd77
bouzuya <m@bouzuya.net>
2022-12-14 07:28:21 +0900

    twiq-light: Add impl post_tweet

--- twiq-light/src/dequeue.rs
+++ twiq-light/src/dequeue.rs
@@ -1,14 +1,30 @@
+use std::env;
+
+use reqwest::{Client, Method};
+use tracing::debug;
+
 use crate::{domain::ScheduledTweet, store::TweetQueueStore};
 
-async fn post_tweet(tweet: ScheduledTweet) -> anyhow::Result<()> {
-    println!("TODO: post {:?}", tweet);
-    Ok(())
+async fn post_tweet(bearer_token: &str, tweet: ScheduledTweet) -> anyhow::Result<()> {
+    let url = "https://api.twitter.com/2/tweets";
+    let response = Client::builder()
+        .build()?
+        .request(Method::POST, url)
+        .bearer_auth(bearer_token)
+        .json(&tweet)
+        .send()
+        .await?;
+
+    debug!("response.status={:?}", response.status());
+    Ok(response.json().await?)
 }
 
 pub async fn run(store: TweetQueueStore) -> anyhow::Result<()> {
+    let bearer_token = env::var("TWITTER_BEARER_TOKEN")?;
+    debug!(bearer_token);
     let mut queue = store.read_all().await?;
     if let Some(item) = queue.pop_front() {
-        post_tweet(item).await?;
+        post_tweet(&bearer_token, item).await?;
         store.write_all(&queue).await?;
     }
     Ok(())
