33b740098044c069bb0abaf782ea9fa71c6b0de2
bouzuya <m@bouzuya.net>
2022-12-13 22:21:36 +0900

    twiq-light: Add Resource

--- twiq-light/src/main.rs
+++ twiq-light/src/main.rs
@@ -15,17 +15,29 @@ mod store;
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[command(subcommand)]
-    subcommand: Subcommand,
+    resource: Resource,
 }
 
-#[derive(Debug, clap::Subcommand)]
-enum Subcommand {
+#[derive(Clone, Debug, clap::Subcommand)]
+enum Resource {
+    #[clap(subcommand)]
+    Queue(QueueSubcommand),
+    #[clap(subcommand)]
+    Tweet(TweetSubcommand),
+}
+
+#[derive(Clone, Debug, clap::Subcommand)]
+enum QueueSubcommand {
     Dequeue,
     Enqueue { tweet: String },
+    List,
+    Reorder { src: usize, dst: usize },
+}
+
+#[derive(Clone, Debug, clap::Subcommand)]
+enum TweetSubcommand {
     Fetch,
     Import { file: String },
-    ListQueue,
-    Reorder { src: usize, dst: usize },
     Search { query: Option<String> },
 }
 
@@ -35,13 +47,17 @@ async fn main() -> anyhow::Result<()> {
     let queue_store = TweetQueueStore::default();
     let store = TweetStore::default();
     let args = <Args as clap::Parser>::parse();
-    match args.subcommand {
-        Subcommand::Dequeue => dequeue::run(queue_store).await,
-        Subcommand::Enqueue { tweet } => enqueue::run(queue_store, tweet).await,
-        Subcommand::Fetch => fetch::run(store).await,
-        Subcommand::Import { file } => import::run(store, file).await,
-        Subcommand::ListQueue => list_queue::run(queue_store).await,
-        Subcommand::Reorder { src, dst } => reorder::run(queue_store, src, dst).await,
-        Subcommand::Search { query } => search::run(store, query).await,
+    match args.resource {
+        Resource::Queue(command) => match command {
+            QueueSubcommand::Dequeue => dequeue::run(queue_store).await,
+            QueueSubcommand::Enqueue { tweet } => enqueue::run(queue_store, tweet).await,
+            QueueSubcommand::List => list_queue::run(queue_store).await,
+            QueueSubcommand::Reorder { src, dst } => reorder::run(queue_store, src, dst).await,
+        },
+        Resource::Tweet(command) => match command {
+            TweetSubcommand::Fetch => fetch::run(store).await,
+            TweetSubcommand::Import { file } => import::run(store, file).await,
+            TweetSubcommand::Search { query } => search::run(store, query).await,
+        },
     }
 }
