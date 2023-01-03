232b8e7a4e2c828ed0a00581e8ecc4ba841d4589
bouzuya <m@bouzuya.net>
2023-01-03 10:59:35 +0900

    date-to-week-date: Fix create-service-account-and-key shell script

--- date-to-week-date/scripts/create-service-account-and-key.sh
+++ date-to-week-date/scripts/create-service-account-and-key.sh
@@ -8,6 +8,13 @@ description=
 display_name="${service_account_name}"
 key_file="${service_account_name}.json"
 
+if [ -z "${project_id}" ] || [ -z "${service_account_name}" ]
+then
+  exit 1
+fi
+
+"$(dirname "${0}")/enable-iam-api.sh"
+
 gcloud iam service-accounts create "${service_account_name}" \
     --description="${description}" \
     --display-name="${display_name}"
