65575d37e0822f80d0c2480722b580d75dd77d8d
bouzuya <m@bouzuya.net>
2023-01-11 07:09:13 +0900

    sitemaps: Add SitemapWriter

--- sitemaps/src/writer.rs
+++ sitemaps/src/writer.rs
@@ -1,11 +1,76 @@
+use std::io::Write;
+
+use quick_xml::{
+    events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event},
+    Writer,
+};
+
+// TODO: improve error
+#[derive(Clone, Debug, thiserror::Error)]
+#[error("error")]
+pub struct Error;
+
+impl From<quick_xml::Error> for Error {
+    fn from(_value: quick_xml::Error) -> Self {
+        Self
+    }
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+pub struct SitemapWriter<W: Write>(Writer<W>);
+
+impl<W: Write> SitemapWriter<W> {
+    pub fn new(inner: W) -> Result<Self> {
+        let mut writer = Writer::new(inner);
+        writer.write_event(Event::Decl(BytesDecl::new("1.0", Some("UTF-8"), None)))?;
+        let mut elm = BytesStart::new("urlset");
+        elm.push_attribute(("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9"));
+        writer.write_event(Event::Start(elm))?;
+        Ok(Self(writer))
+    }
+
+    pub fn write(&mut self, url_loc: &str) -> Result<()> {
+        self.0.write_event(Event::Start(BytesStart::new("url")))?;
+        self.0.write_event(Event::Start(BytesStart::new("loc")))?;
+        self.0.write_event(Event::Text(BytesText::new(url_loc)))?;
+        self.0.write_event(Event::End(BytesEnd::new("loc")))?;
+        self.0.write_event(Event::End(BytesEnd::new("url")))?;
+        Ok(())
+    }
+
+    pub fn into_inner(mut self) -> Result<W> {
+        self.0.write_event(Event::End(BytesEnd::new("urlset")))?;
+        Ok(self.0.into_inner())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use quick_xml::events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event};
     use quick_xml::writer::Writer;
     use std::io::Cursor;
 
     #[test]
     fn test() -> anyhow::Result<()> {
+        let mut writer = SitemapWriter::new(Cursor::new(Vec::new()))?;
+        writer.write("http://www.example.com/")?;
+        let result = writer.into_inner()?.into_inner();
+        let expected = concat!(
+            r#"<?xml version="1.0" encoding="UTF-8"?>"#,
+            r#"<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"#,
+            r#"<url>"#,
+            r#"<loc>http://www.example.com/</loc>"#,
+            r#"</url>"#,
+            r#"</urlset>"#
+        );
+        assert_eq!(result, expected.as_bytes());
+        Ok(())
+    }
+
+    #[test]
+    fn test_quick_xml() -> anyhow::Result<()> {
         let mut writer = Writer::new(Cursor::new(Vec::new()));
         writer.write_event(Event::Decl(BytesDecl::new("1.0", Some("UTF-8"), None)))?;
         let mut elm = BytesStart::new("urlset");
