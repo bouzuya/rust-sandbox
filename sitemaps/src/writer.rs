7c81170107cc953b7c3929d15e3a6c914745ee3d
bouzuya <m@bouzuya.net>
2023-01-11 22:01:00 +0900

    sitemaps: Remove quick-xml example

--- sitemaps/src/writer.rs
+++ sitemaps/src/writer.rs
@@ -149,8 +149,6 @@ impl<'a> UrlBuilder<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use quick_xml::events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event};
-    use quick_xml::writer::Writer;
     use std::io::Cursor;
 
     #[test]
@@ -305,34 +303,6 @@ mod tests {
             r#"</urlset>"#
         );
         assert_eq!(actual, expected);
-        Ok(())
-    }
-
-    #[test]
-    fn test_quick_xml() -> anyhow::Result<()> {
-        let mut writer = Writer::new(Cursor::new(Vec::new()));
-        writer.write_event(Event::Decl(BytesDecl::new("1.0", Some("UTF-8"), None)))?;
-        let mut elm = BytesStart::new("urlset");
-        elm.push_attribute(("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9"));
-        writer.write_event(Event::Start(elm))?;
-
-        writer.write_event(Event::Start(BytesStart::new("url")))?;
-        writer.write_event(Event::Start(BytesStart::new("loc")))?;
-        writer.write_event(Event::Text(BytesText::new("http://www.example.com/")))?;
-        writer.write_event(Event::End(BytesEnd::new("loc")))?;
-        writer.write_event(Event::End(BytesEnd::new("url")))?;
-
-        writer.write_event(Event::End(BytesEnd::new("urlset")))?;
-        let result = writer.into_inner().into_inner();
-        let expected = concat!(
-            r#"<?xml version="1.0" encoding="UTF-8"?>"#,
-            r#"<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"#,
-            r#"<url>"#,
-            r#"<loc>http://www.example.com/</loc>"#,
-            r#"</url>"#,
-            r#"</urlset>"#
-        );
-        assert_eq!(result, expected.as_bytes());
         Ok(())
     }
 }
