76ab23c47846878f1c5c43d9f0bfdf9dd18ba293
bouzuya <m@bouzuya.net>
2023-01-18 22:59:30 +0900

    sitemap-xml: Add time feature

--- sitemap-xml/src/writer/lastmod.rs
+++ sitemap-xml/src/writer/lastmod.rs
@@ -1,5 +1,8 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::borrow::Cow;
 
+#[cfg(feature = "time")]
 use time::format_description::well_known::Iso8601;
 
 // TODO: Error
@@ -21,16 +24,25 @@ impl<'a> TryFrom<&'a str> for Lastmod<'a> {
     type Error = Error;
 
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        if value.len() == 10 {
-            let format = time::macros::format_description!("[year]-[month]-[day]");
-            time::Date::parse(value, &format).map_err(|_| Error)?;
-        } else {
-            time::OffsetDateTime::parse(value, &Iso8601::DEFAULT).map_err(|_| Error)?;
+        // <https://www.w3.org/TR/xmlschema11-2/#date>
+        // <https://www.w3.org/TR/xmlschema11-2/#dateTime>
+        lazy_static! {
+            static ref DATE_RE: Regex = Regex::new(
+                r#"\A-?([1-9][0-9]{3,}|0[0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?\z"#
+            ).unwrap();
+
+            static ref DATE_TIME_RE: Regex = Regex::new(
+                r#"\A-?([1-9][0-9]{3,}|0[0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]+)?|(24:00:00(\.0+)?))(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?\z"#
+            ).unwrap();
+        }
+        if !DATE_RE.is_match(value) && !DATE_TIME_RE.is_match(value) {
+            return Err(Error);
         }
         Ok(Self(Cow::Borrowed(value)))
     }
 }
 
+#[cfg(feature = "time")]
 impl<'a> TryFrom<time::Date> for Lastmod<'a> {
     type Error = Error;
 
@@ -41,6 +53,7 @@ impl<'a> TryFrom<time::Date> for Lastmod<'a> {
     }
 }
 
+#[cfg(feature = "time")]
 impl<'a> TryFrom<time::OffsetDateTime> for Lastmod<'a> {
     type Error = Error;
 
@@ -64,6 +77,7 @@ mod tests {
         Ok(())
     }
 
+    #[cfg(feature = "time")]
     #[test]
     fn test_date() -> anyhow::Result<()> {
         #[rustfmt::skip]
@@ -72,6 +86,7 @@ mod tests {
         Ok(())
     }
 
+    #[cfg(feature = "time")]
     #[test]
     fn test_date_time() -> anyhow::Result<()> {
         #[rustfmt::skip]
