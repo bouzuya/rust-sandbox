39b49d5717b9b3bc1dd45a2a42ab51c58bdcab1a
bouzuya <m@bouzuya.net>
2023-01-22 13:37:55 +0900

    bbn: Update json command to write /tags.json

--- bbn/crates/bbn/src/command/json.rs
+++ bbn/crates/bbn/src/command/json.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
 use bbn_repository::{BbnRepository, Query};
 use std::{
+    collections::BTreeMap,
     convert::TryFrom,
     fs::{self, File},
     io::BufWriter,
@@ -24,11 +25,30 @@ pub struct AllJsonItem {
     pub title: String,
 }
 
+// <https://github.com/bouzuya/kraken/tree/v4.0.2/doc#tags-json>
+// tags json (`/tags.json`)
+#[derive(serde::Serialize)]
+pub struct TagsJson(Vec<TagsJsonItem>);
+
+#[derive(serde::Serialize)]
+pub struct TagsJsonItem {
+    pub name: String,
+    pub count: u32,
+}
+
 fn write_all_json(out_dir: &Path, all_json: &AllJson) -> anyhow::Result<()> {
-    let all_json_path = out_dir.join("posts.json");
-    let all_json_file = File::create(all_json_path)?;
-    let writer = BufWriter::new(all_json_file);
+    let path = out_dir.join("posts.json");
+    let file = File::create(path)?;
+    let writer = BufWriter::new(file);
     serde_json::to_writer(writer, all_json)?;
+    Ok(())
+}
+
+fn write_tags_json(out_dir: &Path, tags_json: &TagsJson) -> anyhow::Result<()> {
+    let path = out_dir.join("tags.json");
+    let file = File::create(path)?;
+    let writer = BufWriter::new(file);
+    serde_json::to_writer(writer, tags_json)?;
     Ok(())
 }
 
@@ -44,12 +64,18 @@ pub fn run(out_dir: PathBuf) -> anyhow::Result<()> {
     let mut entry_ids = bbn_repository.find_ids_by_query(query)?;
     entry_ids.sort();
 
-    let mut items = vec![];
+    let mut all_json_items = vec![];
+    let mut tag_count_map = BTreeMap::new();
     for entry_id in entry_ids {
         let meta = bbn_repository
             .find_meta_by_id(&entry_id)?
             .context("meta not found")?;
-        let item = AllJsonItem {
+
+        for name in meta.tags.clone() {
+            *tag_count_map.entry(name).or_insert(0) += 1;
+        }
+
+        let all_json_item = AllJsonItem {
             date: entry_id.date().to_string(),
             id_title: entry_id.id_title().map(|s| s.to_owned()),
             minutes: u32::try_from(meta.minutes)?,
@@ -57,11 +83,20 @@ pub fn run(out_dir: PathBuf) -> anyhow::Result<()> {
             tags: meta.tags,
             title: meta.title,
         };
-        items.push(item);
+        all_json_items.push(all_json_item);
     }
-    let all_json = AllJson(items);
+
+    let tags_json = TagsJson(
+        tag_count_map
+            .into_iter()
+            .map(|(name, count)| TagsJsonItem { name, count })
+            .collect::<Vec<_>>(),
+    );
+
+    let all_json = AllJson(all_json_items);
 
     fs::create_dir_all(out_dir.as_path())?;
     write_all_json(out_dir.as_path(), &all_json)?;
+    write_tags_json(out_dir.as_path(), &tags_json)?;
     Ok(())
 }
