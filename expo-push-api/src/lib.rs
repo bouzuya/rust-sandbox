bee73d8031df327a65c09ea7a3239a6df650c148
bouzuya <m@bouzuya.net>
2024-01-05 22:24:16 +0900

    expo-push-api: Fix ExpoPushMessage

--- expo-push-api/src/lib.rs
+++ expo-push-api/src/lib.rs
@@ -7,17 +7,29 @@ pub struct SendPushNotificationsRequest(Vec<ExpoPushMessage>);
 #[serde(rename_all = "camelCase")]
 pub struct ExpoPushMessage {
     pub to: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<serde_json::Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub body: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub ttl: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub expiration: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub priority: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub subtitle: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub sound: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub badge: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub channel_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub category_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub mutable_content: Option<bool>,
 }
 
@@ -46,8 +58,21 @@ pub enum ExpoPushTicket {
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
 pub struct ExpoPushErrorReceipt {
-    message: String,
-    details: Option<serde_json::Value>,
+    pub message: String,
+    pub details: Option<ExpoPushErrorReceiptDetails>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
+pub struct ExpoPushErrorReceiptDetails {
+    pub errors: Option<ExpoPushErrorReceiptDetailsError>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
+pub enum ExpoPushErrorReceiptDetailsError {
+    DeviceNotRegistered,
+    InvalidCredentials,
+    MessageRateExceeded,
+    MessageTooBig,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Serialize)]
@@ -71,4 +96,39 @@ pub enum GetPushNotificationReceiptsResponse {
 pub enum ExpoPushReceipt {
     Ok,
     Error(ExpoPushErrorReceipt),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expo_push_message() -> anyhow::Result<()> {
+        let expo_push_message = ExpoPushMessage {
+            to: vec!["ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]".to_string()],
+            data: None,
+            title: Some("hello".to_string()),
+            body: Some("world".to_string()),
+            ttl: None,
+            expiration: None,
+            priority: None,
+            subtitle: None,
+            sound: None,
+            badge: None,
+            channel_id: None,
+            category_id: None,
+            mutable_content: None,
+        };
+        assert_eq!(
+            serde_json::to_string_pretty(&expo_push_message)?,
+            r#"{
+  "to": [
+    "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]"
+  ],
+  "title": "hello",
+  "body": "world"
+}"#
+        );
+        Ok(())
+    }
 }
