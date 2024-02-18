e89a4c5adb56a6e2ce3eb6b43651a2a003f46d7b
bouzuya <m@bouzuya.net>
2024-02-19 06:49:19 +0900

    roguelike-tdd: Add Passage::get_central_passage

--- roguelike-tdd/src/dungeon/passage.rs
+++ roguelike-tdd/src/dungeon/passage.rs
@@ -22,6 +22,20 @@ impl Passage {
         Passage { steps }
     }
 
+    // 中央の部屋とその上下左右の部屋のうちランダムな部屋を配列で返す
+    pub fn get_central_passage(rooms: &[Room]) -> Vec<Room> {
+        let center = rooms[4].clone();
+        let center_top = rooms[1].clone();
+        let center_left = rooms[3].clone();
+        let center_right = rooms[5].clone();
+        let center_bottom = rooms[7].clone();
+        let choices = vec![center_top, center_left, center_right, center_bottom];
+        let choice = rand::seq::SliceRandom::choose(choices.as_slice(), &mut rand::thread_rng())
+            .expect("slice is not empty")
+            .clone();
+        vec![center, choice]
+    }
+
     // 外周順 (0, 1, 2, 5, 8, 7, 6, 3, 0) に並べる
     pub fn get_outer_perimeter(rooms: &[Room]) -> Vec<Room> {
         vec![
@@ -112,7 +126,30 @@ WWWWWWWW
 
     #[test]
     fn test_get_outer_perimeter_外周の部屋を連結順に並べた配列が変えること() {
-        let rooms = vec![
+        let rooms = build_rooms();
+        let expeted = vec![0, 1, 2, 5, 8, 7, 6, 3, 0]
+            .into_iter()
+            .map(|i| rooms[i].clone())
+            .collect::<Vec<Room>>();
+        let actual = Passage::get_outer_perimeter(&rooms);
+        assert_eq!(actual, expeted);
+    }
+
+    #[test]
+    fn test_get_centeral_passage_中央の部屋とランダムな部屋を通路でつなぐ配列が返ること() {
+        let rooms = build_rooms();
+        let center = rooms[4].clone();
+        let center_top = rooms[1].clone();
+        let center_left = rooms[3].clone();
+        let center_right = rooms[5].clone();
+        let center_bottom = rooms[7].clone();
+        let actual = Passage::get_central_passage(&rooms);
+        assert_eq!(actual[0], center);
+        assert!([center_top, center_left, center_right, center_bottom].contains(&actual[1]));
+    }
+
+    fn build_rooms() -> Vec<Room> {
+        vec![
             (0, 0),
             (2, 0),
             (4, 0),
@@ -130,13 +167,7 @@ WWWWWWWW
             width: 2,
             height: 2,
         })
-        .collect::<Vec<Room>>();
-        let expeted = vec![0, 1, 2, 5, 8, 7, 6, 3, 0]
-            .into_iter()
-            .map(|i| rooms[i].clone())
-            .collect::<Vec<Room>>();
-        let actual = Passage::get_outer_perimeter(&rooms);
-        assert_eq!(actual, expeted);
+        .collect::<Vec<Room>>()
     }
 
     fn map_util_parse(s: &str) -> Vec<Vec<MapChip>> {
