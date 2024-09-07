e1b70e141928457bb7921542b48527325ea6a89b
bouzuya <m@bouzuya.net>
2024-09-07 18:25:03 +0900

    rustack: 2

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,8 +1,24 @@
-fn main() {
+use anyhow::Context as _;
+
+fn main() -> anyhow::Result<()> {
     let mut stack = vec![];
 
     stack.push(42);
     stack.push(36);
 
+    add(&mut stack)?;
+
+    stack.push(22);
+
+    add(&mut stack)?;
+
     println!("stack {:#?}", stack);
+    Ok(())
+}
+
+fn add(stack: &mut Vec<i32>) -> anyhow::Result<()> {
+    let lhs = stack.pop().context("lhs is none")?;
+    let rhs = stack.pop().context("rhs is none")?;
+    stack.push(lhs + rhs);
+    Ok(())
 }
