f79b459e4f5ea9bacca930cd3b14b87422bc6c3c
bouzuya <m@bouzuya.net>
2024-09-13 22:38:14 +0900

    rustack: 8

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,32 +1,37 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+};
 
 use anyhow::Context as _;
 
 #[derive(Debug)]
-struct Vm<'src> {
-    stack: Vec<Value<'src>>,
-    vars: HashMap<&'src str, Value<'src>>,
+struct Vm {
+    stack: Vec<Value>,
+    vars: HashMap<String, Value>,
+    blocks: Vec<Vec<Value>>,
 }
 
-impl<'src> Vm<'src> {
+impl Vm {
     fn new() -> Self {
         Self {
             stack: vec![],
             vars: HashMap::new(),
+            blocks: vec![],
         }
     }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-enum Value<'src> {
+enum Value {
     Num(i32),
-    Op(&'src str),
-    Sym(&'src str),
-    Block(Vec<Value<'src>>),
+    Op(String),
+    Sym(String),
+    Block(Vec<Value>),
 }
 
-impl<'src> Value<'src> {
-    fn as_block(&self) -> anyhow::Result<&Vec<Value<'src>>> {
+impl Value {
+    fn as_block(&self) -> anyhow::Result<&Vec<Value>> {
         match self {
             Value::Block(val) => Ok(val),
             _ => anyhow::bail!("Value is not a block"),
@@ -40,97 +45,113 @@ impl<'src> Value<'src> {
         }
     }
 
-    fn as_sym(&self) -> anyhow::Result<&'src str> {
+    fn as_sym(&self) -> anyhow::Result<&str> {
         match self {
-            Value::Sym(sym) => Ok(sym),
+            Value::Sym(sym) => Ok(sym.as_str()),
             _ => anyhow::bail!("Value is not a symbol"),
         }
     }
 }
 
+impl std::fmt::Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Num(num) => num.to_string(),
+            Value::Op(op) => op.clone(),
+            Value::Sym(sym) => sym.clone(),
+            Value::Block(block) => {
+                let mut s = String::from("{");
+                for val in block {
+                    s.push_str(&val.to_string());
+                    s.push(' ');
+                }
+                s.push('}');
+                s
+            }
+        }
+        .fmt(f)
+    }
+}
+
 fn main() -> anyhow::Result<()> {
-    for line in std::io::stdin().lines() {
-        let line = line?;
-        let stack = parse(&line)?;
-        println!("stack {:#?}", stack);
+    if let Some(f) = std::env::args()
+        .nth(1)
+        .and_then(|f| std::fs::File::open(f).ok())
+    {
+        parse_batch(BufReader::new(f))?;
+    } else {
+        parse_interactive()?;
+    }
+    Ok(())
+}
+
+fn parse_batch(source: impl BufRead) -> anyhow::Result<Vec<Value>> {
+    let mut vm = Vm::new();
+    for line in source.lines().map_while(Result::ok) {
+        for word in line.split(' ') {
+            parse_word(word, &mut vm)?;
+        }
+    }
+    Ok(vm.stack)
+}
+
+fn parse_interactive() -> anyhow::Result<()> {
+    let mut vm = Vm::new();
+    for line in std::io::stdin().lines().map_while(Result::ok) {
+        for word in line.split(' ') {
+            parse_word(word, &mut vm)?;
+        }
+        println!("stack: {:#?}", vm.stack);
+    }
+    Ok(())
+}
+
+fn parse_word(word: &str, vm: &mut Vm) -> anyhow::Result<()> {
+    if word.is_empty() {
+        return Ok(());
+    }
+    if word == "{" {
+        vm.blocks.push(vec![]);
+    } else if word == "}" {
+        let block = vm.blocks.pop().context("block is none")?;
+        vm.stack.push(Value::Block(block));
+    } else {
+        let value = match word.parse::<i32>() {
+            Ok(parsed) => Value::Num(parsed),
+            Err(_) => {
+                if let Some(word) = word.strip_prefix('/') {
+                    Value::Sym(word.to_owned())
+                } else {
+                    Value::Op(word.to_owned())
+                }
+            }
+        };
+        eval(value, vm)?;
     }
 
     Ok(())
 }
 
-fn parse(line: &str) -> anyhow::Result<Vec<Value>> {
-    let mut vm = Vm::new();
-    let words = line.split(' ').collect::<Vec<_>>();
-    let mut words = &words[..];
-
-    while let Some((&word, mut rest)) = words.split_first() {
-        if word.is_empty() {
-            break;
-        }
-        if word == "{" {
-            let value;
-            (value, rest) = parse_block(rest)?;
-            vm.stack.push(value);
-        } else {
-            let value = match word.parse::<i32>() {
-                Ok(parsed) => Value::Num(parsed),
-                Err(_) => {
-                    if word.starts_with('/') {
-                        Value::Sym(&word[1..])
-                    } else {
-                        Value::Op(word)
-                    }
-                }
-            };
-            eval(value, &mut vm)?;
-            println!("vm {:#?}", vm);
-        }
-        words = rest;
+fn eval(code: Value, vm: &mut Vm) -> anyhow::Result<()> {
+    if let Some(block) = vm.blocks.last_mut() {
+        block.push(code);
+        return Ok(());
     }
-    Ok(vm.stack)
-}
-
-fn parse_block<'src, 'a>(input: &'a [&'src str]) -> anyhow::Result<(Value<'src>, &'a [&'src str])> {
-    let mut tokens = vec![];
-    let mut words = input;
-
-    while let Some((&word, mut rest)) = words.split_first() {
-        if word.is_empty() {
-            break;
-        }
-        if word == "{" {
-            let value;
-            (value, rest) = parse_block(rest)?;
-            tokens.push(value);
-        } else if word == "}" {
-            return Ok((Value::Block(tokens), rest));
-        } else if let Ok(value) = word.parse::<i32>() {
-            tokens.push(Value::Num(value));
-        } else if word.starts_with('/') {
-            tokens.push(Value::Sym(&word[1..]));
-        } else {
-            // op or sym
-            tokens.push(Value::Op(word));
-        }
-        words = rest;
-    }
-
-    Ok((Value::Block(tokens), words))
-}
-
-fn eval<'src>(code: Value<'src>, vm: &mut Vm<'src>) -> anyhow::Result<()> {
     match code {
-        Value::Op("+") => add(&mut vm.stack)?,
-        Value::Op("-") => sub(&mut vm.stack)?,
-        Value::Op("*") => mul(&mut vm.stack)?,
-        Value::Op("/") => div(&mut vm.stack)?,
-        Value::Op("<") => lt(vm)?,
-        Value::Op("def") => op_def(vm)?,
-        Value::Op("if") => op_if(vm)?,
-        Value::Op(op) => {
-            let val = vm.vars.get(op).context("{:?} is not a defined operation")?;
-            vm.stack.push(val.clone());
-        }
+        Value::Op(op) => match op.as_str() {
+            "+" => add(&mut vm.stack)?,
+            "-" => sub(&mut vm.stack)?,
+            "*" => mul(&mut vm.stack)?,
+            "/" => div(&mut vm.stack)?,
+            "<" => lt(vm)?,
+            "def" => op_def(vm)?,
+            "if" => op_if(vm)?,
+            "puts" => puts(vm)?,
+            op => {
+                let val = vm.vars.get(op).context("{:?} is not a defined operation")?;
+                vm.stack.push(val.clone());
+            }
+        },
         _ => vm.stack.push(code.clone()),
     }
     Ok(())
@@ -152,21 +173,22 @@ impl_op!(sub, -);
 impl_op!(mul, *);
 impl_op!(div, /);
 
-fn lt<'src>(vm: &mut Vm<'src>) -> anyhow::Result<()> {
+fn lt(vm: &mut Vm) -> anyhow::Result<()> {
     let rhs = vm.stack.pop().context("lhs is none")?.as_num()?;
     let lhs = vm.stack.pop().context("rhs is none")?.as_num()?;
     vm.stack.push(Value::Num(if lhs < rhs { 1 } else { 0 }));
     Ok(())
 }
 
-fn op_def<'src>(vm: &mut Vm<'src>) -> anyhow::Result<()> {
+fn op_def(vm: &mut Vm) -> anyhow::Result<()> {
     let val = vm.stack.pop().context("val is none")?;
-    let sym = vm.stack.pop().context("sym is none")?.as_sym()?;
-    vm.vars.insert(sym, val);
+    let sym = vm.stack.pop().context("sym is none")?;
+    let sym = sym.as_sym()?;
+    vm.vars.insert(sym.to_owned(), val);
     Ok(())
 }
 
-fn op_if<'src>(vm: &mut Vm<'src>) -> anyhow::Result<()> {
+fn op_if(vm: &mut Vm) -> anyhow::Result<()> {
     let false_branch = vm
         .stack
         .pop()
@@ -197,15 +219,23 @@ fn op_if<'src>(vm: &mut Vm<'src>) -> anyhow::Result<()> {
     Ok(())
 }
 
+fn puts(vm: &mut Vm) -> anyhow::Result<()> {
+    let val = vm.stack.pop().context("val is none")?;
+    println!("{}", val);
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
     use super::*;
 
     #[test]
     fn test_def() -> anyhow::Result<()> {
         use Value::*;
         assert_eq!(
-            parse("/x 10 def /y 20 def { x y < } { x } { y } if")?,
+            parse_batch(Cursor::new("/x 10 def /y 20 def { x y < } { x } { y } if"))?,
             vec![Num(10)]
         );
         Ok(())
@@ -215,7 +245,7 @@ mod tests {
     fn test_group() -> anyhow::Result<()> {
         use Value::*;
         assert_eq!(
-            parse("1 2 + { 3 4 }")?,
+            parse_batch(Cursor::new("1 2 + { 3 4 }"))?,
             vec![Num(3), Block(vec![Num(3), Num(4)])]
         );
         Ok(())
@@ -224,14 +254,20 @@ mod tests {
     #[test]
     fn test_if_false() -> anyhow::Result<()> {
         use Value::*;
-        assert_eq!(parse("{ 1 -1 + } { 100 } { -100 } if")?, vec![Num(-100)]);
+        assert_eq!(
+            parse_batch(Cursor::new("{ 1 -1 + } { 100 } { -100 } if"))?,
+            vec![Num(-100)]
+        );
         Ok(())
     }
 
     #[test]
     fn test_if_true() -> anyhow::Result<()> {
         use Value::*;
-        assert_eq!(parse("{ 1 1 + } { 100 } { -100 } if")?, vec![Num(100)]);
+        assert_eq!(
+            parse_batch(Cursor::new("{ 1 1 + } { 100 } { -100 } if"))?,
+            vec![Num(100)]
+        );
         Ok(())
     }
 
@@ -247,7 +283,7 @@ mod tests {
     #[test]
     fn test_sub() -> anyhow::Result<()> {
         use Value::*;
-        assert_eq!(parse("1 2 -")?, vec![Num(-1)]);
+        assert_eq!(parse_batch(Cursor::new("1 2 -"))?, vec![Num(-1)]);
         Ok(())
     }
 }
