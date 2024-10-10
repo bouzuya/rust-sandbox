af32d769fa0975c8d845dfd29da5fa19f9daaa58
bouzuya <m@bouzuya.net>
2024-10-10 21:03:27 +0900

    ruscal: 33

--- ruscal/src/main.rs
+++ ruscal/src/main.rs
@@ -14,27 +14,98 @@ use nom::Parser;
 use nom::{Finish, IResult};
 
 fn main() {
-    let instructions = [
-        Instruction::LoadLiteral(42),
-        Instruction::LoadLiteral(36),
-        Instruction::Add,
-    ];
-
-    let result = interpret(&instructions);
-    println!("result: {:?}", result);
+    let mut args = std::env::args();
+    args.next();
+    match args.next().as_ref().map(|s| s as &str) {
+        Some("w") => write_program("bytecode.bin"),
+        Some("r") => {
+            if let Some(instructions) = read_program("bytecode.bin") {
+                let result = interpret(&instructions);
+                println!("result {:?}", result);
+            }
+        }
+        _ => println!("Please specify w or r as an argument"),
+    }
 }
 
-enum Instruction {
-    LoadLiteral(i64),
+#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+enum OpCode {
+    LoadLiteral,
     Add,
+}
+
+impl From<u8> for OpCode {
+    fn from(o: u8) -> Self {
+        const LOAD_LITERAL: u8 = OpCode::LoadLiteral as u8;
+        const ADD: u8 = OpCode::Add as u8;
+        match o {
+            LOAD_LITERAL => OpCode::LoadLiteral,
+            ADD => OpCode::Add,
+            _ => panic!("OpCode \"{:02X}\" unrecognized!", o),
+        }
+    }
+}
+
+#[repr(C)]
+struct Instruction {
+    op: OpCode,
+    arg0: u8,
+}
+
+impl Instruction {
+    fn new(op: OpCode, arg0: u8) -> Self {
+        Self { op, arg0 }
+    }
+
+    fn deserialize(reader: &mut impl std::io::Read) -> Result<Self, std::io::Error> {
+        let mut buf = [0u8; 2];
+        reader.read_exact(&mut buf)?;
+        Ok(Self::new(buf[0].into(), buf[1]))
+    }
+
+    fn serialize(&self, writer: &mut impl std::io::Write) -> Result<(), std::io::Error> {
+        writer.write_all(&[self.op as u8, self.arg0])?;
+        Ok(())
+    }
+}
+
+fn read_program(file: &str) -> Option<Vec<Instruction>> {
+    if let Ok(reader) = std::fs::File::open(file) {
+        let mut reader = std::io::BufReader::new(reader);
+        let mut instructions = vec![];
+        while let Ok(instruction) = Instruction::deserialize(&mut reader) {
+            instructions.push(instruction);
+        }
+        Some(instructions)
+    } else {
+        None
+    }
+}
+
+fn write_program(file: &str) {
+    let instructions = [
+        Instruction::new(OpCode::LoadLiteral, 42),
+        Instruction::new(OpCode::LoadLiteral, 36),
+        Instruction::new(OpCode::Add, 0),
+    ];
+
+    if let Ok(writer) = std::fs::File::create(file) {
+        let mut writer = std::io::BufWriter::new(writer);
+        for instruction in &instructions {
+            instruction.serialize(&mut writer).unwrap();
+        }
+
+        println!("Written {} instructions", instructions.len());
+    }
 }
 
 fn interpret(instructions: &[Instruction]) -> Option<i64> {
     let mut stack = vec![];
     for instruction in instructions {
-        match instruction {
-            Instruction::LoadLiteral(value) => stack.push(*value),
-            Instruction::Add => {
+        match instruction.op {
+            OpCode::LoadLiteral => stack.push(instruction.arg0 as i64),
+            OpCode::Add => {
                 let rhs = stack.pop().expect("Stack underflow");
                 let lhs = stack.pop().expect("Stack underflow");
                 stack.push(lhs + rhs);
