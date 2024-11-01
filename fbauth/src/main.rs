0ad326883bcf6aa508bd6431d9a6719866a5c16d
bouzuya <m@bouzuya.net>
2024-11-01 23:21:25 +0900

    fbauth: Add axum

--- fbauth/src/main.rs
+++ fbauth/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let router = Router::new().nest_service("/", ServeDir::new("public"));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, router).await?;
+    Ok(())
 }
