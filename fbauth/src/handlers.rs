2df31f8ce57575a9c459e96e9efaf1cf7689444a
bouzuya <m@bouzuya.net>
2024-11-18 22:39:56 +0900

    fbauth: Extract create_session

--- fbauth/src/handlers.rs
+++ fbauth/src/handlers.rs
@@ -1,12 +1,8 @@
 mod create_authorization_urls;
+mod create_session;
 
-use std::str::FromStr as _;
-
-use crate::session_id::SessionId;
 use crate::user::User;
-use crate::user_id::UserId;
-use crate::user_secret::UserSecret;
-use crate::{session::Session, AppState};
+use crate::AppState;
 use axum::{
     extract::{Query, State},
     response::Html,
@@ -96,58 +92,6 @@ impl<T> From<std::sync::PoisonError<T>> for Error {
     }
 }
 
-#[derive(serde::Deserialize)]
-struct CreateSessionRequestBody {
-    user_id: String,
-    user_secret: String,
-}
-
-#[derive(serde::Serialize)]
-struct CreateSessionResponse {
-    session_token: String,
-}
-
-async fn create_session(
-    State(app_state): State<AppState>,
-    Json(CreateSessionRequestBody {
-        user_id,
-        user_secret,
-    }): Json<CreateSessionRequestBody>,
-) -> Result<Json<CreateSessionResponse>, Error> {
-    let users = app_state.users.lock()?;
-    let user_id = UserId::from_str(&user_id).map_err(|_| Error::Client)?;
-    let user_secret = UserSecret::from_str(&user_secret).map_err(|_| Error::Client)?;
-    let user = users.get(&user_id).ok_or_else(|| Error::Client)?;
-    if user.secret != user_secret {
-        return Err(Error::Client);
-    }
-
-    let mut sessions = app_state.sessions.lock().map_err(|_| Error::Server)?;
-    let session_id = SessionId::generate();
-    sessions.insert(
-        session_id,
-        Session {
-            id: session_id,
-            user_id,
-            state: None,
-        },
-    );
-
-    let token = jsonwebtoken::encode(
-        &jsonwebtoken::Header::new(jsonwebtoken::Algorithm::RS256),
-        &Claims {
-            sid: session_id.to_string(),
-            sub: user_id.to_string(),
-        },
-        &jsonwebtoken::EncodingKey::from_rsa_pem(include_bytes!("../key.pem"))
-            .map_err(|_| Error::Server)?,
-    )
-    .map_err(|_| Error::Server)?;
-    Ok(Json(CreateSessionResponse {
-        session_token: token.to_string(),
-    }))
-}
-
 #[derive(serde::Serialize)]
 struct CreateUserResponse {
     user_id: String,
@@ -167,9 +111,9 @@ async fn create_user(State(app_state): State<AppState>) -> Json<CreateUserRespon
 pub fn route() -> axum::Router<AppState> {
     axum::Router::new()
         .merge(create_authorization_urls::route())
+        .merge(create_session::route())
         .route_service("/", ServeFile::new("assets/index.html"))
         .nest_service("/assets", ServeDir::new("assets"))
         .route("/callback", get(callback))
-        .route("/sessions", post(create_session))
         .route("/users", post(create_user))
 }
---
+++ fbauth/src/handlers/create_session.rs
@@ -0,0 +1,65 @@
+use std::str::FromStr as _;
+
+use crate::session_id::SessionId;
+use crate::user_id::UserId;
+use crate::user_secret::UserSecret;
+use crate::{session::Session, AppState};
+use axum::{extract::State, routing::post, Json};
+
+use super::{Claims, Error};
+
+#[derive(serde::Deserialize)]
+struct CreateSessionRequestBody {
+    user_id: String,
+    user_secret: String,
+}
+
+#[derive(serde::Serialize)]
+struct CreateSessionResponse {
+    session_token: String,
+}
+
+async fn create_session(
+    State(app_state): State<AppState>,
+    Json(CreateSessionRequestBody {
+        user_id,
+        user_secret,
+    }): Json<CreateSessionRequestBody>,
+) -> Result<Json<CreateSessionResponse>, Error> {
+    let users = app_state.users.lock()?;
+    let user_id = UserId::from_str(&user_id).map_err(|_| Error::Client)?;
+    let user_secret = UserSecret::from_str(&user_secret).map_err(|_| Error::Client)?;
+    let user = users.get(&user_id).ok_or_else(|| Error::Client)?;
+    if user.secret != user_secret {
+        return Err(Error::Client);
+    }
+
+    let mut sessions = app_state.sessions.lock().map_err(|_| Error::Server)?;
+    let session_id = SessionId::generate();
+    sessions.insert(
+        session_id,
+        Session {
+            id: session_id,
+            user_id,
+            state: None,
+        },
+    );
+
+    let token = jsonwebtoken::encode(
+        &jsonwebtoken::Header::new(jsonwebtoken::Algorithm::RS256),
+        &Claims {
+            sid: session_id.to_string(),
+            sub: user_id.to_string(),
+        },
+        &jsonwebtoken::EncodingKey::from_rsa_pem(include_bytes!("../../key.pem"))
+            .map_err(|_| Error::Server)?,
+    )
+    .map_err(|_| Error::Server)?;
+    Ok(Json(CreateSessionResponse {
+        session_token: token.to_string(),
+    }))
+}
+
+pub fn route() -> axum::Router<AppState> {
+    axum::Router::new().route("/sessions", post(create_session))
+}
