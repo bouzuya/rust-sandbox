5b06e78cffc8dbce21c4130895c5ad54e5b32905
bouzuya <m@bouzuya.net>
2024-11-22 21:44:38 +0900

    fbauth: Fix session_id

--- fbauth/src/session_id.rs
+++ fbauth/src/session_id.rs
@@ -1,16 +1,19 @@
-// FIXME: use safe secret
+use argon2::password_hash::rand_core::{OsRng, RngCore};
+
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct SessionId(uuid::Uuid);
+pub struct SessionId([u8; 54]);
 
 impl SessionId {
     pub fn generate() -> Self {
-        Self(uuid::Uuid::new_v4())
+        let mut bytes = [0u8; 54];
+        OsRng.fill_bytes(&mut bytes);
+        Self(bytes)
     }
 }
 
 impl std::fmt::Display for SessionId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
+        hex::encode(&self.0).fmt(f)
     }
 }
 
@@ -18,6 +21,32 @@ impl std::str::FromStr for SessionId {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(uuid::Uuid::from_str(s)?))
+        let bytes = hex::decode(s)?;
+        anyhow::ensure!(bytes.len() == 54);
+        Ok(Self(bytes.as_slice().try_into()?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashSet, str::FromStr as _};
+
+    use super::*;
+
+    #[test]
+    fn test() -> anyhow::Result<()> {
+        let s = "1231b72d839bb2691c76a61a3d466fa0b9afb2411f25fab0991f060543621738ef604b74babcb6243c566db7424cdceeddbb95ee23fc";
+        let session_id = SessionId::from_str(s)?;
+        assert_eq!(session_id.to_string(), s);
+
+        assert_eq!(
+            (0..100)
+                .map(|_| SessionId::generate())
+                .collect::<HashSet<_>>()
+                .len(),
+            100
+        );
+
+        Ok(())
     }
 }
