3e60098c73184c3a42fcfa5638096b1c6f9f8dbb
bouzuya <m@bouzuya.net>
2024-11-22 21:47:24 +0900

    fbauth: Fix create_user error handling

--- fbauth/src/handlers/create_user.rs
+++ fbauth/src/handlers/create_user.rs
@@ -2,20 +2,22 @@ use crate::user::User;
 use crate::AppState;
 use axum::{extract::State, routing::post, Json};
 
+use super::Error;
+
 #[derive(serde::Serialize)]
 struct CreateUserResponse {
     user_id: String,
     user_secret: String,
 }
 
-async fn create_user(State(app_state): State<AppState>) -> Json<CreateUserResponse> {
-    let mut users = app_state.users.lock().unwrap();
-    let (user, raw) = User::new().expect("FIXME");
+async fn create_user(State(app_state): State<AppState>) -> Result<Json<CreateUserResponse>, Error> {
+    let mut users = app_state.users.lock()?;
+    let (user, raw) = User::new().map_err(|_| Error::Server)?;
     users.insert(user.id, user.clone());
-    Json(CreateUserResponse {
+    Ok(Json(CreateUserResponse {
         user_id: user.id.to_string(),
         user_secret: raw,
-    })
+    }))
 }
 
 pub fn route() -> axum::Router<AppState> {
