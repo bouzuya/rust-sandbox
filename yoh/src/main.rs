410bebd3053a4db147f9d1374367ccf43f324b83
bouzuya <m@bouzuya.net>
2025-01-18 22:05:11 +0900

    yoh: Add GET /yc/login/index.html

--- yoh/src/main.rs
+++ yoh/src/main.rs
@@ -1,6 +1,18 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let response = reqwest::get("https://bouzuya.net/").await?;
+    let client = reqwest::Client::builder()
+        .cookie_store(true)
+        .gzip(true)
+        .build()?;
+    let response = client
+        .get("https://order.yodobashi.com/yc/login/index.html")
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
+        )
+        .header("Accept-Language", "ja")
+        .send()
+        .await?;
     let body = response.text().await?;
     println!("{}", body);
     Ok(())
