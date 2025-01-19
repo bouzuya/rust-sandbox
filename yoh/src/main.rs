d1f1284a62847ad8b20112341838eb4f9598dd95
bouzuya <m@bouzuya.net>
2025-01-19 22:42:10 +0900

    yoh: Use headless_chrome

--- yoh/src/main.rs
+++ yoh/src/main.rs
@@ -1,19 +1,34 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let client = reqwest::Client::builder()
-        .cookie_store(true)
-        .gzip(true)
-        .build()?;
-    let response = client
-        .get("https://order.yodobashi.com/yc/login/index.html")
-        .header(
-            "User-Agent",
-            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
-        )
-        .header("Accept-Language", "ja")
-        .send()
-        .await?;
-    let body = response.text().await?;
-    println!("{}", body);
+    let email = "".to_owned();
+    let password = "".to_owned();
+
+    let browser = headless_chrome::Browser::default()?;
+
+    let tab = browser.new_tab()?;
+    tab.set_user_agent(
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+        Some("en-US"),
+        Some("Linux x86_64")
+    )?;
+
+    tab.navigate_to("https://order.yodobashi.com/yc/login/index.html")?;
+    tab.wait_until_navigated()?;
+
+    tab.wait_for_element("input[name=\"memberId\"]")?.click()?;
+    tab.type_str(&email)?;
+
+    tab.wait_for_element("input[name=\"password\"]")?.click()?;
+    tab.type_str(&password)?;
+
+    tab.press_key("Enter")?;
+
+    tab.wait_until_navigated()?;
+
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+
+    let pdf = tab.print_to_pdf(None)?;
+    std::fs::write("screenshot.pdf", pdf)?;
+
     Ok(())
 }
