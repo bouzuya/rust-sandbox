9e475e6f275a972d8e203181ad338726f1333fbf
bouzuya <m@bouzuya.net>
2025-03-25 07:40:44 +0900

    base64: Add Error and decode

--- base64/src/lib.rs
+++ base64/src/lib.rs
@@ -1,8 +1,71 @@
-const CHARS: std::cell::LazyCell<Vec<char>> = std::cell::LazyCell::new(|| {
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-        .chars()
-        .collect::<Vec<char>>()
-});
+#[derive(Debug)]
+pub struct Error(ErrorKind);
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            ErrorKind::InvalidCharacter(c) => write!(f, "invalid character: {}", c),
+            ErrorKind::InvalidLength(l) => write!(f, "invalid length: {}", l),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum ErrorKind {
+    InvalidCharacter(char),
+    InvalidLength(usize),
+}
+
+impl From<ErrorKind> for Error {
+    fn from(v: ErrorKind) -> Self {
+        Error(v)
+    }
+}
+
+pub fn decode<S: AsRef<str>>(s: S) -> Result<Vec<u8>, Error> {
+    let s = s.as_ref();
+    if s.len() % 4 != 0 {
+        return Err(Error::from(ErrorKind::InvalidLength(s.len())));
+    }
+
+    let mut bytes = vec![];
+    let mut iter = s.chars();
+
+    // count padding and remove it
+    let mut pad_len = 0;
+    let mut tail = vec![];
+    for _ in 0..2 {
+        if let Some(c) = iter.next_back() {
+            if c == '=' {
+                tail.push('A'); // 'A' = 0b0000_0000 = '='
+                pad_len += 1;
+            } else {
+                tail.push(c);
+            }
+        }
+    }
+    let mut iter = iter.chain(tail.into_iter().rev());
+
+    loop {
+        match (iter.next(), iter.next(), iter.next(), iter.next()) {
+            (Some(c1), Some(c2), Some(c3), Some(c4)) => {
+                let (i1, i2, i3, i4) = (c2i(c1)?, c2i(c2)?, c2i(c3)?, c2i(c4)?);
+                bytes.push((i1 & 0b0011_1111) << 2 | (i2 & 0b0011_0000) >> 4);
+                bytes.push((i2 & 0b0000_1111) << 4 | (i3 & 0b0011_1100) >> 2);
+                bytes.push((i3 & 0b0000_0011) << 6 | (i4 & 0b0011_1111));
+            }
+            _ => break,
+        }
+    }
+
+    for _ in 0..pad_len {
+        bytes.pop();
+    }
+
+    Ok(bytes)
+}
 
 pub fn encode<I: AsRef<[u8]>>(bytes: I) -> String {
     let bytes = bytes.as_ref();
@@ -22,10 +85,10 @@ pub fn encode<I: AsRef<[u8]>>(bytes: I) -> String {
                 let i3 = ((b2 & 0b0000_1111) << 2) | (b3 >> 6);
                 let i4 = b3 & 0b0011_1111;
 
-                s.push(CHARS[i1 as usize]);
-                s.push(CHARS[i2 as usize]);
-                s.push(CHARS[i3 as usize]);
-                s.push(CHARS[i4 as usize]);
+                s.push(i2c(i1));
+                s.push(i2c(i2));
+                s.push(i2c(i3));
+                s.push(i2c(i4));
             }
             _ => unreachable!(),
         }
@@ -38,4 +101,25 @@ pub fn encode<I: AsRef<[u8]>>(bytes: I) -> String {
     }
 
     s
+}
+
+const CHARS: std::cell::LazyCell<Vec<char>> = std::cell::LazyCell::new(|| {
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+        .chars()
+        .collect::<Vec<char>>()
+});
+
+fn c2i(c: char) -> Result<u8, Error> {
+    Ok(match c {
+        'A'..='Z' => c as u8 - b'A',
+        'a'..='z' => c as u8 - b'a' + 26,
+        '0'..='9' => c as u8 - b'0' + 52,
+        '+' => 62,
+        '/' => 63,
+        _ => return Err(Error::from(ErrorKind::InvalidCharacter(c))),
+    })
+}
+
+fn i2c(i: u8) -> char {
+    CHARS[i as usize]
 }
--- base64/tests/lib.rs
+++ base64/tests/lib.rs
@@ -1,4 +1,40 @@
 #[test]
+fn test_decode() -> Result<(), base64::Error> {
+    use base64::decode;
+
+    // <https://datatracker.ietf.org/doc/html/rfc4648#section-9>
+    assert_eq!(
+        decode("FPucA9l+")?,
+        vec![0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x7e]
+    );
+    assert_eq!(decode("FPucA9k=")?, vec![0x14, 0xfb, 0x9c, 0x03, 0xd9]);
+    assert_eq!(decode("FPucAw==")?, vec![0x14, 0xfb, 0x9c, 0x03]);
+
+    // <https://datatracker.ietf.org/doc/html/rfc4648#section-10>
+    assert_eq!(decode("")?, vec![]);
+    assert_eq!(decode("Zg==")?, "f".as_bytes().to_vec());
+    assert_eq!(decode("Zm8=")?, "fo".as_bytes().to_vec());
+    assert_eq!(decode("Zm9v")?, "foo".as_bytes().to_vec());
+    assert_eq!(decode("Zm9vYg==")?, "foob".as_bytes().to_vec());
+    assert_eq!(decode("Zm9vYmE=")?, "fooba".as_bytes().to_vec());
+    assert_eq!(decode("Zm9vYmFy")?, "foobar".as_bytes().to_vec());
+
+    assert_eq!(decode("1").unwrap_err().to_string(), "invalid length: 1");
+    assert_eq!(decode("12").unwrap_err().to_string(), "invalid length: 2");
+    assert_eq!(decode("123").unwrap_err().to_string(), "invalid length: 3");
+
+    assert_eq!(
+        decode("123!").unwrap_err().to_string(),
+        "invalid character: !"
+    );
+
+    fn assert_fn<T: std::error::Error + Send + Sync>() {}
+    assert_fn::<base64::Error>();
+
+    Ok(())
+}
+
+#[test]
 fn test_encode() {
     use base64::encode;
 
