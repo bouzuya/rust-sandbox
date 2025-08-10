a3d5965e0fed9b4a3e4cbc541de5769bfeb95936
bouzuya <m@bouzuya.net>
2025-08-10 23:01:13 +0900

    firestore-path-helper: Refactor firestore-path-helper-proc-macro

--- firestore-path-helper/crates/firestore-path-helper-proc-macro/src/lib.rs
+++ firestore-path-helper/crates/firestore-path-helper-proc-macro/src/lib.rs
@@ -2,17 +2,16 @@ use std::collections::HashSet;
 
 #[proc_macro]
 pub fn firestore_path_helper(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(input as MacroInput);
-    let segments = match validate_macro_input(&input) {
-        Ok(segments) => segments,
-        Err(e) => return e.to_compile_error().into(),
-    };
-    format_macro_output(input, segments)
+    format_macro_output(syn::parse_macro_input!(input as MacroInput))
+}
+
+enum Segment {
+    CollectionId(String),
+    DocumentId(syn::Ident, syn::Type),
 }
 
 struct MacroInput {
-    format: syn::LitStr,
-    args: Vec<(syn::Ident, syn::Type)>,
+    segments: Vec<Segment>,
 }
 
 impl syn::parse::Parse for MacroInput {
@@ -38,33 +37,50 @@ impl syn::parse::Parse for MacroInput {
             .map(|arg| (arg.ident, arg.typ))
             .collect::<Vec<(syn::Ident, syn::Type)>>();
 
-        Ok(MacroInput { format, args })
+        let segments = parse_format(format, &args)?;
+
+        Ok(MacroInput { segments })
     }
 }
 
-fn format_macro_output(input: MacroInput, segments: Vec<Segment>) -> proc_macro::TokenStream {
-    let MacroInput { format: _, args } = input;
+fn format_macro_output(input: MacroInput) -> proc_macro::TokenStream {
+    let MacroInput { segments } = input;
 
-    let fields = args.into_iter().map(|(field_name, field_type)| {
-        quote::quote! {
-            pub #field_name: #field_type
+    let fields = {
+        let mut fields = Vec::new();
+        let mut seen = HashSet::new();
+        for segment in &segments {
+            match segment {
+                Segment::CollectionId(_) => {
+                    // do nothing
+                }
+                Segment::DocumentId(field_name, field_type) => {
+                    if seen.insert(field_name.to_string()) {
+                        fields.push(quote::quote! {
+                            pub #field_name: #field_type
+                        });
+                    }
+                }
+            }
         }
-    });
+        fields
+    };
 
     let path_method = {
         let push_first_segment = match segments.first() {
             Some(Segment::CollectionId(id)) => quote::quote! {
                 path.push_str(#id);
             },
-            Some(Segment::DocumentId(_)) | None => unreachable!(),
+            Some(Segment::DocumentId(_, _)) | None => unreachable!(),
         };
         let push_other_segments = segments.iter().skip(1).map(|segment| match segment {
             Segment::CollectionId(id) => quote::quote! {
                 path.push('/');
                 path.push_str(#id);
             },
-            Segment::DocumentId(field_name) => {
-                let field = syn::Ident::new(&field_name, proc_macro2::Span::call_site());
+            Segment::DocumentId(field_name, _) => {
+                let field =
+                    syn::Ident::new(&field_name.to_string(), proc_macro2::Span::call_site());
                 quote::quote! {
                     path.push('/');
                     path.push_str(self.#field.to_string().as_str());
@@ -94,20 +110,23 @@ fn format_macro_output(input: MacroInput, segments: Vec<Segment>) -> proc_macro:
     proc_macro::TokenStream::from(output)
 }
 
-enum Segment {
-    CollectionId(String),
-    DocumentId(String),
-}
-
-fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> {
-    let format = input.format.value();
+fn parse_format(
+    input_format: syn::LitStr,
+    input_args: &[(syn::Ident, syn::Type)],
+) -> Result<Vec<Segment>, syn::Error> {
+    let format = input_format.value();
 
     let segments = format.split('/').collect::<Vec<&str>>();
     if segments.is_empty() || segments.len() % 2 != 0 {
         return Err(syn::Error::new(
-            input.format.span(),
+            input_format.span(),
             "format must contain an even number of segments separated by '/'",
         ));
+    }
+
+    enum S {
+        C(String),
+        D(String),
     }
 
     let segments = segments
@@ -121,10 +140,10 @@ fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> 
                         .chars()
                         .all(|c| c.is_ascii_alphanumeric() || c == '_')
                 {
-                    Ok(Segment::CollectionId(segment.to_string()))
+                    Ok(S::C(segment.to_string()))
                 } else {
                     Err(syn::Error::new(
-                        input.format.span(),
+                        input_format.span(),
                         format!(
                             "format contains invalid collection id segment: '{}'",
                             segment
@@ -132,6 +151,11 @@ fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> 
                     ))
                 }
             } else {
+                // ??? TODO: Allow literal document id???
+                // ```rust
+                // firestore_path_helper!("col1/doc1/col2/{doc_id}", doc_id = i32);
+                // assert_eq!(Document { doc_id = 123 }.path(), "col1/doc1/col2/123");
+                // ```
                 if segment.starts_with('{') && segment.ends_with('}') {
                     let field_name = &segment[1..segment.len() - 1]; // remove '{' and '}'
                     if !field_name.is_empty()
@@ -144,26 +168,27 @@ fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> 
                             .chars()
                             .all(|c| c.is_ascii_alphanumeric() || c == '_')
                     {
-                        Ok(Segment::DocumentId(field_name.to_owned()))
+                        Ok(S::D(field_name.to_owned()))
                     } else {
                         Err(syn::Error::new(
-                            input.format.span(),
+                            input_format.span(),
                             format!("format contains invalid document id segment: '{}'", segment),
                         ))
                     }
                 } else {
                     Err(syn::Error::new(
-                        input.format.span(),
+                        input_format.span(),
                         format!("format contains invalid document id segment: '{}'", segment),
                     ))
                 }
             }
         })
-        .collect::<Result<Vec<Segment>, syn::Error>>()?;
+        .collect::<Result<Vec<S>, syn::Error>>()?;
 
     // Check for duplicate document id arguments
+    // (duplicate arguments are not allowed)
     let mut seen = HashSet::new();
-    for (ident, _) in &input.args {
+    for (ident, _) in input_args {
         if !seen.insert(ident.to_string()) {
             return Err(syn::Error::new(
                 ident.span(),
@@ -173,17 +198,17 @@ fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> 
     }
 
     // Check that all document id segments match the arguments
+    // (missing arguments are not allowed)
     for document_id in segments.iter().filter_map(|it| match it {
-        Segment::CollectionId(_) => None,
-        Segment::DocumentId(field_name) => Some(field_name),
+        S::C(_) => None,
+        S::D(field_name) => Some(field_name),
     }) {
-        if !input
-            .args
+        if !input_args
             .iter()
             .any(|(ident, _)| &ident.to_string() == document_id)
         {
             return Err(syn::Error::new(
-                input.format.span(),
+                input_format.span(),
                 format!(
                     "document id segment '{}' does not match any argument",
                     document_id
@@ -196,13 +221,14 @@ fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> 
     // firestore_path_helper!("col1/doc1");
     // assert_eq!(Document {}.path(), "col1/doc1");
     // ```
-    assert!(!input.args.is_empty());
+    assert!(!input_args.is_empty());
 
     // Check that all arguments match a document id segment
-    for (ident, _) in &input.args {
+    // (extra arguments are not allowed)
+    for (ident, _) in input_args {
         if !segments.iter().any(|segment| match segment {
-            Segment::CollectionId(_) => false,
-            Segment::DocumentId(field_name) => field_name == &ident.to_string(),
+            S::C(_) => false,
+            S::D(field_name) => field_name == &ident.to_string(),
         }) {
             return Err(syn::Error::new(
                 ident.span(),
@@ -214,5 +240,16 @@ fn validate_macro_input(input: &MacroInput) -> Result<Vec<Segment>, syn::Error> 
         }
     }
 
-    Ok(segments)
+    Ok(segments
+        .into_iter()
+        .map(|segment| match segment {
+            S::C(id) => Segment::CollectionId(id),
+            S::D(field_name) => input_args
+                .iter()
+                .find(|(ident, _)| &ident.to_string() == &field_name)
+                .cloned()
+                .map(|(ident, typ)| Segment::DocumentId(ident, typ))
+                .expect("document id segment must match an argument"),
+        })
+        .collect::<Vec<Segment>>())
 }
